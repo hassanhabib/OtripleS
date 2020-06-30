b8977f1a6126657a1cebd9d980ec83c1788c520e
Craigory Coppola <craigory.coppola@universalplant.com>
2020-06-30 08:34:33 -0500

    Added GetStudent(Guid) method to StorageBroker

--- OtripleS.Web.Api/Brokers/Storage/StorageBroker.Students.cs
+++ OtripleS.Web.Api/Brokers/Storage/StorageBroker.Students.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OtripleS.Web.Api.Models.Students;
+using System;
 
 namespace OtripleS.Web.Api.Brokers.Storage
 {
     public partial class StorageBroker
     {
         public DbSet<Student> Students { get; set; }
+
+        public Student GetStudent(Guid StudentId) => Students.Find(StudentId);
     }
 }
