18dc398ebbb8cc2e0211dbf5996dbbf8bd66d5a7
Craigory Coppola <craigory.coppola@universalplant.com>
2020-06-30 14:10:35 -0500

    Adding SelectStudentById to IStorageBroker

--- OtripleS.Web.Api/Brokers/Storage/IStorageBroker.cs
+++ OtripleS.Web.Api/Brokers/Storage/IStorageBroker.cs
@@ -1,6 +1,12 @@
-﻿namespace OtripleS.Web.Api.Brokers.Storage
+﻿using System;
+using System.Threading.Tasks;
+
+using OtripleS.Web.Api.Models.Students;
+
+namespace OtripleS.Web.Api.Brokers.Storage
 {
     public interface IStorageBroker
     {
+        public ValueTask<Student> SelectStudentById(Guid studentId);
     }
 }
