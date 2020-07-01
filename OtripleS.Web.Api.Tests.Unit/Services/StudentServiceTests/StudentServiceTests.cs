27644f9dc359a8c16cc81444263153f8457f95f9
eris.adhami <eri.adhami@gmail.com>
2020-07-01 15:39:50 +0200

    Added Unit Test ShouldDeleteStudentAsync()

---
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentServiceTests/StudentServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using OtripleS.Web.Api.Brokers.DateTimes;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.Students;
+using OtripleS.Web.Api.Services;
+using Tynamix.ObjectFiller;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.StudentServiceTests
+{
+    public partial class StudentServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly IStudentService studentService;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly DateTimeBroker dateTimeBroker;
+
+        public StudentServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.dateTimeBroker = new DateTimeBroker();
+
+            this.studentService = new StudentService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private Student CreateRandomStudent()
+        {
+            var filler = new Filler<Student>();
+            filler.Setup()
+                .OnProperty(student => student.BirthDate).Use(this.dateTimeBroker.GetCurrentDateTime())
+                .OnProperty(student => student.CreatedDate).Use(this.dateTimeBroker.GetCurrentDateTime())
+                .OnProperty(student => student.UpdatedDate).Use(this.dateTimeBroker.GetCurrentDateTime());
+            //return new Filler<Student>().Create();
+            return filler.Create();
+        }
+    }
+}
---
+++ OtripleS.Web.Api/Services/StudentService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.Students;
+
+namespace OtripleS.Web.Api.Services
+{
+    public partial class StudentService : IStudentService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public StudentService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public async ValueTask<Student> DeleteStudentAsync(Guid studentId)
+        {
+            Student maybeStudent =
+                await this.storageBroker.SelectStudentByIdAsync(studentId);
+
+            return await this.storageBroker.DeleteStudentAsync(maybeStudent);
+        }
+    }
+}
