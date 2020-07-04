c0e7feff537159a3d7001a50477b71c99980f2b4
Viral Pandya <virav.pandya@gmail.com>
2020-07-04 16:16:32 -0700

    Controller - Get

--- OtripleS.Web.Api/Controllers/StudentController.cs
+++ OtripleS.Web.Api/Controllers/StudentController.cs
@@ -2,12 +2,13 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OtripleS.Web.Api.Models.Students;
+using OtripleS.Web.Api.Models.Students.Exceptions;
 using OtripleS.Web.Api.Services;
 
 namespace OtripleS.Web.Api.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class StudentController : ControllerBase
     {
         private readonly IStudentService studentService;
@@ -24,6 +25,37 @@ namespace OtripleS.Web.Api.Controllers
                 await this.studentService.DeleteStudentAsync(studentId);
 
             return Ok(storageStudent);
+        }
+
+        [HttpGet("{studentId}")]
+        public async ValueTask<ActionResult<Student>> GetStudentAsync(Guid studentId)
+        {
+            try
+            {
+                Student storageStudent =
+                        await this.studentService.RetrieveStudentByIdAsync(studentId);
+
+                return Ok(storageStudent);
+            }
+            catch (StudentValidationException studentValidationException)
+                when (studentValidationException.InnerException is NotFoundStudentException)
+            {
+                string innerMessage = studentValidationException.InnerException.Message;
+                return NotFound(innerMessage);
+            }
+            catch (StudentValidationException studentValidationException)
+            {
+                string innerMessage = studentValidationException.InnerException.Message;
+                return BadRequest(studentValidationException);
+            }
+            catch (StudentDependencyException studentDependencyException)
+            {
+                return Problem(studentDependencyException.Message);
+            }
+            catch (StudentServiceException studentServiceException)
+            {
+                return Problem(studentServiceException.Message);
+            }
         }
     }
 }
