1a172b375a0a03afe60beaa427107d36bc76a1d3
Hassan Habib <hassanhabib@live.com>
2020-07-05 17:37:36 -0700

    ShouldThorwValidationExceptionOnModifyWhenStudentIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentServiceTests/StudentServiceTests.Validations.Modify.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.Students;
+using OtripleS.Web.Api.Models.Students.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.StudentServiceTests
+{
+    public partial class StudentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThorwValidationExceptionOnModifyWhenStudentIsNullAndLogItAsync()
+        {
+            // given
+            Student invalidStudent = null;
+
+            var nullStudentException = new NullStudentException();
+
+            var expectedStudentValidationException =
+                new StudentValidationException(nullStudentException);
+
+            // when
+            ValueTask<Student> modifyStudentTask =
+                this.studentService.ModifyStudentAsync(invalidStudent);
+
+            // then
+            await Assert.ThrowsAsync<StudentValidationException>(() => 
+                modifyStudentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentValidationException))),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        } 
+    }
+}
