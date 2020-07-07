c36df6bf22913abc34b0755cbe95ebb4b1468827
Viral Pandya <virav.pandya@gmail.com>
2020-07-06 19:16:01 -0700

    ShouldThrowValidatonExceptionOnDeleteWhenIdIsInvalidAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/TeacherServiceTests/TeacherServiceTests.Exceptions.Delete.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.Teachers;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.TeacherServiceTests
+{
+    public partial class TeacherServiceTests
+    {
+        
+    }
+}
