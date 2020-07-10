4daddb3831474982e8f30b74091d29dd304f0b10
Viral Pandya <virav.pandya@gmail.com>
2020-07-09 19:45:50 -0700

    ShouldThrowValidationExceptionOnModifyWhenTeacherIdIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/TeacherServiceTests/TeacherServiceTests.Validations.Modify.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/TeacherServiceTests/TeacherServiceTests.Validations.Modify.cs
@@ -3,6 +3,7 @@
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using Moq;
 using OtripleS.Web.Api.Models.Teachers;
@@ -22,6 +23,40 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.TeacherServiceTests
 
             var expectedTeacherValidationException =
                 new TeacherValidationException(nullTeacherException);
+
+            // when
+            ValueTask<Teacher> modifyTeacherTask =
+                this.teacherService.ModifyTeacherAsync(invalidTeacher);
+
+            // then
+            await Assert.ThrowsAsync<TeacherValidationException>(() =>
+                modifyTeacherTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedTeacherValidationException))),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyWhenTeacherIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidTeacherId = Guid.Empty;
+            DateTimeOffset dateTime = GetRandomDateTime();
+            Teacher randomTeacher = CreateRandomTeacher(dateTime);
+            Teacher invalidTeacher = randomTeacher;
+            invalidTeacher.Id = invalidTeacherId;
+
+            var invalidTeacherException = new InvalidTeacherInputException(
+                parameterName: nameof(Teacher.Id),
+                parameterValue: invalidTeacher.Id);
+
+            var expectedTeacherValidationException =
+                new TeacherValidationException(invalidTeacherException);
 
             // when
             ValueTask<Teacher> modifyTeacherTask =
