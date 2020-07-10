8c63cf8dbb06ab41a3ab36dd022af780d1b7ddf5
tobidub <github@tobiasdubral.de>
2020-07-10 22:09:07 +0200

    TeacherPostAcceptane Test

---
+++ OtripleS.Web.Api.Tests.Acceptance/APIs/Teachers/TeachersApiTests.Logic.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using OtripleS.Web.Api.Models.Teachers;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Acceptance.APIs.Teachers
+{
+	public partial class TeachersApiTests
+	{
+		[Fact]
+		public async Task ShouldPostTeacherAsync()
+		{
+			// given
+			Teacher randomTeacher = CreateRandomTeacher();
+			Teacher inputTeacher = randomTeacher;
+			Teacher expectedTeacher = inputTeacher;
+
+			// when 
+			await this.otripleSApiBroker.PostTeacherAsync(inputTeacher);
+
+			Teacher actualTeacher =
+				await this.otripleSApiBroker.GetTeacherByIdAsync(inputTeacher.Id);
+
+			// then
+			actualTeacher.Should().BeEquivalentTo(expectedTeacher);
+
+			await this.otripleSApiBroker.DeleteTeacherByIdAsync(actualTeacher.Id);
+		}
+
+	}
+}
---
+++ OtripleS.Web.Api.Tests.Acceptance/Brokers/OtripleSApiBroker.Teacher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Models.Teachers;
+
+namespace OtripleS.Web.Api.Tests.Acceptance.Brokers
+{
+    public partial class OtripleSApiBroker
+    {
+        private const string TeachersRelativeUrl = "api/teachers";
+
+        public async ValueTask<Teacher> PostTeacherAsync(Teacher student) =>
+            await this.apiFactoryClient.PostContentAsync(TeachersRelativeUrl, student);
+
+        public async ValueTask<Teacher> GetTeacherByIdAsync(Guid studentId) =>
+            await this.apiFactoryClient.GetContentAsync<Teacher>($"{TeachersRelativeUrl}/{studentId}");
+
+        public async ValueTask<Teacher> DeleteTeacherByIdAsync(Guid studentId) =>
+            await this.apiFactoryClient.DeleteContentAsync<Teacher>($"{TeachersRelativeUrl}/{studentId}");
+
+        public async ValueTask<Teacher> PutTeacherAsync(Teacher student) =>
+            await this.apiFactoryClient.PutContentAsync(TeachersRelativeUrl, student);
+
+        public async ValueTask<List<Teacher>> GetAllTeachers() =>
+            await this.apiFactoryClient.GetContentAsync<List<Teacher>>($"{TeachersRelativeUrl}/");
+
+    }
+}
