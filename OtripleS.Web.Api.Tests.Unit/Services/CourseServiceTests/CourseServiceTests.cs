243a68721b2fcff0f8004e67c81feecd3b60a05b
gauri <gauri.ramesh15@gmail.com>
2020-07-14 13:57:51 -0500

    Class Declarations for Services

---
+++ OtripleS.Web.Api.Tests.Unit/Services/CourseServiceTests/CourseServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.CourseServiceTests
+{
+    public partial class CourseServiceTests
+    {
+    }
+}
---
+++ OtripleS.Web.Api/Services/Courses/CourseService.Exceptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtripleS.Web.Api.Services.Courses
+{
+    public partial class CourseService
+    {
+    }
+}
