5436782daa585637b5a99c3230224268369df0f1
gauri <gauri.ramesh15@gmail.com>
2020-07-15 14:26:14 -0500

    ShouldThrowValidationExceptionOnModifyWhenCourseIsNullAndLogItAsync() => FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/CourseServiceTests/CourseServiceTests.Validations.Modify.cs
@@ -0,0 +1,49 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+
+using Moq;
+using OtripleS.Web.Api.Models.Courses;
+using OtripleS.Web.Api.Models.Courses.Exceptions;
+using OtripleS.Web.Api.Models.Students.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.CourseServiceTests
+{
+    public partial class CourseServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyWhenCourseIsNullAndLogItAsync()
+        {
+            //given
+            Course invalidCourse = null;
+
+            var nullCourseException = new NullCourseException();
+            var expectedCourseValidationException = 
+                new CourseValidationException(nullCourseException);
+
+            //when
+            ValueTask<Course> modifyCourseTask =
+                this.courseService.ModifyCourseAsync(invalidCourse);
+
+            //then
+            await Assert.ThrowsAsync<CourseValidationException>(() =>
+                modifyCourseTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedCourseValidationException))),
+                Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
---
+++ OtripleS.Web.Api/Models/Courses/Exceptions/NullCourseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtripleS.Web.Api.Models.Courses.Exceptions
+{
+    public class NullCourseException : Exception
+    {
+        public NullCourseException() :base("The course is null.")
+        {
+        }
+    }
+}
