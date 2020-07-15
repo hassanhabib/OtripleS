231313e268b240dc9ea1602649819885261e5a62
eris.adhami <eri.adhami@gmail.com>
2020-07-15 14:21:28 +0200

    ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync - FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/CourseServiceTests/CourseServiceTests.Exceptions.Delete.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/CourseServiceTests/CourseServiceTests.Exceptions.Delete.cs
@@ -3,10 +3,52 @@
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
 
+using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Moq;
+using OtripleS.Web.Api.Models.Courses;
+using OtripleS.Web.Api.Models.Courses.Exceptions;
+using Xunit;
+
 namespace OtripleS.Web.Api.Tests.Unit.Services.CourseServiceTests
 {
     public partial class CourseServiceTests
     {
-        
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid randomCourseId = Guid.NewGuid();
+            Guid inputCourseId = randomCourseId;
+            SqlException sqlException = GetSqlException();
+
+            var expectedCourseDependencyException =
+                new CourseDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectCourseByIdAsync(inputCourseId))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Course> deleteCourseTask =
+                this.courseService.DeleteCourseAsync(inputCourseId);
+
+            // then
+            await Assert.ThrowsAsync<CourseDependencyException>(() =>
+                deleteCourseTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedCourseDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCourseByIdAsync(inputCourseId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
--- OtripleS.Web.Api.Tests.Unit/Services/CourseServiceTests/CourseServiceTests.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/CourseServiceTests/CourseServiceTests.cs
@@ -5,6 +5,8 @@
 
 using System;
 using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using Microsoft.Data.SqlClient;
 using Moq;
 using OtripleS.Web.Api.Brokers.DateTimes;
 using OtripleS.Web.Api.Brokers.Loggings;
@@ -56,6 +58,9 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.CourseServiceTests
 
             return filler;
         }
+
+        private static SqlException GetSqlException() =>
+            (SqlException)FormatterServices.GetUninitializedObject(typeof(SqlException));
 
     }
 }
