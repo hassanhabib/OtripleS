933248545289034c43d9f9901cbb3ccbfa216a36
tobidub <github@tobiasdubral.de>
2020-07-17 18:52:04 +0200

    Delete Character

--- OtripleS.Web.Api/Services/Courses/CourseService.cs
+++ OtripleS.Web.Api/Services/Courses/CourseService.cs
@@ -1,4 +1,4 @@
-﻿v// ---------------------------------------------------------------
+﻿// ---------------------------------------------------------------
 // Copyright (c) Coalition of the Good-Hearted Engineers
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
