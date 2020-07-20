6d512ea309ab225b3db98af7f9327dd6165815a0
Hiral Desai <hiraldesai@gmail.com>
2020-07-20 14:02:38 -0700

    Adding copyright notice to new files

--- OtripleS.Web.Api/Migrations/20200720164344_AddClassroom.cs
+++ OtripleS.Web.Api/Migrations/20200720164344_AddClassroom.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
 using Microsoft.EntityFrameworkCore.Migrations;
+
+using System;
 
 namespace OtripleS.Web.Api.Migrations
 {
--- OtripleS.Web.Api/Models/Classrooms/ClassroomStatus.cs
+++ OtripleS.Web.Api/Models/Classrooms/ClassroomStatus.cs
@@ -1,4 +1,9 @@
-﻿namespace OtripleS.Web.Api.Models.Classrooms
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+namespace OtripleS.Web.Api.Models.Classrooms
 {
     public enum ClassroomStatus
     {
