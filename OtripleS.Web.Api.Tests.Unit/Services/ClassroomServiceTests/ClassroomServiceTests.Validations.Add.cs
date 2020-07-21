f965763b4f632a124d0189b89553b6b3f4506e65
Viral Pandya <virav.pandya@gmail.com>
2020-07-20 21:44:57 -0700

    ShouldThrowValidationExceptionOnCreateWhenClassroomIsNullAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/ClassroomServiceTests/ClassroomServiceTests.Validations.Add.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/ClassroomServiceTests/ClassroomServiceTests.Validations.Add.cs
@@ -3,9 +3,47 @@
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
 
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.Classrooms;
+using OtripleS.Web.Api.Models.Classrooms.Exceptions;
+using Xunit;
+
 namespace OtripleS.Web.Api.Tests.Unit.Services.ClassroomServiceTests
 {
     public partial class ClassroomServiceTests
     {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnCreateWhenClassroomIsNullAndLogItAsync()
+        {
+            // given
+            Classroom randomClassroom = null;
+            Classroom nullClassroom = randomClassroom;
+            var nullClassroomException = new NullClassroomException();
+
+            var expectedClassroomValidationException =
+                new ClassroomValidationException(nullClassroomException);
+
+            // when
+            ValueTask<Classroom> createClassroomTask =
+                this.classroomService.CreateClassroomAsync(nullClassroom);
+
+            // then
+            await Assert.ThrowsAsync<ClassroomValidationException>(() =>
+                createClassroomTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedClassroomValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectClassroomByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
