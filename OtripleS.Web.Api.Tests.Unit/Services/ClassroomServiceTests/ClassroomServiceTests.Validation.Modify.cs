8eab5142214afe6bb0aa7f96664901b2ffe6d43b
tobidub <github@tobiasdubral.de>
2020-07-22 18:44:13 +0200

    ShouldThrowValidationExceptionOnModifyWhenClassroomIdIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/ClassroomServiceTests/ClassroomServiceTests.Validation.Modify.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/ClassroomServiceTests/ClassroomServiceTests.Validation.Modify.cs
@@ -3,12 +3,12 @@
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using Moq;
 using OtripleS.Web.Api.Models.Classrooms;
 using OtripleS.Web.Api.Models.Classrooms.Exceptions;
 using Xunit;
-
 
 namespace OtripleS.Web.Api.Tests.Unit.Services.ClassroomServiceTests
 {
@@ -40,5 +40,39 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.ClassroomServiceTests
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
-    }
+
+		[Fact]
+		public async Task ShouldThrowValidationExceptionOnModifyWhenClassroomIdIsInvalidAndLogItAsync()
+		{
+			//given
+			Guid invalidClassroomId = Guid.Empty;
+			DateTimeOffset dateTime = GetRandomDateTime();
+			Classroom randomClassroom = CreateRandomClassroom(dateTime);
+			Classroom invalidClassroom = randomClassroom;
+			invalidClassroom.Id = invalidClassroomId;
+
+			var invalidClassroomException = new InvalidClassroomInputException(
+				parameterName: nameof(Classroom.Id),
+				parameterValue: invalidClassroom.Id);
+
+			var expectedClassroomValidationException =
+				new ClassroomValidationException(invalidClassroomException);
+
+			//when
+			ValueTask<Classroom> modifyClassroomTask =
+				this.classroomService.ModifyClassroomAsync(invalidClassroom);
+
+			//then
+			await Assert.ThrowsAsync<ClassroomValidationException>(() =>
+				modifyClassroomTask.AsTask());
+
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogError(It.Is(SameExceptionAs(expectedClassroomValidationException))),
+				Times.Once);
+
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.dateTimeBrokerMock.VerifyNoOtherCalls();
+		}
+	}
 }
