33fdcba2cd945d00a1530d7f086604ccb1de6d16
tobidub <github@tobiasdubral.de>
2020-07-22 19:00:04 +0200

    ShouldThrowValidationExceptionOnModifyWhenCreatedByIsInvalidAndLogItAsync -> PASS

--- OtripleS.Web.Api/Services/Classrooms/ClassroomService.Validations.cs
+++ OtripleS.Web.Api/Services/Classrooms/ClassroomService.Validations.cs
@@ -9,138 +9,152 @@ using OtripleS.Web.Api.Models.Classrooms.Exceptions;
 
 namespace OtripleS.Web.Api.Services.Classrooms
 {
-    public partial class ClassroomService
-    {
-        private void ValidateClassroom(Classroom classroom)
-        {
-            ValidateClassroomIsNull(classroom);
-            ValidateClassroomIdIsNull(classroom);
-            ValidateClassroomFields(classroom);
-            ValidateInvalidAuditFieldsOnCreate(classroom);
-            ValidateAuditFieldsDataOnCreate(classroom);
-        }
-
-        private void ValidateAuditFieldsDataOnCreate(Classroom classroom)
-        {
-            switch (classroom)
-            {
-                case { } when classroom.UpdatedBy != classroom.CreatedBy:
-                    throw new InvalidClassroomException(
-                    parameterName: nameof(Classroom.UpdatedBy),
-                    parameterValue: classroom.UpdatedBy);
-
-                case { } when classroom.UpdatedDate != classroom.CreatedDate:
-                    throw new InvalidClassroomException(
-                    parameterName: nameof(Classroom.UpdatedDate),
-                    parameterValue: classroom.UpdatedDate);
-
-                case { } when IsDateNotRecent(classroom.CreatedDate):
-                    throw new InvalidClassroomException(
-                    parameterName: nameof(Classroom.CreatedDate),
-                    parameterValue: classroom.CreatedDate);
-            }
-        }
-
-        private void ValidateInvalidAuditFieldsOnCreate(Classroom classroom)
-        {
-            switch (classroom)
-            {
-                case { } when classroom.CreatedBy == default:
-                    throw new InvalidClassroomException(
-                    parameterName: nameof(Classroom.CreatedBy),
-                    parameterValue: classroom.CreatedBy);
-
-                case { } when classroom.CreatedDate == default:
-                    throw new InvalidClassroomException(
-                    parameterName: nameof(Classroom.CreatedDate),
-                    parameterValue: classroom.CreatedDate);
-
-                case { } when classroom.UpdatedBy == default:
-                    throw new InvalidClassroomException(
-                    parameterName: nameof(Classroom.UpdatedBy),
-                    parameterValue: classroom.UpdatedBy);
-
-                case { } when classroom.UpdatedDate == default:
-                    throw new InvalidClassroomException(
-                    parameterName: nameof(Classroom.UpdatedDate),
-                    parameterValue: classroom.UpdatedDate);
-            }
-        }
-
-        private void ValidateClassroomFields(Classroom classroom)
-        {
-            if (IsEmpty(classroom.Name))
-            {
-                throw new InvalidClassroomException(
-                    parameterName: nameof(Classroom.Name),
-                    parameterValue: classroom.Name);
-            }
-        }
-
-        private void ValidateClassroomIsNull(Classroom classroom)
-        {
-            if (classroom is null)
-            {
-                throw new NullClassroomException();
-            }
-        }
-
-        private void ValidateClassroomIdIsNull(Classroom classroom)
-        {
-            if (classroom.Id == default)
-            {
-                throw new InvalidClassroomException(
-                    parameterName: nameof(Classroom.Id),
-                    parameterValue: classroom.Id);
-            }
-        }
-
-        private void ValidateClassroomStrings(Classroom classroom)
-        {
-            switch (classroom)
-            {
-                case { } when IsInvalid(classroom.Name):
-                    throw new InvalidClassroomInputException(
-                        parameterName: nameof(Classroom.Name),
-                        parameterValue: classroom.Name);
-            }
-        }
-
-        private static bool IsEmpty(string input) => String.IsNullOrWhiteSpace(input);
-        private static bool IsInvalid(string input) => String.IsNullOrWhiteSpace(input);
-
-        private bool IsDateNotRecent(DateTimeOffset dateTime)
-        {
-            DateTimeOffset now = this.dateTimeBroker.GetCurrentDateTime();
-            int oneMinute = 1;
-            TimeSpan difference = now.Subtract(dateTime);
-
-            return Math.Abs(difference.TotalMinutes) > oneMinute;
-        }
-
-        private void ValidateClassroomId(Guid classroomId)
-        {
-            if (classroomId == Guid.Empty)
-            {
-                throw new InvalidClassroomInputException(
-                    parameterName: nameof(Classroom.Id),
-                    parameterValue: classroomId);
-            }
-        }
-
-        private void ValidateStorageClassroom(Classroom storageClassroom, Guid classroomId)
-        {
-            if (storageClassroom == null)
-            {
-                throw new NotFoundClassroomException(classroomId);
-            }
-        }
-
-        private void ValidateClassroomOnModify(Classroom classroom)
-        {
+	public partial class ClassroomService
+	{
+		private void ValidateClassroom(Classroom classroom)
+		{
 			ValidateClassroomIsNull(classroom);
-            ValidateClassroomIdIsNull(classroom);
-            ValidateClassroomStrings(classroom);
+			ValidateClassroomIdIsNull(classroom);
+			ValidateClassroomFields(classroom);
+			ValidateInvalidAuditFieldsOnCreate(classroom);
+			ValidateAuditFieldsDataOnCreate(classroom);
 		}
-    }
+
+		private void ValidateAuditFieldsDataOnCreate(Classroom classroom)
+		{
+			switch (classroom)
+			{
+				case { } when classroom.UpdatedBy != classroom.CreatedBy:
+					throw new InvalidClassroomException(
+					parameterName: nameof(Classroom.UpdatedBy),
+					parameterValue: classroom.UpdatedBy);
+
+				case { } when classroom.UpdatedDate != classroom.CreatedDate:
+					throw new InvalidClassroomException(
+					parameterName: nameof(Classroom.UpdatedDate),
+					parameterValue: classroom.UpdatedDate);
+
+				case { } when IsDateNotRecent(classroom.CreatedDate):
+					throw new InvalidClassroomException(
+					parameterName: nameof(Classroom.CreatedDate),
+					parameterValue: classroom.CreatedDate);
+			}
+		}
+
+		private void ValidateInvalidAuditFieldsOnCreate(Classroom classroom)
+		{
+			switch (classroom)
+			{
+				case { } when classroom.CreatedBy == default:
+					throw new InvalidClassroomException(
+					parameterName: nameof(Classroom.CreatedBy),
+					parameterValue: classroom.CreatedBy);
+
+				case { } when classroom.CreatedDate == default:
+					throw new InvalidClassroomException(
+					parameterName: nameof(Classroom.CreatedDate),
+					parameterValue: classroom.CreatedDate);
+
+				case { } when classroom.UpdatedBy == default:
+					throw new InvalidClassroomException(
+					parameterName: nameof(Classroom.UpdatedBy),
+					parameterValue: classroom.UpdatedBy);
+
+				case { } when classroom.UpdatedDate == default:
+					throw new InvalidClassroomException(
+					parameterName: nameof(Classroom.UpdatedDate),
+					parameterValue: classroom.UpdatedDate);
+			}
+		}
+
+		private void ValidateClassroomFields(Classroom classroom)
+		{
+			if (IsEmpty(classroom.Name))
+			{
+				throw new InvalidClassroomException(
+					parameterName: nameof(Classroom.Name),
+					parameterValue: classroom.Name);
+			}
+		}
+
+		private void ValidateClassroomIsNull(Classroom classroom)
+		{
+			if (classroom is null)
+			{
+				throw new NullClassroomException();
+			}
+		}
+
+		private void ValidateClassroomIdIsNull(Classroom classroom)
+		{
+			if (classroom.Id == default)
+			{
+				throw new InvalidClassroomException(
+					parameterName: nameof(Classroom.Id),
+					parameterValue: classroom.Id);
+			}
+		}
+
+		private void ValidateClassroomStrings(Classroom classroom)
+		{
+			switch (classroom)
+			{
+				case { } when IsInvalid(classroom.Name):
+					throw new InvalidClassroomInputException(
+						parameterName: nameof(Classroom.Name),
+						parameterValue: classroom.Name);
+			}
+		}
+
+		private void ValidateClassroomIds(Classroom classroom)
+		{
+			switch (classroom)
+			{
+				case { } when IsInvalid(classroom.CreatedBy):
+					throw new InvalidClassroomInputException(
+						parameterName: nameof(Classroom.CreatedBy),
+						parameterValue: classroom.CreatedBy);
+
+			}
+		}
+
+		private static bool IsEmpty(string input) => String.IsNullOrWhiteSpace(input);
+		private static bool IsInvalid(string input) => String.IsNullOrWhiteSpace(input);
+		private static bool IsInvalid(Guid input) => input == default;
+
+		private bool IsDateNotRecent(DateTimeOffset dateTime)
+		{
+			DateTimeOffset now = this.dateTimeBroker.GetCurrentDateTime();
+			int oneMinute = 1;
+			TimeSpan difference = now.Subtract(dateTime);
+
+			return Math.Abs(difference.TotalMinutes) > oneMinute;
+		}
+
+		private void ValidateClassroomId(Guid classroomId)
+		{
+			if (classroomId == Guid.Empty)
+			{
+				throw new InvalidClassroomInputException(
+					parameterName: nameof(Classroom.Id),
+					parameterValue: classroomId);
+			}
+		}
+
+		private void ValidateStorageClassroom(Classroom storageClassroom, Guid classroomId)
+		{
+			if (storageClassroom == null)
+			{
+				throw new NotFoundClassroomException(classroomId);
+			}
+		}
+
+		private void ValidateClassroomOnModify(Classroom classroom)
+		{
+			ValidateClassroomIsNull(classroom);
+			ValidateClassroomIdIsNull(classroom);
+			ValidateClassroomStrings(classroom);
+			ValidateClassroomIds(classroom);
+		}
+	}
 }
