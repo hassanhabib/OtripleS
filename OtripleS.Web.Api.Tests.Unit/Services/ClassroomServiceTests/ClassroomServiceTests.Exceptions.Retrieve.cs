f8d75567a76a44f1eacbfcaf963a331fdf960801
devmanzur <devmanzur@gmail.com>
2020-07-24 00:22:39 +0600

    ShouldThrowDependencyExceptionOnRetrieveByIdWhenSqlExceptionOccursAndLogItAsync

---
+++ OtripleS.Web.Api.Tests.Unit/Services/ClassroomServiceTests/ClassroomServiceTests.Exceptions.Retrieve.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Moq;
+using OtripleS.Web.Api.Models.Classrooms;
+using OtripleS.Web.Api.Models.Classrooms.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.ClassroomServiceTests
+{
+    public partial class ClassroomServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveByIdWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid randomClassroomId = Guid.NewGuid();
+            Guid inputClassroomId = randomClassroomId;
+            SqlException sqlException = GetSqlException();
+            
+            var expectedClassroomDependencyException =
+                new ClassroomDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                    broker.SelectClassroomByIdAsync(inputClassroomId))
+                .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Classroom> retrieveClassroomTask =
+                this.classroomService.RetrieveClassroomById(inputClassroomId);
+            
+            // then
+            await Assert.ThrowsAsync<ClassroomDependencyException>(() =>
+                retrieveClassroomTask.AsTask());
+            
+            this.loggingBrokerMock.Verify(broker =>
+                    broker.LogCritical(It.Is(SameExceptionAs(expectedClassroomDependencyException))),
+                Times.Once);
+            
+            this.storageBrokerMock.Verify(broker =>
+                    broker.SelectClassroomByIdAsync(inputClassroomId),
+                Times.Once);
+            
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
