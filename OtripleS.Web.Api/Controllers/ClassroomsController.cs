93fc4fd2435bb634e669f22a3543227c3c2e13d2
Mohamad Ali <mohamad.ali@dhhs.vic.gov.au>
2020-07-24 11:15:03 +1000

    Add Get All Classroom endpoint

---
+++ OtripleS.Web.Api/Controllers/ClassroomsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using OtripleS.Web.Api.Models.Classrooms;
+using OtripleS.Web.Api.Models.Classrooms.Exceptions;
+using OtripleS.Web.Api.Services.Classrooms;
+using RESTFulSense.Controllers;
+
+namespace OtripleS.Web.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ClassroomsController : RESTFulController
+	{
+		private readonly IClassroomService classroomService;
+
+		public ClassroomsController(IClassroomService classroomService) =>
+			this.classroomService = classroomService;
+
+		[HttpGet]
+		public ActionResult<IQueryable<Classroom>> GetAllClassrooms()
+		{
+			try
+			{
+				IQueryable storageClassroom =
+					this.classroomService.RetrieveAllClassrooms();
+
+				return Ok(storageClassroom);
+			}
+			catch (ClassroomDependencyException classRoomDependencyException)
+			{
+				return Problem(classRoomDependencyException.Message);
+			}
+			catch (ClassroomServiceException classRoomServiceException)
+			{
+				return Problem(classRoomServiceException.Message);
+			}
+		}
+	}
+}
