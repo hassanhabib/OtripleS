de9ab83074fa95de2c0f18dec8bb1e1cee6de668
Hatem Gamal Abbas <hatooma_90@hotmail.com>
2020-07-28 16:56:12 +0300

    Assignments Broker

--- OtripleS.Web.Api/Brokers/Storage/IStorageBroker.Assignments.cs
+++ OtripleS.Web.Api/Brokers/Storage/IStorageBroker.Assignments.cs
@@ -3,9 +3,19 @@
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
 
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Models.Assignments;
+
 namespace OtripleS.Web.Api.Brokers.Storage
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Assignment> InsertAssignmentAsync(Assignment Classroom);
+        IQueryable<Assignment> SelectAllAssignments();
+        ValueTask<Assignment> SelectAssignmentByIdAsync(Guid ClassroomId);
+        ValueTask<Assignment> UpdateAssignmentAsync(Assignment Classroom);
+        ValueTask<Assignment> DeleteAssignmentAsync(Assignment Classroom);
     }
 }
