8c5b5fd946cf9ce15ff12b9f0534355d7c436f26
eris.adhami <eri.adhami@gmail.com>
2020-07-29 11:11:00 +0200

    ShouldCreateAssignmentAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/AssignmentServiceTests/AssignmentServiceTests.Logic.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.Assignments;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.AssignmentServiceTests
+{
+    public partial class AssignmentServiceTests
+    {
+		[Fact]
+		public async Task ShouldCreateAssignmentAsync()
+		{
+			// given
+			DateTimeOffset randomDateTime = GetRandomDateTime();
+			DateTimeOffset dateTime = randomDateTime;
+			Assignment randomAssignment = CreateRandomAssignment(randomDateTime);
+			randomAssignment.UpdatedBy = randomAssignment.CreatedBy;
+			randomAssignment.UpdatedDate = randomAssignment.CreatedDate;
+			Assignment inputAssignment= randomAssignment;
+			Assignment storageAssignment = randomAssignment;
+			Assignment expectedAssignment = storageAssignment;
+
+			this.storageBrokerMock.Setup(broker =>
+				broker.InsertAssignmentAsync(inputAssignment))
+					.ReturnsAsync(storageAssignment);
+
+			// when
+			Assignment actualAssignment =
+				await this.assignmentService.CreateAssignmentAsync(inputAssignment);
+
+			// then
+			actualAssignment.Should().BeEquivalentTo(expectedAssignment);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.InsertAssignmentAsync(inputAssignment),
+					Times.Once);
+
+			this.dateTimeBrokerMock.VerifyNoOtherCalls();
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.storageBrokerMock.VerifyNoOtherCalls();
+		}
+	}
+}
---
+++ OtripleS.Web.Api.Tests.Unit/Services/AssignmentServiceTests/AssignmentServiceTests.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Moq;
+using OtripleS.Web.Api.Brokers.DateTimes;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.Assignments;
+using OtripleS.Web.Api.Services.Assignments;
+using Tynamix.ObjectFiller;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.AssignmentServiceTests
+{
+    public partial class AssignmentServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly IAssignmentService assignmentService;
+
+        public AssignmentServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+
+            this.assignmentService = new AssignmentService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private Assignment CreateRandomAssignment(DateTimeOffset dates) =>
+            CreateAssignmentFiller(dates).Create();
+
+        private static Filler<Assignment> CreateAssignmentFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Assignment>();
+
+            filler.Setup()
+                .OnProperty(assignment => assignment.CreatedDate).Use(dates)
+                .OnProperty(assignment => assignment.UpdatedDate).Use(dates)
+                .OnProperty(assignment => assignment.Deadline).Use(dates.AddDays(GetRandomNumber()));
+
+            return filler;
+        }
+
+        private static int GetRandomNumber() => new IntRange(min: 1, max: 150).GetValue();
+    }
+}
