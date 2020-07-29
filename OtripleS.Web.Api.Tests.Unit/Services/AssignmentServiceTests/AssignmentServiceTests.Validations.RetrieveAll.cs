1dbcc8aa5b33e9c3cf1fc50b7053c28579cd32cf
syed <syedib@outlook.com>
2020-07-29 08:14:59 +0530

    ShouldLogWarningOnRetrieveAllWhenAssignmentsWasEmptyAndLogIt -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/AssignmentServiceTests/AssignmentServiceTests.Validations.RetrieveAll.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.Assignments;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.AssignmentServiceTests
+{
+    public partial class AssignmentServiceTests
+    {
+        [Fact]
+        public void ShouldLogWarningOnRetrieveAllWhenAssignmentsWasEmptyAndLogIt()
+        {
+            // given
+            IQueryable<Assignment> emptyStorageAssignments = new List<Assignment>().AsQueryable();
+            IQueryable<Assignment> expectedAssignments = emptyStorageAssignments;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllAssignments())
+                    .Returns(expectedAssignments);
+
+            // when
+            IQueryable<Assignment> actualAssignments =
+                this.assignmentService.RetrieveAllAssignments();
+
+            // then
+            actualAssignments.Should().BeEquivalentTo(emptyStorageAssignments);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogWarning("No Assignments found in storage."),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllAssignments(),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
