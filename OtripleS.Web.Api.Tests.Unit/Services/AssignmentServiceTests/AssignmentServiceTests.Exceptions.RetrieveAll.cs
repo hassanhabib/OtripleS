e5e9099ab60cb946c6f869aca46cf94cc24a9231
syed <syedib@outlook.com>
2020-07-29 07:47:56 +0530

    ShouldThrowDependencyExceptionOnRetrieveAllWhenDbExceptionOccursAndLogIt -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/AssignmentServiceTests/AssignmentServiceTests.Exceptions.RetrieveAll.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/AssignmentServiceTests/AssignmentServiceTests.Exceptions.RetrieveAll.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using OtripleS.Web.Api.Models.Assignments.Exceptions;
 using Xunit;
@@ -25,6 +26,39 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.AssignmentServiceTests
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(expectedAssignmentDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllAssignments(),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+        [Fact]
+        public void ShouldThrowDependencyExceptionOnRetrieveAllWhenDbExceptionOccursAndLogIt()
+        {
+            // given
+            var databaseUpdateException = new DbUpdateException();
+
+            var expectedAssignmentDependencyException =
+                new AssignmentDependencyException(databaseUpdateException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllAssignments())
+                    .Throws(databaseUpdateException);
+
+            // when . then
+            Assert.Throws<AssignmentDependencyException>(() =>
+                this.assignmentService.RetrieveAllAssignments());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedAssignmentDependencyException))),
                     Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
