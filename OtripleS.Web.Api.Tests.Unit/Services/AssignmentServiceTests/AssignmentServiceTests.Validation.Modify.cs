d6e4f5a0f51bc1266299f4adca07753a50a1eee2
tobidub <github@tobiasdubral.de>
2020-07-29 21:46:16 +0200

    ShouldThrowValidationExceptionOnModifyWhenAssignmentIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/AssignmentServiceTests/AssignmentServiceTests.Validation.Modify.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.Assignments;
+using OtripleS.Web.Api.Models.Assignments.Exceptions;
+using Xunit;
+using EFxceptions.Models.Exceptions;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.AssignmentServiceTests
+{
+    public partial class AssignmentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyWhenAssignmentIsNullAndLogItAsync()
+        {
+            //given
+            Assignment invalidAssignment = null;
+            var nullAssignmentException = new NullAssignmentException();
+
+            var expectedAssignmentValidationException =
+                new AssignmentValidationException(nullAssignmentException);
+
+            //when
+            ValueTask<Assignment> modifyAssignmentTask =
+                this.assignmentService.ModifyAssignmentAsync(invalidAssignment);
+
+            //then
+            await Assert.ThrowsAsync<AssignmentValidationException>(() =>
+                modifyAssignmentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedAssignmentValidationException))),
+                Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
