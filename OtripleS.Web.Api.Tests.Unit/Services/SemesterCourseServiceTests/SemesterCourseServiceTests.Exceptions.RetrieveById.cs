e6e1394bb63a3c011e2e2a659fc75ef9a62fcf20
tobidub <github@tobiasdubral.de>
2020-08-05 21:18:42 +0200

    ShouldThrowDependencyExceptionOnRetrieveWhenSqlExceptionOccursAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/SemesterCourseServiceTests/SemesterCourseServiceTests.Exceptions.RetrieveById.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/SemesterCourseServiceTests/SemesterCourseServiceTests.Exceptions.RetrieveById.cs
@@ -3,10 +3,55 @@
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
 
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.SemesterCourses;
+using OtripleS.Web.Api.Models.SemesterCourses.Exceptions;
+using Xunit;
+
 namespace OtripleS.Web.Api.Tests.Unit.Services.SemesterCourseServiceTests
 {
 	public partial class SemesterCourseServiceTests
     {
-     
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid randomSemesterCourseId = Guid.NewGuid();
+            Guid inputSemesterCourseId = randomSemesterCourseId;
+            var sqlException = GetSqlException();
+
+            var expectedSemesterCourseDependencyException =
+                new SemesterCourseDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSemesterCourseByIdAsync(inputSemesterCourseId))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<SemesterCourse> retrieveSemesterCourseByIdTask =
+                this.semesterCourseService.RetrieveSemesterCourseByIdAsync(inputSemesterCourseId);
+
+            // then
+            await Assert.ThrowsAsync<SemesterCourseDependencyException>(() =>
+                retrieveSemesterCourseByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedSemesterCourseDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSemesterCourseByIdAsync(inputSemesterCourseId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
--- OtripleS.Web.Api.Tests.Unit/Services/SemesterCourseServiceTests/SemesterCourseServiceTests.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/SemesterCourseServiceTests/SemesterCourseServiceTests.cs
@@ -5,6 +5,8 @@
 
 using System;
 using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using Microsoft.Data.SqlClient;
 using Moq;
 using OtripleS.Web.Api.Brokers.DateTimes;
 using OtripleS.Web.Api.Brokers.Loggings;
@@ -40,6 +42,8 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.SemesterCourseServiceTests
 				expectedException.Message == actualException.Message
 				&& expectedException.InnerException.Message == actualException.InnerException.Message;
 		}
+		private static SqlException GetSqlException() =>
+			(SqlException)FormatterServices.GetUninitializedObject(typeof(SqlException));
 
 		private static DateTimeOffset GetRandomDateTime() =>
 			new DateTimeRange(earliestDate: new DateTime()).GetValue();
