33fdff0eaa69bf16b1f6305dea3d4d5c081cbe76
tobidub <github@tobiasdubral.de>
2020-08-05 20:53:11 +0200

    ShouldRetrieveSemesterCourseByIdAsync  -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/SemesterCourseServiceTests/SemesterCourseServiceTests.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/SemesterCourseServiceTests/SemesterCourseServiceTests.cs
@@ -15,40 +15,39 @@ using Tynamix.ObjectFiller;
 namespace OtripleS.Web.Api.Tests.Unit.Services.SemesterCourseServiceTests
 {
 	public partial class SemesterCourseServiceTests
-    {
-        private readonly Mock<IStorageBroker> storageBrokerMock;
-        private readonly Mock<ILoggingBroker> loggingBrokerMock;
-        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
-        private readonly ISemesterCourseService semesterCourseService;
+	{
+		private readonly Mock<IStorageBroker> storageBrokerMock;
+		private readonly Mock<ILoggingBroker> loggingBrokerMock;
+		private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+		private readonly ISemesterCourseService semesterCourseService;
 
-        public SemesterCourseServiceTests()
-        {
-            this.storageBrokerMock = new Mock<IStorageBroker>();
-            this.loggingBrokerMock = new Mock<ILoggingBroker>();
-            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+		public SemesterCourseServiceTests()
+		{
+			this.storageBrokerMock = new Mock<IStorageBroker>();
+			this.loggingBrokerMock = new Mock<ILoggingBroker>();
+			this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
 
-            this.semesterCourseService = new SemesterCourseService(
-                storageBroker: this.storageBrokerMock.Object,
-                loggingBroker: this.loggingBrokerMock.Object,
-                dateTimeBroker: this.dateTimeBrokerMock.Object);
-        }
+			this.semesterCourseService = new SemesterCourseService(
+				storageBroker: this.storageBrokerMock.Object,
+				loggingBroker: this.loggingBrokerMock.Object,
+				dateTimeBroker: this.dateTimeBrokerMock.Object);
+		}
 
-        private static DateTimeOffset GetRandomDateTime() =>
-            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+		private static DateTimeOffset GetRandomDateTime() =>
+			new DateTimeRange(earliestDate: new DateTime()).GetValue();
 
-        private SemesterCourse CreateRandomSemesterCourse(DateTimeOffset dates) =>
-            CreateSemesterCourseFiller(dates).Create();
+		private SemesterCourse CreateRandomSemesterCourse(DateTimeOffset dates) =>
+			CreateSemesterCourseFiller(dates).Create();
 
-        private static Filler<SemesterCourse> CreateSemesterCourseFiller(DateTimeOffset dates)
-        {
-            var filler = new Filler<SemesterCourse>();
-            filler.Setup()
-                .OnProperty(semesterCourse => semesterCourse.StartDate).Use(GetRandomDateTime())
-                .OnProperty(semesterCourse => semesterCourse.EndDate).Use(GetRandomDateTime())
-                .OnProperty(semesterCourse => semesterCourse.CreatedDate).Use(dates)
-                .OnProperty(semesterCourse => semesterCourse.UpdatedDate).Use(dates);
+		private static Filler<SemesterCourse> CreateSemesterCourseFiller(DateTimeOffset dates)
+		{
+			var filler = new Filler<SemesterCourse>();
+			filler.Setup()
+				.OnType<DateTimeOffset>().Use(dates)
+				.OnProperty(semesterCourse => semesterCourse.CreatedDate).Use(dates)
+				.OnProperty(semesterCourse => semesterCourse.UpdatedDate).Use(dates);
 
-            return filler;
-        }
-    }
+			return filler;
+		}
+	}
 }
