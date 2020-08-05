627ce655572799ad3b2fdfa42724d38d878b8eb8
devmanzur <devmanzur@gmail.com>
2020-08-06 01:00:32 +0600

    ShouldThrowDependencyExceptionOnDeleteWhenDbUpdateConcurrencyExceptionOccursAndLogItAsync -> PASS

--- OtripleS.Web.Api/Services/SemesterCourses/SemesterCourseService.cs
+++ OtripleS.Web.Api/Services/SemesterCourses/SemesterCourseService.cs
@@ -52,10 +52,16 @@ namespace OtripleS.Web.Api.Services.SemesterCourses
             {
                 throw CreateAndLogCriticalDependencyException(sqlException);
             }
+            catch (DbUpdateConcurrencyException dbUpdateConcurrencyException)
+            {
+                var lockedSemesterCourseException = new LockedSemesterCourseException(dbUpdateConcurrencyException);
+                throw CreateAndLogDependencyException(lockedSemesterCourseException);
+            }
             catch (DbUpdateException dbUpdateException)
             {
                 throw CreateAndLogDependencyException(dbUpdateException);
             }
+            
         }
     }
 }
