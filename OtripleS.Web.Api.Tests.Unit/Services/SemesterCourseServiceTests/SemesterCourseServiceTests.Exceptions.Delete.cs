624144c2291ef69a55afae2f0aedff48aa86d789
devmanzur <devmanzur@gmail.com>
2020-08-06 00:53:35 +0600

    ShouldThrowDependencyExceptionOnDeleteWhenDbUpdateConcurrencyExceptionOccursAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/SemesterCourseServiceTests/SemesterCourseServiceTests.Exceptions.Delete.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/SemesterCourseServiceTests/SemesterCourseServiceTests.Exceptions.Delete.cs
@@ -1,3 +1,8 @@
+// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
 using System;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
@@ -72,6 +77,44 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.SemesterCourseServiceTests
 
             this.loggingBrokerMock.Verify(broker =>
                     broker.LogError(It.Is(SameExceptionAs(expectedSemesterCourseDependencyException))),
+                Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                    broker.SelectSemesterCourseByIdAsync(inputSemesterCourseId),
+                Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+        
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnDeleteWhenDbUpdateConcurrencyExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid randomSemesterCourseId = Guid.NewGuid();
+            Guid inputSemesterCourseId = randomSemesterCourseId;
+            var databaseUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedSemesterCourseException = new LockedSemesterCourseException(databaseUpdateConcurrencyException);
+
+            var expectedSemesterCourseException =
+                new SemesterCourseDependencyException(lockedSemesterCourseException);
+
+            this.storageBrokerMock.Setup(broker =>
+                    broker.SelectSemesterCourseByIdAsync(inputSemesterCourseId))
+                .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<SemesterCourse> deleteSemesterCourseTask =
+                this.semesterCourseService.DeleteSemesterCourseAsync(inputSemesterCourseId);
+
+            // then
+            await Assert.ThrowsAsync<SemesterCourseDependencyException>(() =>
+                deleteSemesterCourseTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                    broker.LogError(It.Is(SameExceptionAs(expectedSemesterCourseException))),
                 Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
