3bfc027893c90ab1ec1535d13d92cc20fd278394
devmanzur <devmanzur@gmail.com>
2020-08-06 03:33:44 +0600

    fixed ordering and removed empty lines

--- OtripleS.Web.Api/Services/SemesterCourses/SemesterCourseService.Exceptions.cs
+++ OtripleS.Web.Api/Services/SemesterCourses/SemesterCourseService.Exceptions.cs
@@ -9,40 +9,8 @@ namespace OtripleS.Web.Api.Services.SemesterCourses
 {
     public partial class SemesterCourseService
     {
-        private SemesterCourseValidationException CreateAndLogValidationException(Exception exception)
-        {
-            var semesterCourseValidationException = new SemesterCourseValidationException(exception);
-            this.loggingBroker.LogError(semesterCourseValidationException);
-
-            return semesterCourseValidationException;
-        }
-        
-        private SemesterCourseDependencyException CreateAndLogCriticalDependencyException(Exception exception)
-        {
-            var semesterCourseDependencyException = new SemesterCourseDependencyException(exception);
-            this.loggingBroker.LogCritical(semesterCourseDependencyException);
-
-            return semesterCourseDependencyException;
-        }
-        private SemesterCourseDependencyException CreateAndLogDependencyException(Exception exception)
-        {
-            var semesterCourseDependencyException = new SemesterCourseDependencyException(exception);
-            this.loggingBroker.LogError(semesterCourseDependencyException);
-
-            return semesterCourseDependencyException;
-        }
-        
-        private SemesterCourseServiceException CreateAndLogServiceException(Exception exception)
-        {
-            var semesterCourseServiceException = new SemesterCourseServiceException(exception);
-            this.loggingBroker.LogError(semesterCourseServiceException);
-
-            return semesterCourseServiceException;
-        }
-        
         private delegate ValueTask<SemesterCourse> ReturningSemesterCourseFunction();
 
-        
         private async ValueTask<SemesterCourse> TryCatch(ReturningSemesterCourseFunction returningStudentFunction)
         {
             try
@@ -74,6 +42,35 @@ namespace OtripleS.Web.Api.Services.SemesterCourses
             {
                 throw CreateAndLogServiceException(exception);
             }
+        }
+
+        private SemesterCourseValidationException CreateAndLogValidationException(Exception exception)
+        {
+            var semesterCourseValidationException = new SemesterCourseValidationException(exception);
+            this.loggingBroker.LogError(semesterCourseValidationException);
+            return semesterCourseValidationException;
+        }
+
+        private SemesterCourseDependencyException CreateAndLogCriticalDependencyException(Exception exception)
+        {
+            var semesterCourseDependencyException = new SemesterCourseDependencyException(exception);
+            this.loggingBroker.LogCritical(semesterCourseDependencyException);
+            return semesterCourseDependencyException;
+        }
+
+        private SemesterCourseDependencyException CreateAndLogDependencyException(Exception exception)
+        {
+            var semesterCourseDependencyException = new SemesterCourseDependencyException(exception);
+            this.loggingBroker.LogError(semesterCourseDependencyException);
+            return semesterCourseDependencyException;
+        }
+
+        private SemesterCourseServiceException CreateAndLogServiceException(Exception exception)
+        {
+            var semesterCourseServiceException = new SemesterCourseServiceException(exception);
+            this.loggingBroker.LogError(semesterCourseServiceException);
+
+            return semesterCourseServiceException;
         }
     }
 }
