2ffdfcb9b7d39cdf5d81408aafeb580c68f7a84f
devmanzur <devmanzur@gmail.com>
2020-08-13 00:59:21 +0600

    ShouldLogWarningOnRetrieveAllWhenStudentSemesterCoursesWereEmptyAndLogIt -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentSemesterCourseServiceTests/StudentSemesterCourseServiceTests.Validations.Retrieve.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.StudentSemesterCourses;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.StudentSemesterCourseServiceTests
+{
+    public partial class StudentSemesterCourseServiceTests
+    {
+        [Fact]
+        public void ShouldLogWarningOnRetrieveAllWhenStudentSemesterCoursesWereEmptyAndLogIt()
+        {
+            // given
+            IQueryable<StudentSemesterCourse> emptyStorageStudentSemesterCourses =
+                new List<StudentSemesterCourse>().AsQueryable();
+            IQueryable<StudentSemesterCourse> expectedStudentSemesterCourses = emptyStorageStudentSemesterCourses;
+
+            string expectedMessage = "No studentSemesterSemesterCourses found in storage.";
+            
+            this.storageBrokerMock.Setup(broker =>
+                    broker.SelectAllStudentSemesterCourses())
+                .Returns(expectedStudentSemesterCourses);
+
+            // when
+            IQueryable<StudentSemesterCourse> actualSemesterCourses =
+                this.studentSemesterCourseService.RetrieveAllStudentSemesterCourses();
+
+            // then
+            actualSemesterCourses.Should().BeEquivalentTo(emptyStorageStudentSemesterCourses);
+
+            this.loggingBrokerMock.Verify(broker =>
+                    broker.LogWarning(expectedMessage),
+                Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                    broker.SelectAllSemesterCourses(),
+                Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
