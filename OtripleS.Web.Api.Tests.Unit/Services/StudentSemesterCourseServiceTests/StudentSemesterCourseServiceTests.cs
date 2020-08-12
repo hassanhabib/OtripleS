a454fea59c702528f41a5e048a2e797eef6f0099
AgentEnder <craigorycoppola@gmail.com>
2020-08-12 07:09:25 -0500

    StudentSemesterCourseService File Structure

---
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentSemesterCourseServiceTests/StudentSemesterCourseServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.StudentSemesterCourseServiceTests
+{
+    public partial class StudentSemesterCourseServiceTests
+    {
+    }
+}
---
+++ OtripleS.Web.Api/Services/StudentSemesterCourses/StudentSemesterCourseService.cs
@@ -0,0 +1,16 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtripleS.Web.Api.Services.StudentSemesterCourses
+{
+    public partial class StudentSemesterCourseService: IStudentSemesterCourseService
+    {
+    }
+}
