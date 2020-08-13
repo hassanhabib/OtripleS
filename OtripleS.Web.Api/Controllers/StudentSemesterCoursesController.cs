465abdca670c87b1e71ab3dc6cbf2657952752ad
eris.adhami <eri.adhami@gmail.com>
2020-08-13 21:25:02 +0200

    StudentSemesterCourse Controller Delete Endpoint

--- OtripleS.Web.Api/Controllers/StudentSemesterCoursesController.cs
+++ OtripleS.Web.Api/Controllers/StudentSemesterCoursesController.cs
@@ -56,6 +56,45 @@ namespace OtripleS.Web.Api.Controllers
             }
         }
 
+        [HttpDelete("{studentId}/{semesterCourseId}")]
+        public async ValueTask<ActionResult<StudentSemesterCourse>>
+        DeleteStudentSemesterCourseAsync(Guid semesterCourseId, Guid studentId)
+        {
+            try
+            {
+                StudentSemesterCourse storageStudentSemesterCourse =
+                    await this.studentSemesterCourseService.DeleteStudentSemesterCourseAsync(semesterCourseId, studentId);
+
+                return Ok(storageStudentSemesterCourse);
+            }
+            catch (StudentSemesterCourseValidationException studentSemesterCourseValidationException)
+                when (studentSemesterCourseValidationException.InnerException is NotFoundStudentSemesterCourseException)
+            {
+                string innerMessage = GetInnerMessage(studentSemesterCourseValidationException);
+
+                return NotFound(innerMessage);
+            }
+            catch (StudentSemesterCourseValidationException studentSemesterCourseValidationException)
+            {
+                return BadRequest(studentSemesterCourseValidationException.Message);
+            }
+            catch (StudentSemesterCourseDependencyException studentSemesterCourseDependencyException)
+                when (studentSemesterCourseDependencyException.InnerException is LockedStudentSemesterCourseException)
+            {
+                string innerMessage = GetInnerMessage(studentSemesterCourseDependencyException);
+
+                return Locked(innerMessage);
+            }
+            catch (StudentSemesterCourseDependencyException studentSemesterCourseDependencyException)
+            {
+                return Problem(studentSemesterCourseDependencyException.Message);
+            }
+            catch (StudentSemesterCourseServiceException studentSemesterCourseServiceException)
+            {
+                return Problem(studentSemesterCourseServiceException.Message);
+            }
+        }
+
         private static string GetInnerMessage(Exception exception) =>
             exception.InnerException.Message;
     }
