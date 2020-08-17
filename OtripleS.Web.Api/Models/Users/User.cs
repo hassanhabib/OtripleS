cd1dc221e44bc399a32f707c6db0989febde8fe1
eris.adhami <eri.adhami@gmail.com>
2020-08-18 00:00:46 +0200

    UserModel SetUp and Migrations

---
+++ OtripleS.Web.Api/Models/Users/User.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace OtripleS.Web.Api.Models.Users
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; }
+        public string FamilyName { get; set; }
+        public UserStatus Status { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}
