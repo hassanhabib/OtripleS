5c2c4dcf5776e586cbf23d03df2e2107672c042c
eris.adhami <eri.adhami@gmail.com>
2020-08-18 12:30:02 +0200

    User Broker Select All function

--- OtripleS.Web.Api/Brokers/Storage/StorageBroker.Users.cs
+++ OtripleS.Web.Api/Brokers/Storage/StorageBroker.Users.cs
@@ -4,6 +4,7 @@
 // ---------------------------------------------------------------
 
 using System;
+using System.Linq;
 using Microsoft.AspNetCore.Identity;
 using OtripleS.Web.Api.Models.Users;
 
@@ -11,9 +12,11 @@ namespace OtripleS.Web.Api.Brokers.Storage
 {
     public partial class StorageBroker
     {
+        public IQueryable<User> SelectAllUsers() => this.userManager.Users;
+
         public async System.Threading.Tasks.ValueTask<User> InsertUserAsync(User user, string password)
         {
-            await userManager.CreateAsync(user, password);
+            await this.userManager.CreateAsync(user, password);
             await this.SaveChangesAsync();
 
             return user;
