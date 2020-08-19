bf859dd462aa37b88ff381d5114c5b240823fa3d
eris.adhami <eri.adhami@gmail.com>
2020-08-19 17:42:58 +0200

    Rebased from StudentSemesterCourseControllerGetByIdEndpoint

---
+++ OtripleS.Web.Api/Models/Users/Role.cs
@@ -0,0 +1,12 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace OtripleS.Web.Api.Models.Users
+{
+    public class Role : IdentityRole<Guid> { }
+}
