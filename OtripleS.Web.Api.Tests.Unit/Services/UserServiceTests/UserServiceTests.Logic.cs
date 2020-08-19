2b5035ceae4c72eddc1c1a4d5d804fabf5e157a5
eris.adhami <eri.adhami@gmail.com>
2020-08-19 20:22:48 +0200

    ShouldRegisterUserAsync => FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/UserServiceTests/UserServiceTests.Logic.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.Users;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.UserServiceTests
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldRegisterUserAsync()
+        {
+            // given
+            User randomUser = CreateRandomUser();
+            User inputUser = randomUser;
+            User storageUser = randomUser;
+            User expectedUser = storageUser;
+            string password = GetRandomPassword();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertUserAsync(inputUser, password))
+                    .ReturnsAsync(storageUser);
+
+            // when
+            User actualUser =
+                await this.userService.RegisterUserAsync(inputUser, password);
+
+            // then
+            actualUser.Should().BeEquivalentTo(expectedUser);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserAsync(inputUser, password),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ OtripleS.Web.Api.Tests.Unit/Services/UserServiceTests/UserServiceTests.cs
@@ -0,0 +1,67 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Moq;
+using OtripleS.Web.Api.Brokers.DateTimes;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.Users;
+using OtripleS.Web.Api.Services.Users;
+using Tynamix.ObjectFiller;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.UserServiceTests
+{
+    public partial class UserServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly IUserService userService;
+
+        public UserServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+
+            this.userService = new UserService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object);
+        }
+
+        //private User CreateRandomUser() =>
+        //    CreateUserFiller(dates: DateTimeOffset.UtcNow).Create();
+
+        //private static Filler<User> CreateUserFiller(DateTimeOffset dates)
+        //{
+        //    var filler = new Filler<User>();
+
+        //    filler.Setup()
+        //        .OnType<DateTimeOffset>().Use(dates);
+
+        //    return filler;
+        //}
+
+        private User CreateRandomUser()
+        {
+            var user = new User
+            {
+                Id = new Guid(),
+                UserName = new Filler<EmailAddresses>().Create().ToString(),
+                Name = new Filler<RealNames>().Create().ToString(),
+                FamilyName = new Filler<RealNames>().Create().ToString(),
+                CreatedDate = DateTimeOffset.UtcNow,
+                UpdatedDate = DateTimeOffset.UtcNow
+
+            };
+
+            return user;
+        }
+
+        private static string GetRandomPassword() => new MnemonicString(1, 8, 20).GetValue();
+    }
+}
