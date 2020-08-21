ef405b6f286a39b4d3f2b5fd419b0182abc85c25
eris.adhami <eri.adhami@gmail.com>
2020-08-21 22:00:11 +0200

    ShouldThrowDependencyExceptionOnModifyIfSqlExceptionOccursAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/UserServiceTests/UserServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Moq;
+using OtripleS.Web.Api.Models.Users;
+using OtripleS.Web.Api.Models.Users.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.UserServiceTests
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            int randomNegativeNumber = GetNegativeRandomNumber();
+            DateTimeOffset randomDateTime = GetRandomDateTime();
+            User randomUser = CreateRandomUser(dates: randomDateTime);
+            User someUser = randomUser;
+            someUser.CreatedDate = randomDateTime.AddMinutes(randomNegativeNumber);
+            SqlException sqlException = GetSqlException();
+
+            var expectedUserDependencyException =
+                new UserDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserByIdAsync(someUser.Id))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<User> modifyUserTask =
+                this.userService.ModifyUserAsync(someUser);
+
+            // then
+            await Assert.ThrowsAsync<UserDependencyException>(() =>
+                modifyUserTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(someUser.Id),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedUserDependencyException))),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- OtripleS.Web.Api/Services/Users/UserService.Exceptions.cs
+++ OtripleS.Web.Api/Services/Users/UserService.Exceptions.cs
@@ -44,10 +44,6 @@ namespace OtripleS.Web.Api.Services.Users
 
                 throw CreateAndLogValidationException(alreadyExistsUserException);
             }
-            catch (SqlException sqlException)
-            {
-                throw CreateAndLogCriticalDependencyException(sqlException);
-            }
             catch (DbUpdateConcurrencyException dbUpdateConcurrencyException)
             {
                 var lockedUserException = new LockedUserException(dbUpdateConcurrencyException);
