efbea4f39743164a8f0024f0d3cdfcccd666af0f
eris.adhami <eri.adhami@gmail.com>
2020-08-21 17:31:45 +0200

    ShouldThrowValidationExceptionOnModifyWhenUserIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/UserServiceTests/UserServiceTests.Validations.Modify.cs
@@ -0,0 +1,49 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.Users;
+using OtripleS.Web.Api.Models.Users.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.UserServiceTests
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnModifyWhenUserIsNullAndLogItAsync()
+        {
+            // given
+            User randomUser = null;
+            User nullUser = randomUser;
+            var nullUserException = new NullUserException();
+
+            var expectedUserValidationException =
+                new UserValidationException(nullUserException);
+
+            // when
+            ValueTask<User> modifyUserTask =
+                this.userService.ModifyUserAsync(nullUser);
+
+            // then
+            await Assert.ThrowsAsync<UserValidationException>(() =>
+                modifyUserTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedUserValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
