d73cfd657c7b241a65579f132c58f2ee506cb185
Viral Pandya <virav.pandya@gmail.com>
2020-08-24 19:01:44 -0700

    ShouldThrowValidationExceptionOnRetrieveByIdWhenIdIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/AttendanceServiceTests/AttendanceServiceTests.Retrieve.cs
@@ -0,0 +1,54 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.Attendances;
+using OtripleS.Web.Api.Models.Attendances.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.AttendanceServiceTests
+{
+    public partial class AttendanceServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdWhenIdIsNullAndLogItAsync()
+        {
+            // given
+            DateTimeOffset dateTimeOffset = GetRandomDateTime();
+            Guid randomAttendanceId = default;
+            Guid inputAttendanceId = randomAttendanceId;
+
+            var invalidAttendanceInputException = new InvalidAttendanceInputException(
+                parameterName: nameof(Attendance.Id),
+                parameterValue: inputAttendanceId);
+
+            var expectedValidationException = 
+                new AttendanceValidationException(invalidAttendanceInputException);
+
+            // when
+            ValueTask<Attendance> retrieveAttendanceTask =
+                this.attendanceService.RetrieveAttendanceByIdAsync(inputAttendanceId);
+
+            // then
+            await Assert.ThrowsAsync<AttendanceValidationException>(() => 
+                retrieveAttendanceTask.AsTask());
+            
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAttendanceByIdAsync(inputAttendanceId),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ OtripleS.Web.Api/Models/Attendances/Exceptions/AttendanceValidationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+
+namespace OtripleS.Web.Api.Models.Attendances.Exceptions
+{
+    public class AttendanceValidationException : Exception
+    {
+        public AttendanceValidationException(Exception innerException)
+            : base("Invalid input, contact support.", innerException) { }
+    }
+}
