4ce4bb974a169b905c31e3be71d0d1fec20b4f2c
Viral Pandya <virav.pandya@gmail.com>
2020-08-24 18:47:43 -0700

    ShouldRetrieveAttendanceByIdAsync -> FAIL

---
+++ OtripleS.Web.Api/Services/Attendances/AttendanceService.cs
@@ -0,0 +1,36 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Brokers.DateTimes;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.Attendances;
+
+namespace OtripleS.Web.Api.Services.Attendances
+{
+    public class AttendanceService : IAttendanceService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+
+        public AttendanceService(
+            IStorageBroker storageBroker, 
+            ILoggingBroker loggingBroker, 
+            IDateTimeBroker dateTimeBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+            this.dateTimeBroker = dateTimeBroker;
+        }
+
+        public ValueTask<Attendance> RetrieveAttendanceByIdAsync(Guid attendanceId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OtripleS.Web.Api/Services/Attendances/IAttendanceService.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Models.Attendances;
+
+namespace OtripleS.Web.Api.Services.Attendances
+{
+    public interface IAttendanceService
+    {
+        ValueTask<Attendance> RetrieveAttendanceByIdAsync(Guid attendanceId);
+    }
+}
