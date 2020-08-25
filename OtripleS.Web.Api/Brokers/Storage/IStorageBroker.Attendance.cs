fa9af1667c8a464669114d438596a94bbd3f3527
Hatem Gamal Abbas <hatooma_90@hotmail.com>
2020-08-25 09:21:50 +0300

    Attendance Broker

---
+++ OtripleS.Web.Api/Brokers/Storage/IStorageBroker.Attendance.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using OtripleS.Web.Api.Models.Attendances;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtripleS.Web.Api.Brokers.Storage
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Attendance> InsertAttendanceAsync(Attendance Classroom);
+        IQueryable<Attendance> SelectAllAttendances();
+        ValueTask<Attendance> SelectAttendanceByIdAsync(Guid ClassroomId);
+        ValueTask<Attendance> UpdateAttendanceAsync(Attendance Classroom);
+        ValueTask<Attendance> DeleteAttendanceAsync(Attendance Classroom);
+    }
+}
