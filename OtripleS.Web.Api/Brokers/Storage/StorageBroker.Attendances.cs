09de5fbbedd5fc4477d1e0beb9062ec9462ebc33
Hatem Gamal Abbas <hatooma_90@hotmail.com>
2020-08-25 08:54:33 +0300

    Attendance Model Setup & Migrations

---
+++ OtripleS.Web.Api/Brokers/Storage/StorageBroker.Attendances.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+
+namespace OtripleS.Web.Api.Brokers.Storage
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Attendance> Attendances { get; set; }
+    }
+}
