4b81ed0bfd66f36d58e7053e8d2d16e72ad2f614
tobidub <github@tobiasdubral.de>
2020-08-26 21:24:20 +0200

    ShouldThrowValidationExceptionOnModifyWhenAttendanceIdIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/AttendanceServiceTests/AttendanceServiceTests.Validation.Modify.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/AttendanceServiceTests/AttendanceServiceTests.Validation.Modify.cs
@@ -3,6 +3,7 @@
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using Moq;
 using OtripleS.Web.Api.Models.Attendances;
@@ -22,6 +23,40 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.AttendanceServiceTests
 
             var expectedAttendanceValidationException =
                 new AttendanceValidationException(nullAttendanceException);
+
+            //when
+            ValueTask<Attendance> modifyAttendanceTask =
+                this.attendanceService.ModifyAttendanceAsync(invalidAttendance);
+
+            //then
+            await Assert.ThrowsAsync<AttendanceValidationException>(() =>
+                modifyAttendanceTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedAttendanceValidationException))),
+                Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyWhenAttendanceIdIsInvalidAndLogItAsync()
+        {
+            //given
+            Guid invalidAttendanceId = Guid.Empty;
+            DateTimeOffset dateTime = GetRandomDateTime();
+            Attendance randomAttendance = CreateRandomAttendance(dateTime);
+            Attendance invalidAttendance = randomAttendance;
+            invalidAttendance.Id = invalidAttendanceId;
+
+            var invalidAttendanceException = new InvalidAttendanceInputException(
+                parameterName: nameof(Attendance.Id),
+                parameterValue: invalidAttendance.Id);
+
+            var expectedAttendanceValidationException =
+                new AttendanceValidationException(invalidAttendanceException);
 
             //when
             ValueTask<Attendance> modifyAttendanceTask =
