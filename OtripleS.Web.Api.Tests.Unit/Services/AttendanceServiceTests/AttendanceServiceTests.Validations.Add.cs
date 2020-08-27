d7e09d5d2643158899f997df5b12f34cfac09469
Viral Pandya <virav.pandya@gmail.com>
2020-08-26 19:44:32 -0700

    ShouldThrowValidationExceptionOnCreateWhenAttendanceIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/AttendanceServiceTests/AttendanceServiceTests.Validations.Add.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.Attendances;
+using OtripleS.Web.Api.Models.Attendances.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.AttendanceServiceTests
+{
+    public partial class AttendanceServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnCreateWhenAttendanceIsNullAndLogItAsync()
+        {
+            // given
+            Attendance randomAttendance = null;
+            Attendance nullAttendance = randomAttendance;
+            var nullAttendanceException = new NullAttendanceException();
+
+            var expectedAttendanceValidationException =
+                new AttendanceValidationException(nullAttendanceException);
+
+            // when
+            ValueTask<Attendance> createAttendanceTask =
+                this.attendanceService.CreateAttendanceAsync(nullAttendance);
+
+            // then
+            await Assert.ThrowsAsync<AttendanceValidationException>(() =>
+                createAttendanceTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedAttendanceValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAttendanceByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
