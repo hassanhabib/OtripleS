d71a28a661d41c689d66c68288501b035a4a1f8d
JaAfghan <31682719+javedolfath@users.noreply.github.com>
2020-09-07 13:25:05 +0430

    Copy Right Notice added to Guardian Broker and Model

--- OtripleS.Web.Api/Brokers/Storage/IStorageBroker.Guardians.cs
+++ OtripleS.Web.Api/Brokers/Storage/IStorageBroker.Guardians.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
--- OtripleS.Web.Api/Models/Guardian/Exceptions/GuardianServiceException.cs
+++ OtripleS.Web.Api/Models/Guardian/Exceptions/GuardianServiceException.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
