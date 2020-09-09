f39b94def48583b4182991ee7deae02919eb21cd
Viral Pandya <virav.pandya@gmail.com>
2020-09-09 16:22:36 -0700

    ShouldThrowValidationExceptionOnAddWhenGuardianIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/GuardianServiceTests/GuardianServiceTests.Validations.Add.cs
@@ -0,0 +1,49 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.Guardian;
+using Xunit;
+using OtripleS.Web.Api.Models.Guardian.Exceptions;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.GuardianServiceTests
+{
+    public partial class GuardianServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddWhenGuardianIsNullAndLogItAsync()
+        {
+            // given
+            Guardian randomGuardian = default;
+            Guardian nullGuardian = randomGuardian;
+            var nullGuardianException = new NullGuardianException();
+
+            var expectedGuardianValidationException =
+                new GuardianValidationException(nullGuardianException);
+
+            // when
+            ValueTask<Guardian> registerGuardianTask =
+                this.guardianService.CreateGuardianAsync(nullGuardian);
+
+            // then
+            await Assert.ThrowsAsync<GuardianValidationException>(() =>
+                registerGuardianTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedGuardianValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuardianByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
