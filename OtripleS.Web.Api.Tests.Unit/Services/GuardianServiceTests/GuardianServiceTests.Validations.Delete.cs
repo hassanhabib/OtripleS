3e444d3cad1e160367f5ccac0b7f01b2b3151fc2
Viral Pandya <virav.pandya@gmail.com>
2020-09-09 18:03:04 -0700

    ShouldThrowValidationExceptionOnDeleteWhenIdIsInvalidAndLogItAsync -> PASS

---
+++ OtripleS.Web.Api.Tests.Unit/Services/GuardianServiceTests/GuardianServiceTests.Validations.Delete.cs
@@ -0,0 +1,58 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.Guardian;
+using Xunit;
+using OtripleS.Web.Api.Models.Guardian.Exceptions;
+using EFxceptions.Models.Exceptions;
+using OtripleS.Web.Api.Models.Guardians.Exceptions;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.GuardianServiceTests
+{
+    public partial class GuardianServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnDeleteWhenIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid randomGuardianId = default;
+            Guid inputGuardianId = randomGuardianId;
+
+            var invalidGuardianInputException = new InvalidGuardianException(
+                parameterName: nameof(Guardian.Id),
+                parameterValue: inputGuardianId);
+
+            var expectedGuardianValidationException =
+                new GuardianValidationException(invalidGuardianInputException);
+
+            // when
+            ValueTask<Guardian> deleteGuardianTask =
+                this.guardianService.DeleteGuardianAsync(inputGuardianId);
+
+            // then
+            await Assert.ThrowsAsync<GuardianValidationException>(() =>
+                deleteGuardianTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedGuardianValidationException))),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuardianByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
