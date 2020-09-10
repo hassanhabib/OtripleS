3cf1a9e9d16d541d95a81d3310b8a66baba01a9c
Viral Pandya <virav.pandya@gmail.com>
2020-09-09 18:16:37 -0700

    ShouldThrowDependencyExceptionOnDeleteWhenDbUpdateConcurrencyExceptionOccursAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/GuardianServiceTests/GuardianServiceTests.Exceptions.Delete.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/GuardianServiceTests/GuardianServiceTests.Exceptions.Delete.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Moq;
 using OtripleS.Web.Api.Models.Guardian;
 using OtripleS.Web.Api.Models.Guardian.Exceptions;
+using OtripleS.Web.Api.Models.Guardians.Exceptions;
 using Xunit;
 
 namespace OtripleS.Web.Api.Tests.Unit.Services.GuardianServiceTests
@@ -66,6 +67,44 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.GuardianServiceTests
             this.storageBrokerMock.Setup(broker =>
                 broker.SelectGuardianByIdAsync(inputGuardianId))
                     .ThrowsAsync(databaseUpdateException);
+
+            // when
+            ValueTask<Guardian> deleteGuardianTask =
+                this.guardianService.DeleteGuardianAsync(inputGuardianId);
+
+            // then
+            await Assert.ThrowsAsync<GuardianDependencyException>(() =>
+                deleteGuardianTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedGuardianDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuardianByIdAsync(inputGuardianId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnDeleteWhenDbUpdateConcurrencyExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid randomGuardianId = Guid.NewGuid();
+            Guid inputGuardianId = randomGuardianId;
+            var databaseUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedGuardianException = new LockedGuardianException(databaseUpdateConcurrencyException);
+
+            var expectedGuardianDependencyException =
+                new GuardianDependencyException(lockedGuardianException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGuardianByIdAsync(inputGuardianId))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
 
             // when
             ValueTask<Guardian> deleteGuardianTask =
