0ac9b85fc0fa8acb6ddf7b4cd03a55231a3ca0b1
Viral Pandya <virav.pandya@gmail.com>
2020-09-09 18:13:42 -0700

    ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync -> PASS

---
+++ OtripleS.Web.Api.Tests.Unit/Services/GuardianServiceTests/GuardianServiceTests.Exceptions.Delete.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using OtripleS.Web.Api.Models.Guardian;
+using OtripleS.Web.Api.Models.Guardian.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.GuardianServiceTests
+{
+    public partial class GuardianServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnDeleteWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid randomGuardianId = Guid.NewGuid();
+            Guid inputGuardianId = randomGuardianId;
+            SqlException sqlException = GetSqlException();
+
+            var expectedGuardianDependencyException =
+                new GuardianDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGuardianByIdAsync(inputGuardianId))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Guardian> deleteGuardianTask =
+                this.guardianService.DeleteGuardianAsync(inputGuardianId);
+
+            // then
+            await Assert.ThrowsAsync<GuardianDependencyException>(() =>
+                deleteGuardianTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedGuardianDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuardianByIdAsync(inputGuardianId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
