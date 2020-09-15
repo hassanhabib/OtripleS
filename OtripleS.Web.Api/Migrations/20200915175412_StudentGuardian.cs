a4148393295f5e8c8b68a1367572ac76a9626ed9
Hassan Habib <hassanhabib@live.com>
2020-09-15 10:54:52 -0700

    - Run Migrations

---
+++ OtripleS.Web.Api/Migrations/20200915175412_StudentGuardian.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OtripleS.Web.Api.Migrations
+{
+    public partial class StudentGuardian : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentGuardians",
+                columns: table => new
+                {
+                    GuardianId = table.Column<Guid>(nullable: false),
+                    StudentId = table.Column<Guid>(nullable: false),
+                    Relationship = table.Column<int>(nullable: false),
+                    IsPrimaryContact = table.Column<bool>(nullable: false),
+                    Notes = table.Column<string>(nullable: true),
+                    CreatedDate = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedDate = table.Column<DateTimeOffset>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: false),
+                    UpdatedBy = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentGuardians", x => new { x.StudentId, x.GuardianId });
+                    table.ForeignKey(
+                        name: "FK_StudentGuardians_Guardians_GuardianId",
+                        column: x => x.GuardianId,
+                        principalTable: "Guardians",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_StudentGuardians_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentGuardians_GuardianId",
+                table: "StudentGuardians",
+                column: "GuardianId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentGuardians");
+        }
+    }
+}
