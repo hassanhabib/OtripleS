13dac81faccfffe5808ec619cf3d85c2ed554a26
eris.adhami <eri.adhami@gmail.com>
2020-09-16 12:23:58 +0200

    ShouldThrowServiceExceptionOnRetrieveAllWhenExceptionOccursAndLogIt -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/StudentGuardianServiceTests/StudentGuardianServiceTests.Exceptions.RetrieveAll.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentGuardianServiceTests/StudentGuardianServiceTests.Exceptions.RetrieveAll.cs
@@ -81,5 +81,39 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.StudentGuardianServiceTests
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public void ShouldThrowServiceExceptionOnRetrieveAllWhenExceptionOccursAndLogIt()
+        {
+            // given
+            var exception = new Exception();
+
+            var expectedStudentGuardianServiceException =
+                new StudentGuardianServiceException(exception);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllStudentGuardians())
+                    .Throws(exception);
+
+            // when . then
+            Assert.Throws<StudentGuardianServiceException>(() =>
+                this.studentGuardianService.RetrieveAllStudentGuardians());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentGuardianServiceException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllStudentGuardians(),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
