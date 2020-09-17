6f156e573bf8c601a47450dbf10ca7c8fc221557
Viral Pandya <virav.pandya@gmail.com>
2020-09-16 18:34:10 -0700

    ShouldAddStudentStudentGuardianAsync ->PASS

--- OtripleS.Web.Api.Tests.Unit/Services/StudentGuardianServiceTests/StudentGuardianServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentGuardianServiceTests/StudentGuardianServiceTests.Logic.cs
@@ -128,7 +128,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.StudentGuardianServiceTests
 
 			this.dateTimeBrokerMock.Verify(broker =>
 				broker.GetCurrentDateTime(),
-					Times.Never);
+					Times.Once);
 
 			this.storageBrokerMock.Verify(broker =>
 				broker.InsertStudentGuardianAsync(inputStudentGuardian),
