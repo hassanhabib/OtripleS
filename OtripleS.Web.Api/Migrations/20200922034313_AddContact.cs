ff5533ecb71d3a98cbccca717d0345c021b17696
Viral Pandya <virav.pandya@gmail.com>
2020-09-21 21:09:39 -0700

    Migration changes added

---
+++ OtripleS.Web.Api/Migrations/20200922034313_AddContact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OtripleS.Web.Api.Migrations
+{
+    public partial class AddContact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    IsPrimary = table.Column<bool>(nullable: false),
+                    Type = table.Column<int>(nullable: false),
+                    Information = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true),
+                    CreatedDate = table.Column<DateTimeOffset>(nullable: false),
+                    UpdatedDate = table.Column<DateTimeOffset>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: false),
+                    UpdatedBy = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
