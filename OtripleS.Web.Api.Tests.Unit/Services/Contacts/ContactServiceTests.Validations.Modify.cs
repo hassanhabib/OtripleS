05966822c1cfe78cf6fab2655c2b8f093dfc00a3
tobidub <github@tobiasdubral.de>
2020-09-23 22:15:29 +0200

    ShouldThrowValidationExceptionOnModifyWhenContactIdIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/Contacts/ContactServiceTests.Validations.Modify.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Contacts/ContactServiceTests.Validations.Modify.cs
@@ -3,6 +3,7 @@
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using Moq;
 using OtripleS.Web.Api.Models.Contacts;
@@ -39,5 +40,39 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Contacts
 			this.storageBrokerMock.VerifyNoOtherCalls();
 			this.dateTimeBrokerMock.VerifyNoOtherCalls();
 		}
-	}
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyWhenContactIdIsInvalidAndLogItAsync()
+        {
+            //given
+            Guid invalidContactId = Guid.Empty;
+            DateTimeOffset dateTime = GetRandomDateTime();
+            Contact randomContact = CreateRandomContact(dateTime);
+            Contact invalidContact = randomContact;
+            invalidContact.Id = invalidContactId;
+
+            var invalidContactException = new InvalidContactException(
+                parameterName: nameof(Contact.Id),
+                parameterValue: invalidContact.Id);
+
+            var expectedContactValidationException =
+                new ContactValidationException(invalidContactException);
+
+            //when
+            ValueTask<Contact> modifyContactTask =
+                this.contactService.ModifyContactAsync(invalidContact);
+
+            //then
+            await Assert.ThrowsAsync<ContactValidationException>(() =>
+                modifyContactTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedContactValidationException))),
+                Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
 }
