45b4bff4bca7447465abbe50598fc9b0a62b60e2
tobidub <github@tobiasdubral.de>
2020-09-23 22:10:28 +0200

    ShouldThrowValidationExceptionOnModifyWhenContactIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Contacts/ContactServiceTests.Validations.Modify.cs
@@ -0,0 +1,43 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.Contacts;
+using OtripleS.Web.Api.Models.Contacts.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Contacts
+{
+	public partial class ContactServiceTests
+    {
+		[Fact]
+		public async Task ShouldThrowValidationExceptionOnModifyWhenContactIsNullAndLogItAsync()
+		{
+			//given
+			Contact invalidContact = null;
+			var nullContactException = new NullContactException();
+
+			var expectedContactValidationException =
+				new ContactValidationException(nullContactException);
+
+			//when
+			ValueTask<Contact> modifyContactTask =
+				this.contactService.ModifyContactAsync(invalidContact);
+
+			//then
+			await Assert.ThrowsAsync<ContactValidationException>(() =>
+				modifyContactTask.AsTask());
+
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogError(It.Is(SameExceptionAs(expectedContactValidationException))),
+				Times.Once);
+
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.dateTimeBrokerMock.VerifyNoOtherCalls();
+		}
+	}
+}
