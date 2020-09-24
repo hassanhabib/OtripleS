bf99d84c5ea94bce5842d81162e1d6f6682f6c78
Ghiasuddin Tajwal <tajwhal.g2008@gmail.com>
2020-09-24 10:14:52 +0430

    ContactsController, PostContactAsync.

---
+++ OtripleS.Web.Api/Controllers/ContactsController.cs
@@ -0,0 +1,64 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OtripleS.Web.Api.Models.Contacts;
+using OtripleS.Web.Api.Models.Contacts.Exceptions;
+using OtripleS.Web.Api.Services.Contacts;
+using RESTFulSense.Controllers;
+
+namespace OtripleS.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : RESTFulController
+    {
+        private readonly IContactService contactService;
+
+        public ContactsController(IContactService contactService) => this.contactService = contactService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Contact>> PostContactAsync(Contact contact)
+        {
+            try
+            {
+                Contact persistedContact = await contactService.AddContactAsync(contact);
+
+                return Ok(persistedContact);
+            }
+            catch (ContactValidationException contactValidationException)
+                when (contactValidationException.InnerException is AlreadyExistsContactException)
+            {
+                string innerMessage = GetInnerMessage(contactValidationException);
+
+                return Conflict(innerMessage);
+            }
+            catch (ContactValidationException contactValidationException)
+            {
+                string innerMessage = GetInnerMessage(contactValidationException);
+
+                return BadRequest(innerMessage);
+            }
+            catch (ContactDependencyException contactDependencyException)
+            {
+                string innerMessage = GetInnerMessage(contactDependencyException);
+
+                return Problem(innerMessage);
+            }
+            catch(ContactServiceException contactServiceException)
+            {
+                string innerMessage = GetInnerMessage(contactServiceException);
+
+                return Problem(innerMessage);
+            }
+        }
+
+        private static string GetInnerMessage(Exception exception) =>
+            exception.InnerException.Message;
+    }
+}
