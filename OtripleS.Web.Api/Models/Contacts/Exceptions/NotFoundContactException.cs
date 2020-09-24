eaa756b09019e7dcdc52e0a8967c0159aebfaf15
AgentEnder <craigorycoppola@gmail.com>
2020-09-23 21:30:35 -0500

    ShouldThrowValidationExceptionOnRetrieveWhenStorageContactIsNullAndLogItAsync --> FAIL
    ShouldThrowValidationExceptionOnRetrieveWhenIdIsInvalidAndLogItAsync --> FAIL

---
+++ OtripleS.Web.Api/Models/Contacts/Exceptions/NotFoundContactException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtripleS.Web.Api.Models.Contacts.Exceptions
+{
+    public class NotFoundContactException: Exception
+    {
+        public NotFoundContactException(Guid contactId)
+            : base($"Couldn't find contact with Id: {contactId}.") { }
+    }
+}
