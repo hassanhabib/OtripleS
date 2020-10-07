2512561f01afefa4975bd6a896c8dd210f8aa5ac
Hassan Habib <hassanhabib@live.com>
2020-10-07 05:57:20 -0700

    Generate Migrations for Removing Audit fields

---
+++ OtripleS.Web.Api/Migrations/20201007125652_RemoveStudentContactAuditFields.cs
@@ -0,0 +1,63 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OtripleS.Web.Api.Migrations
+{
+    public partial class RemoveStudentContactAuditFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "StudentContacts");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "StudentContacts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "StudentContacts");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "StudentContacts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedBy",
+                table: "StudentContacts",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedDate",
+                table: "StudentContacts",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UpdatedBy",
+                table: "StudentContacts",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedDate",
+                table: "StudentContacts",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+    }
+}
