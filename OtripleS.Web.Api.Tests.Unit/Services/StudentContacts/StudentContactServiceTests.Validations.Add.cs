47c63495ba4cef4210ee1dd2bdf26336047afe4f
tobidub <github@tobiasdubral.de>
2020-10-08 23:05:18 +0200

    ShouldThrowValidationExceptionOnAddWhenStudentContactIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentContacts/StudentContactServiceTests.Validations.Add.cs
@@ -0,0 +1,51 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.StudentContacts;
+using OtripleS.Web.Api.Models.StudentContacts.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.StudentContacts
+{
+    public partial class StudentContactServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddWhenStudentContactIsNullAndLogItAsync()
+        {
+            // given
+            StudentContact randomStudentContact = default;
+            StudentContact nullStudentContact = randomStudentContact;
+            var nullStudentContactException = new NullStudentContactException();
+
+            var expectedStudentContactValidationException =
+                new StudentContactValidationException(nullStudentContactException);
+
+            // when
+            ValueTask<StudentContact> addStudentContactTask =
+                this.studentContactService.AddStudentContactAsync(nullStudentContact);
+
+            // then
+            await Assert.ThrowsAsync<StudentContactValidationException>(() =>
+                addStudentContactTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentContactValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertStudentContactAsync(It.IsAny<StudentContact>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
