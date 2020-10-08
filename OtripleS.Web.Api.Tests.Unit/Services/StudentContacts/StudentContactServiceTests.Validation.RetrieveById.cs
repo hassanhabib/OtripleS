78139e3f60aa4bc6f1ad594d29b67a2d18eeae22
eris.adhami <eri.adhami@gmail.com>
2020-10-08 20:59:57 +0200

    ShouldThrowValidatonExceptionOnRetrieveWhenStudentIdIsInvalidAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentContacts/StudentContactServiceTests.Validation.RetrieveById.cs
@@ -0,0 +1,52 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.StudentContacts;
+using OtripleS.Web.Api.Models.StudentContacts.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.StudentContacts
+{
+    public partial class StudentContactServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidatonExceptionOnRetrieveWhenStudentIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid randomContactId = Guid.NewGuid();
+            Guid randomStudentId = default;
+            Guid inputContactId = randomContactId;
+            Guid inputStudentId = randomStudentId;
+
+            var invalidStudentContactInputException = new InvalidStudentContactInputException(
+                parameterName: nameof(StudentContact.StudentId),
+                parameterValue: inputStudentId);
+
+            var expectedStudentContactValidationException =
+                new StudentContactValidationException(invalidStudentContactInputException);
+
+            // when
+            ValueTask<StudentContact> actualStudentContactTask =
+                this.studentContactService.RetrieveStudentContactByIdAsync(inputStudentId, inputContactId);
+
+            // then
+            await Assert.ThrowsAsync<StudentContactValidationException>(() => actualStudentContactTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentContactValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectStudentContactByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
