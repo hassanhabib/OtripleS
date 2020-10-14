84c4461b0023b7da7bd2e1e16a5944cc01c0d6e9
Viral Pandya <virav.pandya@gmail.com>
2020-10-14 00:26:58 -0700

    ShouldThrowValidatonExceptionOnRemoveWhenTeacherIdIsInvalidAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api/Models/TeacherContacts/Exceptions/TeacherContactValidationException.cs
@@ -0,0 +1,16 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+
+namespace OtripleS.Web.Api.Models.TeacherContacts.Exceptions
+{
+    public class TeacherContactValidationException : Exception
+    {
+        public TeacherContactValidationException(Exception innerException)
+            : base("Invalid input, contact support.", innerException)
+        { }
+    }
+}
