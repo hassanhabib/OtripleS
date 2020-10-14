c50e7f7e29d475027980717f7f5c103281ac28b0
tobidub <github@tobiasdubral.de>
2020-10-14 23:59:32 +0200

    ShouldThrowValidationExceptionOnAddWhenTeacherContactIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/TeacherContacts/TeacherContactServiceTests.Validation.Add.cs
@@ -0,0 +1,47 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.TeacherContacts;
+using OtripleS.Web.Api.Models.TeacherContacts.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.TeacherContacts
+{
+	public partial class TeacherContactServiceTests
+	{
+		[Fact]
+		public async void ShouldThrowValidationExceptionOnAddWhenTeacherContactIsNullAndLogItAsync()
+		{
+			// given
+			TeacherContact randomTeacherContact = default;
+			TeacherContact nullTeacherContact = randomTeacherContact;
+			var nullTeacherContactException = new NullTeacherContactException();
+
+			var expectedTeacherContactValidationException =
+				new TeacherContactValidationException(nullTeacherContactException);
+
+			// when
+			ValueTask<TeacherContact> addTeacherContactTask =
+				this.teacherContactService.AddTeacherContactAsync(nullTeacherContact);
+
+			// then
+			await Assert.ThrowsAsync<TeacherContactValidationException>(() =>
+				addTeacherContactTask.AsTask());
+
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogError(It.Is(SameExceptionAs(expectedTeacherContactValidationException))),
+					Times.Once);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.InsertTeacherContactAsync(It.IsAny<TeacherContact>()),
+					Times.Never);
+
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+			this.storageBrokerMock.VerifyNoOtherCalls();
+		}
+	}
+}
