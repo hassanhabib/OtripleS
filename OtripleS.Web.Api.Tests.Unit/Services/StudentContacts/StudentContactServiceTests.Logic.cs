3ffda569cdb36aa8c7d37f5f0990b08541e7aa09
Viral Pandya <virav.pandya@gmail.com>
2020-10-13 20:40:47 -0700

    ShouldRemoveStudentContactAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/StudentContacts/StudentContactServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentContacts/StudentContactServiceTests.Logic.cs
@@ -95,5 +95,47 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.StudentContacts
 			this.storageBrokerMock.VerifyNoOtherCalls();
 			this.loggingBrokerMock.VerifyNoOtherCalls();
 		}
+
+		[Fact]
+		public async Task ShouldRemoveStudentContactAsync()
+		{
+			// given
+			var randomStudentId = Guid.NewGuid();
+			var randomContactId = Guid.NewGuid();
+			Guid inputStudentId = randomStudentId;
+			Guid inputContactId = randomContactId;
+			DateTimeOffset inputDateTime = GetRandomDateTime();
+			StudentContact randomStudentContact = CreateRandomStudentContact(inputDateTime);
+			randomStudentContact.StudentId = inputStudentId;
+			randomStudentContact.ContactId = inputContactId;
+			StudentContact storageStudentContact = randomStudentContact;
+			StudentContact expectedStudentContact = storageStudentContact;
+
+			this.storageBrokerMock.Setup(broker =>
+				broker.SelectStudentContactByIdAsync(inputStudentId, inputContactId))
+					.ReturnsAsync(storageStudentContact);
+
+			this.storageBrokerMock.Setup(broker =>
+				broker.DeleteStudentContactAsync(storageStudentContact))
+					.ReturnsAsync(expectedStudentContact);
+
+			// when
+			StudentContact actualStudentContact =
+				await this.studentContactService.RemoveStudentContactByIdAsync(inputStudentId, inputContactId);
+
+			// then
+			actualStudentContact.Should().BeEquivalentTo(expectedStudentContact);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.SelectStudentContactByIdAsync(inputStudentId, inputContactId),
+					Times.Once);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.DeleteStudentContactAsync(storageStudentContact),
+					Times.Once);
+
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.storageBrokerMock.VerifyNoOtherCalls();
+		}
 	}
 }
