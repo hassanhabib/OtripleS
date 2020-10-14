a9b52442f0e1cda3c2d87f306bda8d4bab0b1a96
Viral Pandya <virav.pandya@gmail.com>
2020-10-14 00:23:34 -0700

    ShouldRemoveTeacherContactAsync -> FAIL

---
+++ OtripleS.Web.Api/Services/TeacherContacts/ITeacherContactService.cs
@@ -0,0 +1,17 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Models.TeacherContacts;
+
+namespace OtripleS.Web.Api.Services.TeacherContacts
+{
+	public interface ITeacherContactService
+	{
+		ValueTask<TeacherContact> RemoveTeacherContactByIdAsync(Guid teacherId, Guid contactId);
+	}
+}
