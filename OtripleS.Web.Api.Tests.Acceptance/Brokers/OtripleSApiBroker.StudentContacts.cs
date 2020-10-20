d18eefef339a94f1d1098e15d5086bb1ea06725c
JaAfghan <31682719+javedolfath@users.noreply.github.com>
2020-10-20 14:15:47 +0430

    ShouldPostStudentContact Async ->pass

---
+++ OtripleS.Web.Api.Tests.Acceptance/Brokers/OtripleSApiBroker.StudentContacts.cs
@@ -0,0 +1,24 @@
+﻿using OtripleS.Web.Api.Models.StudentContacts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtripleS.Web.Api.Tests.Acceptance.Brokers
+{
+   public partial class OtripleSApiBroker
+    {
+        private const string StudentContactRelativeUrl = "api/studentContacts";
+        public async ValueTask<StudentContact> PostStudentContactAsync(StudentContact studentContact) =>
+            await this.apiFactoryClient.PostContentAsync(StudentContactRelativeUrl, studentContact);
+
+        public async ValueTask<StudentContact> GetStudentContactByIdAsync(Guid studentId, Guid contactId) =>
+          await this.apiFactoryClient.GetContentAsync<StudentContact>(
+              $"{StudentContactRelativeUrl}/students/{studentId}/contacts/{contactId}");
+
+        public async ValueTask<StudentContact> DeleteStudentContactByIdAsync(Guid studentId, Guid contactId) =>
+           await this.apiFactoryClient.DeleteContentAsync<StudentContact>(
+               $"{StudentContactRelativeUrl}/students/{studentId}/contacts/{contactId}");
+    }
+}
