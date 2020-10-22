8c145fdb87932102ff034638cf2b786722353ef7
Viral Pandya <virav.pandya@gmail.com>
2020-10-21 19:56:24 -0700

    Broker changes for code first changes for database

---
+++ OtripleS.Web.Api/Brokers/Storage/StorageBroker.GuardianContacts.References.cs
@@ -0,0 +1,32 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+using OtripleS.Web.Api.Models.GuardianContacts;
+
+namespace OtripleS.Web.Api.Brokers.Storage
+{
+    public partial class StorageBroker
+    {
+        private void AddGuardianContactReferences(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GuardianContact>()
+                .HasKey(guardianContact =>
+                    new { guardianContact.GuardianId, guardianContact.ContactId });
+
+            modelBuilder.Entity<GuardianContact>()
+                .HasOne(guardianContact => guardianContact.Contact)
+                .WithMany(contact => contact.GuardianContacts)
+                .HasForeignKey(guardianContact => guardianContact.ContactId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<GuardianContact>()
+                .HasOne(guardianContact => guardianContact.Guardian)
+                .WithMany(guardian => guardian.GuardianContacts)
+                .HasForeignKey(guardianContact => guardianContact.GuardianId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
---
+++ OtripleS.Web.Api/Brokers/Storage/StorageBroker.GuardianContacts.cs
@@ -0,0 +1,64 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using OtripleS.Web.Api.Models.GuardianContacts;
+
+namespace OtripleS.Web.Api.Brokers.Storage
+{
+    public partial class StorageBroker
+    {
+        public DbSet<GuardianContact> GuardianContacts { get; set; }
+
+        public async ValueTask<GuardianContact> InsertGuardianContactAsync(
+            GuardianContact GuardianContact)
+        {
+            EntityEntry<GuardianContact> GuardianContactEntityEntry =
+                await this.GuardianContacts.AddAsync(GuardianContact);
+
+            await this.SaveChangesAsync();
+
+            return GuardianContactEntityEntry.Entity;
+        }
+
+        public IQueryable<GuardianContact> SelectAllGuardianContacts() =>
+            this.GuardianContacts.AsQueryable();
+
+        public async ValueTask<GuardianContact> SelectGuardianContactByIdAsync(
+            Guid guardianId,
+            Guid contactId)
+        {
+            this.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+
+            return await this.GuardianContacts.FindAsync(guardianId, contactId);
+        }
+
+        public async ValueTask<GuardianContact> UpdateGuardianContactAsync(
+            GuardianContact GuardianContact)
+        {
+            EntityEntry<GuardianContact> GuardianContactEntityEntry =
+                this.GuardianContacts.Update(GuardianContact);
+
+            await this.SaveChangesAsync();
+
+            return GuardianContactEntityEntry.Entity;
+        }
+
+        public async ValueTask<GuardianContact> DeleteGuardianContactAsync(
+            GuardianContact GuardianContact)
+        {
+            EntityEntry<GuardianContact> GuardianContactEntityEntry =
+                this.GuardianContacts.Remove(GuardianContact);
+
+            await this.SaveChangesAsync();
+
+            return GuardianContactEntityEntry.Entity;
+        }
+    }
+}
