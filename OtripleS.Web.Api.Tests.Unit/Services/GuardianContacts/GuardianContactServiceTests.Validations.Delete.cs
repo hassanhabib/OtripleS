e8ba1ca38a9e04d783d69e20641f2e782826bdf3
tobidub <github@tobiasdubral.de>
2020-10-23 00:32:20 +0200

    ShouldThrowValidatonExceptionOnRemoveWhenGuardianIdIsInvalidAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/GuardianContacts/GuardianContactServiceTests.Validations.Delete.cs
@@ -0,0 +1,56 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.GuardianContacts;
+using OtripleS.Web.Api.Models.GuardianContacts.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.GuardianContacts
+{
+	public partial class GuardianContactServiceTests
+	{
+		[Fact]
+		public async Task ShouldThrowValidatonExceptionOnRemoveWhenGuardianIdIsInvalidAndLogItAsync()
+		{
+			// given
+			Guid randomContactId = Guid.NewGuid();
+			Guid randomGuardianId = default;
+			Guid inputContactId = randomContactId;
+			Guid inputGuardianId = randomGuardianId;
+
+			var invalidGuardianContactInputException = new InvalidGuardianContactInputException(
+				parameterName: nameof(GuardianContact.GuardianId),
+				parameterValue: inputGuardianId);
+
+			var expectedGuardianContactValidationException =
+				new GuardianContactValidationException(invalidGuardianContactInputException);
+
+			// when
+			ValueTask<GuardianContact> removeGuardianContactTask =
+				this.guardianContactService.RemoveGuardianContactByIdAsync(inputGuardianId, inputContactId);
+
+			// then
+			await Assert.ThrowsAsync<GuardianContactValidationException>(() => removeGuardianContactTask.AsTask());
+
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogError(It.Is(SameExceptionAs(expectedGuardianContactValidationException))),
+					Times.Once);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.SelectGuardianContactByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()),
+					Times.Never);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.DeleteGuardianContactAsync(It.IsAny<GuardianContact>()),
+					Times.Never);
+
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+		}
+	}
+}
