0320aa3457b7fc5a7edf02d8e51a615f9a2dfe38
Viral Pandya <virav.pandya@gmail.com>
2020-10-21 20:29:50 -0700

    Code Cleanup

--- OtripleS.Web.Api/Services/GuardianContacts/GuardianContactService.Exceptions.cs
+++ OtripleS.Web.Api/Services/GuardianContacts/GuardianContactService.Exceptions.cs
@@ -4,7 +4,6 @@
 //----------------------------------------------------------------
 
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 using EFxceptions.Models.Exceptions;
 using Microsoft.Data.SqlClient;
--- OtripleS.Web.Api/Services/GuardianContacts/GuardianContactService.Validations.cs
+++ OtripleS.Web.Api/Services/GuardianContacts/GuardianContactService.Validations.cs
@@ -4,7 +4,6 @@
 //----------------------------------------------------------------
 
 using System;
-using System.Linq;
 using OtripleS.Web.Api.Models.GuardianContacts;
 using OtripleS.Web.Api.Models.GuardianContacts.Exceptions;
 
