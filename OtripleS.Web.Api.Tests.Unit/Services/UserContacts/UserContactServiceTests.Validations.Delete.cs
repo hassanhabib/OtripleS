0279c7e6dd4a2ca00030cf3365995f6549074869
tobidub <github@tobiasdubral.de>
2020-10-29 23:25:12 +0100

    ShouldThrowValidatonExceptionOnRemoveWhenContactIdIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/UserContacts/UserContactServiceTests.Validations.Delete.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/UserContacts/UserContactServiceTests.Validations.Delete.cs
@@ -14,44 +14,82 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.UserContacts
 {
 	public partial class UserContactServiceTests
 	{
-        [Fact]
-        public async Task ShouldThrowValidatonExceptionOnRemoveWhenUserIdIsInvalidAndLogItAsync()
-        {
-            // given
-            Guid randomContactId = Guid.NewGuid();
-            Guid randomUserId = default;
-            Guid inputContactId = randomContactId;
-            Guid inputUserId = randomUserId;
+		[Fact]
+		public async Task ShouldThrowValidatonExceptionOnRemoveWhenUserIdIsInvalidAndLogItAsync()
+		{
+			// given
+			Guid randomContactId = Guid.NewGuid();
+			Guid randomUserId = default;
+			Guid inputContactId = randomContactId;
+			Guid inputUserId = randomUserId;
 
-            var invalidUserContactInputException = new InvalidUserContactInputException(
-                parameterName: nameof(UserContact.UserId),
-                parameterValue: inputUserId);
+			var invalidUserContactInputException = new InvalidUserContactInputException(
+				parameterName: nameof(UserContact.UserId),
+				parameterValue: inputUserId);
 
-            var expectedUserContactValidationException =
-                new UserContactValidationException(invalidUserContactInputException);
+			var expectedUserContactValidationException =
+				new UserContactValidationException(invalidUserContactInputException);
 
-            // when
-            ValueTask<UserContact> removeUserContactTask =
-                this.userContactService.RemoveUserContactByIdAsync(inputUserId, inputContactId);
+			// when
+			ValueTask<UserContact> removeUserContactTask =
+				this.userContactService.RemoveUserContactByIdAsync(inputUserId, inputContactId);
 
-            // then
-            await Assert.ThrowsAsync<UserContactValidationException>(() => removeUserContactTask.AsTask());
+			// then
+			await Assert.ThrowsAsync<UserContactValidationException>(() => removeUserContactTask.AsTask());
 
-            this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(expectedUserContactValidationException))),
-                    Times.Once);
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogError(It.Is(SameExceptionAs(expectedUserContactValidationException))),
+					Times.Once);
 
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectUserContactByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()),
-                    Times.Never);
+			this.storageBrokerMock.Verify(broker =>
+				broker.SelectUserContactByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()),
+					Times.Never);
 
-            this.storageBrokerMock.Verify(broker =>
-                broker.DeleteUserContactAsync(It.IsAny<UserContact>()),
-                    Times.Never);
+			this.storageBrokerMock.Verify(broker =>
+				broker.DeleteUserContactAsync(It.IsAny<UserContact>()),
+					Times.Never);
 
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+		}
 
-    }
+		[Fact]
+		public async Task ShouldThrowValidatonExceptionOnRemoveWhenContactIdIsInvalidAndLogItAsync()
+		{
+			// given
+			Guid randomContactId = default;
+			Guid randomUserId = Guid.NewGuid();
+			Guid inputContactId = randomContactId;
+			Guid inputUserId = randomUserId;
+
+			var invalidUserContactInputException = new InvalidUserContactInputException(
+				parameterName: nameof(UserContact.ContactId),
+				parameterValue: inputContactId);
+
+			var expectedUserContactValidationException =
+				new UserContactValidationException(invalidUserContactInputException);
+
+			// when
+			ValueTask<UserContact> removeUserContactTask =
+				this.userContactService.RemoveUserContactByIdAsync(inputUserId, inputContactId);
+
+			// then
+			await Assert.ThrowsAsync<UserContactValidationException>(() => removeUserContactTask.AsTask());
+
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogError(It.Is(SameExceptionAs(expectedUserContactValidationException))),
+					Times.Once);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.SelectUserContactByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()),
+					Times.Never);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.DeleteUserContactAsync(It.IsAny<UserContact>()),
+					Times.Never);
+
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+		}
+	}
 }
