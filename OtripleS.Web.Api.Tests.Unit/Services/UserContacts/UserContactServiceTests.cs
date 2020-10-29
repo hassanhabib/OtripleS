43b5b73d11451dc42819a9452fc9b4df1a0c4729
tobidub <github@tobiasdubral.de>
2020-10-29 23:09:22 +0100

    ShouldRemoveUserContactAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/UserContacts/UserContactServiceTests.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Moq;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.UserContacts;
+using OtripleS.Web.Api.Services.UserContacts;
+using Tynamix.ObjectFiller;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.UserContacts
+{
+	public partial class UserContactServiceTests
+	{
+		private readonly Mock<IStorageBroker> storageBrokerMock;
+		private readonly Mock<ILoggingBroker> loggingBrokerMock;
+		private readonly IUserContactService userContactService;
+
+		public UserContactServiceTests()
+		{
+			this.storageBrokerMock = new Mock<IStorageBroker>();
+			this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+			this.userContactService = new UserContactService(
+				storageBroker: this.storageBrokerMock.Object,
+				loggingBroker: this.loggingBrokerMock.Object);
+		}
+
+		private UserContact CreateRandomUserContact() =>
+			CreateUserContactFiller().Create();
+
+		private static Filler<UserContact> CreateUserContactFiller()
+		{
+			var filler = new Filler<UserContact>();
+			filler.Setup()
+				.OnProperty(usercontact => usercontact.User).IgnoreIt()
+				.OnProperty(usercontact => usercontact.Contact).IgnoreIt();
+
+			return filler;
+		}
+	}
+}
---
+++ OtripleS.Web.Api/Services/UserContacts/UserContactService.cs
@@ -0,0 +1,48 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.UserContacts;
+
+namespace OtripleS.Web.Api.Services.UserContacts
+{
+	public partial class UserContactService : IUserContactService
+	{
+		private readonly IStorageBroker storageBroker;
+		private readonly ILoggingBroker loggingBroker;
+
+		public UserContactService(
+			IStorageBroker storageBroker,
+			ILoggingBroker loggingBroker)
+		{
+			this.storageBroker = storageBroker;
+			this.loggingBroker = loggingBroker;
+		}
+
+		public ValueTask<UserContact> AddUserContactAsync(UserContact userContact)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ValueTask<UserContact> RemoveUserContactByIdAsync(Guid userId, Guid contactId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IQueryable<UserContact> RetrieveAllUserContacts()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ValueTask<UserContact> RetrieveUserContactByIdAsync(Guid userId, Guid contactId)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
