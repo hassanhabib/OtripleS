721c96b4d14e7e5178f3a71dc59da525a0603f57
Viral Pandya <virav.pandya@gmail.com>
2020-10-28 20:27:07 -0700

    ShouldAddUserContactAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/UserContacts/UserContactServiceTests.Logic.cs
@@ -0,0 +1,45 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.UserContacts;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.UserContacts
+{
+	public partial class UserContactServiceTests
+	{
+		[Fact]
+		public async Task ShouldAddUserContactAsync()
+		{
+			// given
+			UserContact randomUserContact = CreateRandomUserContact();
+			UserContact inputUserContact = randomUserContact;
+			UserContact storageUserContact = randomUserContact;
+			UserContact expectedUserContact = storageUserContact;
+
+			this.storageBrokerMock.Setup(broker =>
+				broker.InsertUserContactAsync(inputUserContact))
+					.ReturnsAsync(storageUserContact);
+
+			// when
+			UserContact actualUserContact =
+				await this.userContactService.AddUserContactAsync(inputUserContact);
+
+			// then
+			actualUserContact.Should().BeEquivalentTo(expectedUserContact);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.InsertUserContactAsync(inputUserContact),
+					Times.Once);
+
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.storageBrokerMock.VerifyNoOtherCalls();
+		}
+    }
+}
