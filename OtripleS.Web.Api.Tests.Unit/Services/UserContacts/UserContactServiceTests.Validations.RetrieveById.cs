a49af1a063abc58ae207d2c0c98af653526c5eb7
eris.adhami <eri.adhami@gmail.com>
2020-10-30 00:38:30 +0100

    ShouldThrowValidationExceptionOnRetrieveWhenStorageUserContactIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/UserContacts/UserContactServiceTests.Validations.RetrieveById.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/UserContacts/UserContactServiceTests.Validations.RetrieveById.cs
@@ -83,5 +83,44 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.UserContacts
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveWhenStorageUserContactIsInvalidAndLogItAsync()
+        {
+            // given
+            UserContact randomUserContact = CreateRandomUserContact();
+            Guid inputContactId = randomUserContact.ContactId;
+            Guid inputUserId = randomUserContact.UserId;
+            UserContact nullStorageUserContact = null;
+
+            var notFoundUserContactException =
+                new NotFoundUserContactException(inputUserId, inputContactId);
+
+            var expectedUserContactValidationException =
+                new UserContactValidationException(notFoundUserContactException);
+
+            this.storageBrokerMock.Setup(broker =>
+                 broker.SelectUserContactByIdAsync(inputUserId, inputContactId))
+                    .ReturnsAsync(nullStorageUserContact);
+
+            // when
+            ValueTask<UserContact> retrieveUserContactTask =
+                this.userContactService.RetrieveUserContactByIdAsync(inputUserId, inputContactId);
+
+            // then
+            await Assert.ThrowsAsync<UserContactValidationException>(() =>
+                retrieveUserContactTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedUserContactValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserContactByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
