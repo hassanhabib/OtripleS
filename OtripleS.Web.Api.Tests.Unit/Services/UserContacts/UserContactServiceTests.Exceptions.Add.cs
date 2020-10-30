95f22f5b4f3d8d4f73db76d48cef90e2eb382eb5
Viral Pandya <virav.pandya@gmail.com>
2020-10-30 07:30:01 -0700

    ShouldThrowDependencyExceptionOnAddWhenSqlExceptionOccursAndLogItAsync -> PASS

--- OtripleS.Web.Api.Tests.Unit/Services/UserContacts/UserContactServiceTests.Exceptions.Add.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/UserContacts/UserContactServiceTests.Exceptions.Add.cs
@@ -3,10 +3,49 @@
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 //----------------------------------------------------------------
 
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.UserContacts;
+using OtripleS.Web.Api.Models.UserContacts.Exceptions;
+using Xunit;
+
 namespace OtripleS.Web.Api.Tests.Unit.Services.UserContacts
 {
     public partial class UserContactServiceTests
     {
-        
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            UserContact randomUserContact = CreateRandomUserContact();
+            UserContact inputUserContact = randomUserContact;
+            var sqlException = GetSqlException();
+
+            var expectedUserContactDependencyException =
+                new UserContactDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertUserContactAsync(inputUserContact))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<UserContact> addUserContactTask =
+                this.userContactService.AddUserContactAsync(inputUserContact);
+
+            // then
+            await Assert.ThrowsAsync<UserContactDependencyException>(() =>
+                addUserContactTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedUserContactDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertUserContactAsync(inputUserContact),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
