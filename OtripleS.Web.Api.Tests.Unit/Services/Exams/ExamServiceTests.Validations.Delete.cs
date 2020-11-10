af0f3df3022538889db479c7d9b0c73c8cbbf279
tobidub <github@tobiasdubral.de>
2020-11-10 22:07:11 +0100

    ShouldThrowValidatonExceptionOnDeleteWhenStorageExamIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/Exams/ExamServiceTests.Validations.Delete.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Exams/ExamServiceTests.Validations.Delete.cs
@@ -51,5 +51,48 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Exams
 			this.loggingBrokerMock.VerifyNoOtherCalls();
 			this.dateTimeBrokerMock.VerifyNoOtherCalls();
 		}
+
+		[Fact]
+		public async Task ShouldThrowValidatonExceptionOnDeleteWhenStorageExamIsInvalidAndLogItAsync()
+		{
+			// given
+			DateTimeOffset dateTime = GetRandomDateTime();
+			Exam randomExam = CreateRandomExam(dateTime);
+			Guid inputExamId = randomExam.Id;
+			Exam inputExam = randomExam;
+			Exam nullStorageExam = null;
+
+			var notFoundExamException = new NotFoundExamException(inputExamId);
+
+			var expectedExamValidationException =
+				new ExamValidationException(notFoundExamException);
+
+			this.storageBrokerMock.Setup(broker =>
+				broker.SelectExamByIdAsync(inputExamId))
+					.ReturnsAsync(nullStorageExam);
+
+			// when
+			ValueTask<Exam> actualExamTask =
+				this.examService.DeleteExamByIdAsync(inputExamId);
+
+			// then
+			await Assert.ThrowsAsync<ExamValidationException>(() => actualExamTask.AsTask());
+
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogError(It.Is(SameExceptionAs(expectedExamValidationException))),
+					Times.Once);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.SelectExamByIdAsync(inputExamId),
+					Times.Once);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.DeleteExamAsync(It.IsAny<Exam>()),
+					Times.Never);
+
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+			this.dateTimeBrokerMock.VerifyNoOtherCalls();
+		}
 	}
 }
