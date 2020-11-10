e8c757358f9bcf556ee625b558b2140513b7c41d
tobidub <github@tobiasdubral.de>
2020-11-10 21:47:18 +0100

    Add ExamService

---
+++ OtripleS.Web.Api/Services/Exams/ExamService.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Brokers.DateTimes;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.Exams;
+
+namespace OtripleS.Web.Api.Services.Exams
+{
+	public class ExamService : IExamService
+	{
+		private readonly IStorageBroker storageBroker;
+		private readonly ILoggingBroker loggingBroker;
+		private readonly IDateTimeBroker dateTimeBroker;
+
+		public ExamService(IStorageBroker storageBroker,
+			ILoggingBroker loggingBroker,
+			IDateTimeBroker dateTimeBroker)
+		{
+			this.storageBroker = storageBroker;
+			this.loggingBroker = loggingBroker;
+			this.dateTimeBroker = dateTimeBroker;
+		}
+
+		public ValueTask<Exam> DeleteExamByIdAsync(Guid ExamId)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
