1107ad57bad46eecf2e1fc406306c011b75d5ba3
Viral Pandya <virav.pandya@gmail.com>
2020-11-11 20:23:31 -0800

    ShouldThrowValidationExceptionOnAddWhenExamAlreadyExistsAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/Exams/ExamServiceTests.Validations.Add.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Exams/ExamServiceTests.Validations.Add.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using Moq;
 using OtripleS.Web.Api.Models.Exams;
 using OtripleS.Web.Api.Models.Exams.Exceptions;
@@ -353,6 +354,57 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Exams
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddWhenExamAlreadyExistsAndLogItAsync()
+        {
+            // given
+            DateTimeOffset dateTime = GetRandomDateTime();
+            Exam randomExam = CreateRandomExam(dateTime);
+            Exam alreadyExistsExam = randomExam;
+            alreadyExistsExam.UpdatedBy = alreadyExistsExam.CreatedBy;
+            string randomMessage = GetRandomMessage();
+            string exceptionMessage = randomMessage;
+            var duplicateKeyException = new DuplicateKeyException(exceptionMessage);
+
+            var alreadyExistsExamException =
+                new AlreadyExistsExamException(duplicateKeyException);
+
+            var expectedExamValidationException =
+                new ExamValidationException(alreadyExistsExamException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Returns(dateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertExamAsync(alreadyExistsExam))
+                    .ThrowsAsync(duplicateKeyException);
+
+            // when
+            ValueTask<Exam> createExamTask =
+                this.examService.AddExamAsync(alreadyExistsExam);
+
+            // then
+            await Assert.ThrowsAsync<ExamValidationException>(() =>
+                createExamTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(expectedExamValidationException))),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertExamAsync(alreadyExistsExam),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
