d6524cd7af5ebed67387651d3f360260cf83eef6
Viral Pandya <virav.pandya@gmail.com>
2020-11-11 20:09:48 -0800

    ShouldThrowValidationExceptionOnAddWhenIdIsInvalidAndLogItAsync -> PASS

--- OtripleS.Web.Api/Services/Exams/ExamService.Validations.cs
+++ OtripleS.Web.Api/Services/Exams/ExamService.Validations.cs
@@ -9,41 +9,39 @@ using OtripleS.Web.Api.Models.Exams.Exceptions;
 
 namespace OtripleS.Web.Api.Services.Exams
 {
-	public partial class ExamService
-	{
-		private void ValidateExamId(Guid examId)
-		{
-			if (IsInvalid(examId))
-			{
-				throw new InvalidExamInputException(
-					parameterName: nameof(Exam.Id),
-					parameterValue: examId);
-			}
-		}
+    public partial class ExamService
+    {
+        private void ValidateExamOnAdd(Exam exam)
+        {
+            ValidateExamIdIsNotNull(exam);
+            ValidateExamId(exam.Id);
+        }
+        private void ValidateExamId(Guid examId)
+        {
+            if (IsInvalid(examId))
+            {
+                throw new InvalidExamInputException(
+                    parameterName: nameof(Exam.Id),
+                    parameterValue: examId);
+            }
+        }
 
-		private void ValidateStorageExam(Exam storageExam, Guid examId)
-		{
-			if (storageExam == null)
-			{
-				throw new NotFoundExamException(examId);
-			}
-		}
+        private void ValidateStorageExam(Exam storageExam, Guid examId)
+        {
+            if (storageExam == null)
+            {
+                throw new NotFoundExamException(examId);
+            }
+        }
 
+        private void ValidateExamIdIsNotNull(Exam exam)
+        {
+            if (exam == default)
+            {
+                throw new NullExamException();
+            }
+        }
 
-		private void ValidateExamOnAdd(Exam exam)
-		{
-			ValidateExamIdIsNotNull(exam);
-		}
-
-
-		private void ValidateExamIdIsNotNull(Exam exam)
-		{
-			if (exam == default)
-			{
-				throw new NullExamException();
-			}
-		}
-
-		private static bool IsInvalid(Guid input) => input == default;
-	}
+        private static bool IsInvalid(Guid input) => input == default;
+    }
 }
