36b9fc7fa2db5505c7a435fea6b34feb0b0488c9
eris.adhami <eri.adhami@gmail.com>
2020-11-14 09:11:54 +0100

    ShouldThrowValidationExceptionOnModifyWhenUpdatedDateIsNotRecentAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/Exams/ExamServiceTests.Validations.Modify.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Exams/ExamServiceTests.Validations.Modify.cs
@@ -258,5 +258,53 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Exams
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [MemberData(nameof(InvalidMinuteCases))]
+        public async void ShouldThrowValidationExceptionOnModifyWhenUpdatedDateIsNotRecentAndLogItAsync(
+            int minutes)
+        {
+            // given
+            DateTimeOffset dateTime = GetRandomDateTime();
+            Exam randomExam = CreateRandomExam(dateTime);
+            Exam inputExam = randomExam;
+            inputExam.UpdatedBy = inputExam.CreatedBy;
+            inputExam.UpdatedDate = dateTime.AddMinutes(minutes);
+
+            var invalidExamInputException = new InvalidExamInputException(
+                parameterName: nameof(Exam.UpdatedDate),
+                parameterValue: inputExam.UpdatedDate);
+
+            var expectedExamValidationException =
+                new ExamValidationException(invalidExamInputException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Returns(dateTime);
+
+            // when
+            ValueTask<Exam> modifyExamTask =
+                this.examService.ModifyExamAsync(inputExam);
+
+            // then
+            await Assert.ThrowsAsync<ExamValidationException>(() =>
+                modifyExamTask.AsTask());
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedExamValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectExamByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
