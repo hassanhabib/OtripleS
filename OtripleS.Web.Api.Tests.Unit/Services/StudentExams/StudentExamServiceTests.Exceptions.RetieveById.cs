aa960a1ec806c54227bdf1ccb0a8e30e05063a75
eris.adhami <eri.adhami@gmail.com>
2020-11-18 13:19:49 +0100

    ShouldThrowDependencyExceptionOnRetrieveWhenSqlExceptionOccursAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentExams/StudentExamServiceTests.Exceptions.RetieveById.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.StudentExams;
+using OtripleS.Web.Api.Models.StudentExams.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.StudentExams
+{
+    public partial class StudentExamServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid randomStudentExamId = Guid.NewGuid();
+            Guid inputStudentExamId = randomStudentExamId;
+            var sqlException = GetSqlException();
+
+            var expectedStudentExamDependencyException =
+                new StudentExamDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectStudentExamByIdAsync(inputStudentExamId))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<StudentExam> retrieveStudentExamByIdTask =
+                this.studentExamService.RetrieveStudentExamByIdAsync(inputStudentExamId);
+
+            // then
+            await Assert.ThrowsAsync<StudentExamDependencyException>(() =>
+                retrieveStudentExamByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedStudentExamDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectStudentExamByIdAsync(inputStudentExamId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
