ae82d3c7f2803a89e2f183e3520f724c1efc569d
Viral Pandya <virav.pandya@gmail.com>
2020-11-20 22:31:29 -0800

    ShouldThrowValidationExceptionOnModifyWhenStudentIdIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/StudentExams/StudentExamServiceTests.Validations.Modify.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentExams/StudentExamServiceTests.Validations.Modify.cs
@@ -3,6 +3,7 @@
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,6 +26,40 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.StudentExams
 
             var expectedStudentExamValidationException =
                 new StudentExamValidationException(nullStudentExamException);
+
+            //when
+            ValueTask<StudentExam> modifyStudentExamTask =
+                this.studentExamService.ModifyStudentExamAsync(invalidStudentExam);
+
+            //then
+            await Assert.ThrowsAsync<StudentExamValidationException>(() =>
+                modifyStudentExamTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentExamValidationException))),
+                Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyWhenStudentIdIsInvalidAndLogItAsync()
+        {
+            //given
+            Guid invalidStudentId = Guid.Empty;
+            DateTimeOffset dateTime = GetRandomDateTime();
+            StudentExam randomStudentExam = CreateRandomStudentExam(dateTime);
+            StudentExam invalidStudentExam = randomStudentExam;
+            invalidStudentExam.StudentId = invalidStudentId;
+
+            var invalidStudentExamInputException = new InvalidStudentExamInputException(
+                parameterName: nameof(StudentExam.StudentId),
+                parameterValue: invalidStudentExam.StudentId);
+
+            var expectedStudentExamValidationException =
+                new StudentExamValidationException(invalidStudentExamInputException);
 
             //when
             ValueTask<StudentExam> modifyStudentExamTask =
