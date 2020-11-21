ddb5b086d4862b7aa57fc7681af545100cd555e6
Viral Pandya <virav.pandya@gmail.com>
2020-11-20 22:17:42 -0800

    ShouldModifyStudentExamAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/StudentExams/StudentExamServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentExams/StudentExamServiceTests.Logic.cs
@@ -7,6 +7,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Force.DeepCloner;
 using Moq;
 using OtripleS.Web.Api.Models.StudentExams;
 using Xunit;
@@ -78,6 +79,59 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.StudentExams
 			this.dateTimeBrokerMock.VerifyNoOtherCalls();
 			this.storageBrokerMock.VerifyNoOtherCalls();
 			this.storageBrokerMock.VerifyNoOtherCalls();
+		}
+
+		[Fact]
+		public async Task ShouldModifyStudentExamAsync()
+		{
+			// given
+			int randomNumber = GetRandomNumber();
+			int randomDays = randomNumber;
+			DateTimeOffset randomDate = GetRandomDateTime();
+			DateTimeOffset randomInputDate = GetRandomDateTime();
+			StudentExam randomStudentExam = CreateRandomStudentExam(randomInputDate);
+			StudentExam inputStudentExam = randomStudentExam;
+			StudentExam afterUpdateStorageStudentExam = inputStudentExam;
+			StudentExam expectedStudentExam = afterUpdateStorageStudentExam;
+			StudentExam beforeUpdateStorageStudentExam = randomStudentExam.DeepClone();
+			inputStudentExam.UpdatedDate = randomDate;
+			Guid studentId = inputStudentExam.StudentId;
+			Guid guardianId = inputStudentExam.ExamId;
+
+			this.dateTimeBrokerMock.Setup(broker =>
+				broker.GetCurrentDateTime())
+					.Returns(randomDate);
+
+			this.storageBrokerMock.Setup(broker =>
+				broker.SelectStudentExamByIdAsync(inputStudentExam.Id))
+					.ReturnsAsync(beforeUpdateStorageStudentExam);
+
+			this.storageBrokerMock.Setup(broker =>
+				broker.UpdateStudentExamAsync(inputStudentExam))
+					.ReturnsAsync(afterUpdateStorageStudentExam);
+
+			// when
+			StudentExam actualStudentExam =
+				await this.studentExamService.ModifyStudentExamAsync(inputStudentExam);
+
+			// then
+			actualStudentExam.Should().BeEquivalentTo(expectedStudentExam);
+
+			this.dateTimeBrokerMock.Verify(broker =>
+				broker.GetCurrentDateTime(),
+					Times.Once);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.SelectStudentExamByIdAsync(inputStudentExam.Id),
+					Times.Once);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.UpdateStudentExamAsync(inputStudentExam),
+					Times.Once);
+
+			this.storageBrokerMock.VerifyNoOtherCalls();
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+			this.dateTimeBrokerMock.VerifyNoOtherCalls();
 		}
 
 		[Fact]
