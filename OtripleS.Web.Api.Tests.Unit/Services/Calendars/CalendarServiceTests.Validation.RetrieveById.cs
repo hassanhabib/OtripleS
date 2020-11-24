186fdeb3ec62b9cb973e35be7b2cfb33c800fd1e
eris.adhami <eri.adhami@gmail.com>
2020-11-24 14:38:20 +0100

    ShouldThrowValidationExceptionOnRetrieveWhenStorageCalendarIsNullAndLogItAsync - FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/Calendars/CalendarServiceTests.Validation.RetrieveById.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Calendars/CalendarServiceTests.Validation.RetrieveById.cs
@@ -52,5 +52,46 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Calendars
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnRetrieveWhenStorageCalendarIsNullAndLogItAsync()
+        {
+            // given
+            Guid randomCalendarId = Guid.NewGuid();
+            Guid inputCalendarId = randomCalendarId;
+            Calendar invalidStorageCalendar = null;
+            var notFoundCalendarException = new NotFoundCalendarException(inputCalendarId);
+
+            var expectedCalendarValidationException =
+                new CalendarValidationException(notFoundCalendarException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectCalendarByIdAsync(inputCalendarId))
+                    .ReturnsAsync(invalidStorageCalendar);
+
+            // when
+            ValueTask<Calendar> retrieveCalendarByIdTask =
+                this.calendarService.RetrieveCalendarByIdAsync(inputCalendarId);
+
+            // then
+            await Assert.ThrowsAsync<CalendarValidationException>(() =>
+                retrieveCalendarByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedCalendarValidationException))),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCalendarByIdAsync(inputCalendarId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ OtripleS.Web.Api/Models/Calendars/Exceptions/NotFoundCalendarException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace OtripleS.Web.Api.Models.Calendars.Exceptions
+{
+    public class NotFoundCalendarException : Exception
+    {
+        public NotFoundCalendarException(Guid calendarId)
+            : base($"Couldn't find calendar with Id: {calendarId}.") { }
+    }
+}
