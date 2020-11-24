1173db9f5eadf502c69b35d57febfac9514def6a
tobidub <github@tobiasdubral.de>
2020-11-24 23:46:41 +0100

    ShouldThrowValidationExceptionOnModifyWhenUpdatedDateIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/Calendars/CalendarServiceTests.Validation.Modify.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Calendars/CalendarServiceTests.Validation.Modify.cs
@@ -221,5 +221,42 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Calendars
 			this.loggingBrokerMock.VerifyNoOtherCalls();
 			this.storageBrokerMock.VerifyNoOtherCalls();
 		}
+
+		[Fact]
+		public async void ShouldThrowValidationExceptionOnModifyWhenUpdatedDateIsInvalidAndLogItAsync()
+		{
+			// given
+			DateTimeOffset dateTime = GetRandomDateTime();
+			Calendar randomCalendar = CreateRandomCalendar(dateTime);
+			Calendar inputCalendar = randomCalendar;
+			inputCalendar.UpdatedDate = default;
+
+			var invalidCalendarInputException = new InvalidCalendarInputException(
+				parameterName: nameof(Calendar.UpdatedDate),
+				parameterValue: inputCalendar.UpdatedDate);
+
+			var expectedCalendarValidationException =
+				new CalendarValidationException(invalidCalendarInputException);
+
+			// when
+			ValueTask<Calendar> modifyCalendarTask =
+				this.calendarService.ModifyCalendarAsync(inputCalendar);
+
+			// then
+			await Assert.ThrowsAsync<CalendarValidationException>(() =>
+				modifyCalendarTask.AsTask());
+
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogError(It.Is(SameExceptionAs(expectedCalendarValidationException))),
+					Times.Once);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.SelectCalendarByIdAsync(It.IsAny<Guid>()),
+					Times.Never);
+
+			this.dateTimeBrokerMock.VerifyNoOtherCalls();
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+			this.storageBrokerMock.VerifyNoOtherCalls();
+		}
 	}
 }
