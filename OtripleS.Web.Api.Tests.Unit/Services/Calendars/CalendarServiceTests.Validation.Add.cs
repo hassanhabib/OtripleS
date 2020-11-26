555d50d244bd5bcc41d51d8588db2cd5b32e91bb
Viral Pandya <virav.pandya@gmail.com>
2020-11-25 19:05:33 -0800

    ShouldThrowValidationExceptionOnAddWhenCalendarIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Calendars/CalendarServiceTests.Validation.Add.cs
@@ -0,0 +1,51 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using Moq;
+using OtripleS.Web.Api.Models.Calendars;
+using OtripleS.Web.Api.Models.Calendars.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Calendars
+{
+	public partial class CalendarServiceTests
+	{
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddWhenCalendarIsNullAndLogItAsync()
+        {
+            // given
+            Calendar randomCalendar = null;
+            Calendar nullCalendar = randomCalendar;
+
+            var nullCalendarException = new NullCalendarException();
+
+            var expectedCalendarValidationException =
+                new CalendarValidationException(nullCalendarException);
+
+            // when
+            ValueTask<Calendar> registerCalendarTask =
+                this.calendarService.AddCalendarAsync(nullCalendar);
+
+            // then
+            await Assert.ThrowsAsync<CalendarValidationException>(() =>
+                registerCalendarTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedCalendarValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertCalendarAsync(It.IsAny<Calendar>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
