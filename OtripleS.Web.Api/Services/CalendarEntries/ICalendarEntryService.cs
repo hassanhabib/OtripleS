036413c7b3a882b41986e2ea1c42d7d32a2d4e89
Stanislaw Nowak <stanislaw@nowakpost.com>
2020-12-02 00:55:08 +0100

    ShouldRetrieveAllCalendarEntries => FAIL

---
+++ OtripleS.Web.Api/Services/CalendarEntries/ICalendarEntryService.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Linq;
+using OtripleS.Web.Api.Models.CalendarEntries;
+
+namespace OtripleS.Web.Api.Services.CalendarEntries
+{
+    public interface ICalendarEntryService
+    {
+        IQueryable<CalendarEntry> RetrieveAllCalendarEntries();
+    }
+}
