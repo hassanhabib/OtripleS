a5eaf9cb16aae84dabbc9c8f0c3499e4607a9911
Viral Pandya <virav.pandya@gmail.com>
2020-12-03 21:51:57 -0800

    ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync -> PASS

---
+++ OtripleS.Web.Api.Tests.Unit/Services/CalendarEntries/CalendarEntryServiceTests.Exceptions.Remove.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using OtripleS.Web.Api.Models.CalendarEntries;
+using OtripleS.Web.Api.Models.CalendarEntries.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.CalendarEntries
+{
+    public partial class CalendarEntryServiceTests
+    {
+		[Fact]
+		public async Task ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync()
+		{
+			// given
+			Guid randomCalendarEntryId = Guid.NewGuid();
+			Guid inputCalendarEntryId = randomCalendarEntryId;
+			SqlException sqlException = GetSqlException();
+
+			var expectedCalendarEntryDependencyException =
+				new CalendarEntryDependencyException(sqlException);
+
+			this.storageBrokerMock.Setup(broker =>
+				broker.SelectCalendarEntryByIdAsync(inputCalendarEntryId))
+					.ThrowsAsync(sqlException);
+
+			// when
+			ValueTask<CalendarEntry> deleteCalendarEntryTask =
+				this.calendarEntryService.DeleteCalendarEntryByIdAsync(inputCalendarEntryId);
+
+			// then
+			await Assert.ThrowsAsync<CalendarEntryDependencyException>(() =>
+				deleteCalendarEntryTask.AsTask());
+
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogCritical(It.Is(SameExceptionAs(expectedCalendarEntryDependencyException))),
+					Times.Once);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.SelectCalendarEntryByIdAsync(inputCalendarEntryId),
+					Times.Once);
+
+			this.dateTimeBrokerMock.VerifyNoOtherCalls();
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+			this.storageBrokerMock.VerifyNoOtherCalls();
+		}
+	}
+}
