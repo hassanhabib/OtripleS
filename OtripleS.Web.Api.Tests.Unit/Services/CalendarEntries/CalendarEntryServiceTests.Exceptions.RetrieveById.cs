9999266c4593de8429f1475250bd44b1a463dc72
Viral Pandya <virav.pandya@gmail.com>
2020-12-03 18:39:49 -0800

    ShouldThrowDependencyExceptionOnRetrieveWhenSqlExceptionOccursAndLogItAsync  -> PASS

---
+++ OtripleS.Web.Api.Tests.Unit/Services/CalendarEntries/CalendarEntryServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.CalendarEntries;
+using OtripleS.Web.Api.Models.CalendarEntries.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.CalendarEntries
+{
+    public partial class CalendarEntryServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid randomCalendarEntryId = Guid.NewGuid();
+            Guid inputCalendarEntryId = randomCalendarEntryId;
+            var sqlException = GetSqlException();
+
+            var expectedCalendarEntryDependencyException =
+                new CalendarEntryDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectCalendarEntryByIdAsync(inputCalendarEntryId))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<CalendarEntry> retrieveCalendarEntryByIdTask =
+                this.calendarEntryService.RetrieveCalendarEntryByIdAsync(inputCalendarEntryId);
+
+            // then
+            await Assert.ThrowsAsync<CalendarEntryDependencyException>(() =>
+                retrieveCalendarEntryByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedCalendarEntryDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCalendarEntryByIdAsync(inputCalendarEntryId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
