2cfa474ba0dbc7334fc6263ee4e3a85c0486fdc2
Jamshed Ahmedov <jamshed.akhmedov@altusnova.com>
2020-12-04 11:54:52 +0500

    Add Locked and NotFound CalendarEntry exceptions

---
+++ OtripleS.Web.Api/Models/CalendarEntries/Exceptions/LockedCalendarEntryException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace OtripleS.Web.Api.Models.CalendarEntries.Exceptions
+{
+    public class LockedCalendarEntryException : Exception
+    {
+        public LockedCalendarEntryException(Exception innerException)
+            : base("Locked calendar entry record exception, please try again later.", innerException) { }
+    }
+}
