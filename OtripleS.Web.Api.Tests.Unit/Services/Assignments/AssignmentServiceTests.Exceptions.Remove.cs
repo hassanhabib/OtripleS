566cabe0b9d816a3c463a030b4776cade9fe44d2
Viral Pandya <virav.pandya@gmail.com>
2020-12-07 21:13:16 -0800

    ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync -> PASS

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Assignments/AssignmentServiceTests.Exceptions.Remove.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using OtripleS.Web.Api.Models.Assignments;
+using OtripleS.Web.Api.Models.Assignments.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Assignments
+{
+    public partial class AssignmentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRemoveWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid randomAssignmentId = Guid.NewGuid();
+            Guid inputAssignmentId = randomAssignmentId;
+            SqlException sqlException = GetSqlException();
+
+            var expectedAssignmentDependencyException =
+                new AssignmentDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAssignmentByIdAsync(inputAssignmentId))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Assignment> deleteAssignmentTask =
+                this.assignmentService.RemoveAssignmentByIdAsync(inputAssignmentId);
+
+            // then
+            await Assert.ThrowsAsync<AssignmentDependencyException>(() =>
+                deleteAssignmentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedAssignmentDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAssignmentByIdAsync(inputAssignmentId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
