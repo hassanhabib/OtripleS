9371b41407c2afc8012a56ad3f5cfd65a0e2337f
Viral Pandya <virav.pandya@gmail.com>
2020-12-08 18:54:48 -0800

    ShouldModifyCalendarEntryAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/CalendarEntries/CalendarEntryServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/CalendarEntries/CalendarEntryServiceTests.Logic.cs
@@ -7,6 +7,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Force.DeepCloner;
 using Moq;
 using OtripleS.Web.Api.Models.CalendarEntries;
 using Xunit;
@@ -122,6 +123,56 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.CalendarEntries
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldModifyCalendarEntryAsync()
+        {
+            // given
+            DateTimeOffset randomDate = GetRandomDateTime();
+            DateTimeOffset randomInputDate = GetRandomDateTime();
+            CalendarEntry randomCalendarEntry = CreateRandomCalendarEntry(randomInputDate);
+            CalendarEntry inputCalendarEntry = randomCalendarEntry;
+            CalendarEntry afterUpdateStorageCalendarEntry = inputCalendarEntry;
+            CalendarEntry expectedCalendarEntry = afterUpdateStorageCalendarEntry;
+            CalendarEntry beforeUpdateStorageCalendarEntry = randomCalendarEntry.DeepClone();
+            inputCalendarEntry.UpdatedDate = randomDate;
+            Guid calendarEntryId = inputCalendarEntry.Id;
+
+            this.dateTimeBrokerMock.Setup(broker =>
+               broker.GetCurrentDateTime())
+                   .Returns(randomDate);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectCalendarEntryByIdAsync(calendarEntryId))
+                    .ReturnsAsync(beforeUpdateStorageCalendarEntry);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.UpdateCalendarEntryAsync(inputCalendarEntry))
+                    .ReturnsAsync(afterUpdateStorageCalendarEntry);
+
+            // when
+            CalendarEntry actualCalendarEntry =
+                await this.calendarEntryService.ModifyCalendarEntryAsync(inputCalendarEntry);
+
+            // then
+            actualCalendarEntry.Should().BeEquivalentTo(expectedCalendarEntry);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectCalendarEntryByIdAsync(calendarEntryId),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateCalendarEntryAsync(inputCalendarEntry),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
 
         [Fact]
