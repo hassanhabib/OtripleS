155a47742c6d3a7e746611a333671969ce211081
Hassan Habib <hassanhabib@live.com>
2020-12-14 07:33:55 -0800

    Attachment: - Model - Migrations - Storage Broker

--- OtripleS.Web.Api/Brokers/Storage/IStorageBroker.Attachments.cs
+++ OtripleS.Web.Api/Brokers/Storage/IStorageBroker.Attachments.cs
@@ -6,16 +6,16 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using OtripleS.Web.Api.Models.Attendances;
+using OtripleS.Web.Api.Models.Attachments;
 
 namespace OtripleS.Web.Api.Brokers.Storage
 {
     public partial interface IStorageBroker
     {
-        ValueTask<Attendance> InsertAttendanceAsync(Attendance attendance);
-        IQueryable<Attendance> SelectAllAttendances();
-        ValueTask<Attendance> SelectAttendanceByIdAsync(Guid attendanceId);
-        ValueTask<Attendance> UpdateAttendanceAsync(Attendance attendance);
-        ValueTask<Attendance> DeleteAttendanceAsync(Attendance attendance);
+        ValueTask<Attachment> InsertAttachmentAsync(Attachment attachment);
+        IQueryable<Attachment> SelectAllAttachments();
+        ValueTask<Attachment> SelectAttachmentByIdAsync(Guid attachmentId);
+        ValueTask<Attachment> UpdateAttachmentAsync(Attachment attachment);
+        ValueTask<Attachment> DeleteAttachmentAsync(Attachment attachment);
     }
 }
---
+++ OtripleS.Web.Api/Migrations/20201214153301_AddAttachment.cs
@@ -0,0 +1,43 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OtripleS.Web.Api.Migrations
+{
+    public partial class AddAttachment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Attachments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Label = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Contents = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    ContectType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Extension = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExternalUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attachments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attachments");
+        }
+    }
+}
