1afefb16286a4c1c87f35a7ef14b10da44d5e973
Viral Pandya <virav.pandya@gmail.com>
2020-12-15 19:54:20 -0800

    Common code for service tests

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Attachments/AttachmentServiceTests.cs
@@ -0,0 +1,56 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Moq;
+using OtripleS.Web.Api.Brokers.DateTimes;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.Attachments;
+using OtripleS.Web.Api.Services.Attachments;
+using Tynamix.ObjectFiller;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Attachments
+{
+    public partial class AttachmentServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly AttachmentService attachmentService;
+
+        public AttachmentServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+
+            this.attachmentService = new AttachmentService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private Attachment CreateRandomAttachment() =>
+            CreateAttachmentFiller(dates: DateTimeOffset.UtcNow).Create();
+
+        private Attachment CreateRandomAttachment(DateTimeOffset dates) =>
+            CreateAttachmentFiller(dates).Create();
+
+        private static Filler<Attachment> CreateAttachmentFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Attachment>();
+
+            filler.Setup()
+                .OnProperty(attachment => attachment.CreatedDate).Use(dates)
+                .OnProperty(attachment => attachment.UpdatedDate).Use(dates);
+
+            return filler;
+        }
+    }
+}
