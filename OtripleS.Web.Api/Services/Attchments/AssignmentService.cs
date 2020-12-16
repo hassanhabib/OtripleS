ca68e32524fc700933f487b51df9f3f8cd0bcd3d
Viral Pandya <virav.pandya@gmail.com>
2020-12-15 19:53:15 -0800

    Service Init

---
+++ OtripleS.Web.Api/Services/Attchments/AssignmentService.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Brokers.DateTimes;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.Attachments;
+
+namespace OtripleS.Web.Api.Services.Attachments
+{
+    public partial class AttachmentService : IAttachmentService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+
+        public AttachmentService(IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker,
+            IDateTimeBroker dateTimeBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+            this.dateTimeBroker = dateTimeBroker;
+        }
+
+        public ValueTask<Attachment> RetrieveAttachmentByIdAsync(Guid attachmentId) =>
+            throw new NotImplementedException();
+    }
+}
