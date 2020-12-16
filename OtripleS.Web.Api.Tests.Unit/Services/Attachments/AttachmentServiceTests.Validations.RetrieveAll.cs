4ec11c8adb40de459ec59ec3d7f8ba87aadb3dde
Stanislaw Nowak <stanislaw@nowakpost.com>
2020-12-17 00:42:24 +0100

    ShouldLogWarningOnRetrieveAllWhenAttachmentsWereEmptyAndLogIt => FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Attachments/AttachmentServiceTests.Validations.RetrieveAll.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.Attachments;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Attachments
+{
+    public partial class AttachmentServiceTests
+    {
+        [Fact]
+        public void ShouldLogWarningOnRetrieveAllWhenAttachmentsWereEmptyAndLogIt()
+        {
+            // given
+            IQueryable<Attachment> emptyStorageAttachments = new List<Attachment>().AsQueryable();
+            IQueryable<Attachment> expectedAttachments = emptyStorageAttachments;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllAttachments())
+                    .Returns(expectedAttachments);
+
+            // when
+            IQueryable<Attachment> actualAttachments =
+                this.attachmentService.RetrieveAllAttachments();
+
+            // then
+            actualAttachments.Should().BeEquivalentTo(emptyStorageAttachments);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllAttachments(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogWarning("No attachments found in storage."),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
