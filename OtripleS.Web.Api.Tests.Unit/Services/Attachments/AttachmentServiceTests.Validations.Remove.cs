4d7e2f5d4f9fa6b8861baa1283fbed823892e887
eris.adhami <eri.adhami@gmail.com>
2020-12-17 19:24:56 +0100

    ShouldThrowValidatonExceptionOnRemoveWhenIdIsInvalidAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Attachments/AttachmentServiceTests.Validations.Remove.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.Attachments;
+using OtripleS.Web.Api.Models.Attachments.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Attachments
+{
+    public partial class AttachmentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidatonExceptionOnRemoveWhenIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid randomAttachmentId = default;
+            Guid inputAttachmentId = randomAttachmentId;
+
+            var invalidAttachmentException = new InvalidAttachmentException(
+                parameterName: nameof(Attachment.Id),
+                parameterValue: inputAttachmentId);
+
+            var expectedAttachmentValidationException =
+                new AttachmentValidationException(invalidAttachmentException);
+
+            // when
+            ValueTask<Attachment> actualAttachmentTask =
+                this.attachmentService.RemoveAttachmentByIdAsync(inputAttachmentId);
+
+            // then
+            await Assert.ThrowsAsync<AttachmentValidationException>(() => actualAttachmentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedAttachmentValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAttachmentByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteAttachmentAsync(It.IsAny<Attachment>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
