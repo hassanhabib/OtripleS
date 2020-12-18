f34f04bb4d8d033318b4c1672ae4533fc851e02b
tobidub <github@tobiasdubral.de>
2020-12-19 00:12:26 +0100

    ShouldThrowValidationExceptionOnModifyWhenUpdatedDateIsInvalidAndLogItAsync -> PASS

--- OtripleS.Web.Api.Tests.Unit/Services/Attachments/AttachmentServiceTests.Validations.Modify.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Attachments/AttachmentServiceTests.Validations.Modify.cs
@@ -235,5 +235,42 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Attachments
 			this.loggingBrokerMock.VerifyNoOtherCalls();
 			this.storageBrokerMock.VerifyNoOtherCalls();
 		}
+
+		[Fact]
+		public async void ShouldThrowValidationExceptionOnModifyWhenUpdatedDateIsInvalidAndLogItAsync()
+		{
+			// given
+			DateTimeOffset dateTime = GetRandomDateTime();
+			Attachment randomAttachment = CreateRandomAttachment(dateTime);
+			Attachment inputAttachment = randomAttachment;
+			inputAttachment.UpdatedDate = default;
+
+			var invalidAttachmentInputException = new InvalidAttachmentException(
+				parameterName: nameof(Attachment.UpdatedDate),
+				parameterValue: inputAttachment.UpdatedDate);
+
+			var expectedAttachmentValidationException =
+				new AttachmentValidationException(invalidAttachmentInputException);
+
+			// when
+			ValueTask<Attachment> modifyAttachmentTask =
+				this.attachmentService.ModifyAttachmentAsync(inputAttachment);
+
+			// then
+			await Assert.ThrowsAsync<AttachmentValidationException>(() =>
+				modifyAttachmentTask.AsTask());
+
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogError(It.Is(SameExceptionAs(expectedAttachmentValidationException))),
+					Times.Once);
+
+			this.storageBrokerMock.Verify(broker =>
+				broker.SelectAttachmentByIdAsync(It.IsAny<Guid>()),
+					Times.Never);
+
+			this.dateTimeBrokerMock.VerifyNoOtherCalls();
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+			this.storageBrokerMock.VerifyNoOtherCalls();
+		}
 	}
 }
