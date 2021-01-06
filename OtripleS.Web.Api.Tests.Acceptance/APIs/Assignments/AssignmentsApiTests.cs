9629b52a71b6d67b3833a64b48d8e5323fa48960
Viral Pandya <virav.pandya@gmail.com>
2021-01-05 21:11:13 -0800

    Code Rub

--- OtripleS.Web.Api.Tests.Acceptance/APIs/Assignments/AssignmentsApiTests.cs
+++ OtripleS.Web.Api.Tests.Acceptance/APIs/Assignments/AssignmentsApiTests.cs
@@ -22,10 +22,7 @@ namespace OtripleS.Web.Api.Tests.Acceptance.APIs.Assignments
             this.otripleSApiBroker = otripleSApiBroker;
 
         private static int GetRandomNumber() => new IntRange(min: 2, max: 10).GetValue();
-
-        private IEnumerable<Assignment> GetRandomAssignments() =>
-            CreateRandomAssignmentFiller().Create(GetRandomNumber());
-
+     
         private Assignment CreateRandomAssignment() =>
             CreateRandomAssignmentFiller().Create();
 
