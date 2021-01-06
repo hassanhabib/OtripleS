ab1c77a51d13ae498f958025a9d41fa4ae7963fb
eris.adhami <eri.adhami@gmail.com>
2021-01-06 22:21:33 +0100

    ShouldThrowDependencyExceptionOnRetrieveWhenDbExceptionOccursAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/StudentAttachments/StudentAttachmentServiceTests.Exceptions.RetrievById.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentAttachments/StudentAttachmentServiceTests.Exceptions.RetrievById.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using OtripleS.Web.Api.Models.StudentAttachments;
 using OtripleS.Web.Api.Models.StudentAttachments.Exceptions;
@@ -43,6 +44,45 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.StudentAttachments
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(expectedStudentAttachmentDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectStudentAttachmentByIdAsync(inputStudentId, inputAttachmentId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveWhenDbExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid randomAttachmentId = Guid.NewGuid();
+            Guid randomStudentId = Guid.NewGuid();
+            Guid inputAttachmentId = randomAttachmentId;
+            Guid inputStudentId = randomStudentId;
+            var databaseUpdateException = new DbUpdateException();
+
+            var expectedStudentAttachmentDependencyException =
+                new StudentAttachmentDependencyException(databaseUpdateException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectStudentAttachmentByIdAsync(inputStudentId, inputAttachmentId))
+                    .ThrowsAsync(databaseUpdateException);
+
+            // when
+            ValueTask<StudentAttachment> deleteAttachmentTask =
+                this.studentAttachmentService.RetrieveStudentAttachmentByIdAsync
+                (inputStudentId, inputAttachmentId);
+
+            // then
+            await Assert.ThrowsAsync<StudentAttachmentDependencyException>(
+                () => deleteAttachmentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentAttachmentDependencyException))),
                     Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
