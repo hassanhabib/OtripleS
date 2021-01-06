1072b367ce2ff0675c3cffcc325b9a8d8e738c7d
eris.adhami <eri.adhami@gmail.com>
2021-01-06 21:34:14 +0100

    ShouldRetrieveStudentAttachmentById -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentAttachments/StudentAttachmentServiceTests.Logic.cs
@@ -0,0 +1,47 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.StudentAttachments;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.StudentAttachments
+{
+    public partial class StudentAttachmentServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveStudentAttachmentById()
+        {
+            // given
+            StudentAttachment randomStudentAttachment = CreateRandomStudentAttachment();
+            StudentAttachment storageStudentAttachment = randomStudentAttachment;
+            StudentAttachment expectedStudentAttachment = storageStudentAttachment;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectStudentAttachmentByIdAsync
+                (randomStudentAttachment.StudentId, randomStudentAttachment.AttachmentId))
+                    .Returns(new ValueTask<StudentAttachment>(randomStudentAttachment));
+
+            // when
+            StudentAttachment actualStudentAttachment = await
+                this.studentAttachmentService.RetrieveStudentAttachmentByIdAsync(
+                    randomStudentAttachment.StudentId, randomStudentAttachment.AttachmentId);
+
+            // then
+            actualStudentAttachment.Should().BeEquivalentTo(expectedStudentAttachment);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectStudentAttachmentByIdAsync
+                (randomStudentAttachment.StudentId, randomStudentAttachment.AttachmentId),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
