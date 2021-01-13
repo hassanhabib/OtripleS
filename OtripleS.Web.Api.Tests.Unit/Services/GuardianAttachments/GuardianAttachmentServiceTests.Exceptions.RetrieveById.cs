25e8a7a200e2912abfb5f3083c993e9596c76411
eris.adhami <eri.adhami@gmail.com>
2021-01-13 23:26:59 +0100

    ShouldThrowDependencyExceptionOnRetrieveWhenSqlExceptionOccursAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/GuardianAttachments/GuardianAttachmentServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,57 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Moq;
+using OtripleS.Web.Api.Models.GuardianAttachments;
+using OtripleS.Web.Api.Models.GuardianAttachments.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.GuardianAttachments
+{
+    public partial class GuardianAttachmentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveWhenSqlExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid randomAttachmentId = Guid.NewGuid();
+            Guid inputAttachmentId = randomAttachmentId;
+            Guid randomGuardianId = Guid.NewGuid();
+            Guid inputGuardianId = randomGuardianId;
+            SqlException sqlException = GetSqlException();
+
+            var expectedGuardianAttachmentDependencyException
+                = new GuardianAttachmentDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                 broker.SelectGuardianAttachmentByIdAsync(inputGuardianId, inputAttachmentId))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<GuardianAttachment> deleteGuardianAttachmentTask =
+                this.guardianAttachmentService.RetrieveGuardianAttachmentByIdAsync
+                (inputGuardianId, inputAttachmentId);
+
+            // then
+            await Assert.ThrowsAsync<GuardianAttachmentDependencyException>(() =>
+                deleteGuardianAttachmentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedGuardianAttachmentDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuardianAttachmentByIdAsync(inputGuardianId, inputAttachmentId),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ OtripleS.Web.Api/Models/GuardianAttachments/Exceptions/GuardianAttachmentDependencyException.cs
@@ -0,0 +1,14 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+namespace OtripleS.Web.Api.Models.GuardianAttachments.Exceptions
+{
+    public class GuardianAttachmentDependencyException : Exception
+    {
+        public GuardianAttachmentDependencyException(Exception innerException) :
+            base("Service dependency error occurred, contact support.", innerException) { }
+    }
+}
