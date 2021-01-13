4a4e887e94843258c202d2cfb80539dd0e26d721
eris.adhami <eri.adhami@gmail.com>
2021-01-13 22:49:01 +0100

    ShouldRetrieveGuardianAttachmentById -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/GuardianAttachments/GuardianAttachmentServiceTests.Logic.cs
@@ -0,0 +1,47 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.GuardianAttachments;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.GuardianAttachments
+{
+    public partial class GuardianAttachmentServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveGuardianAttachmentById()
+        {
+            // given
+            GuardianAttachment randomGuardianAttachment = CreateRandomGuardianAttachment();
+            GuardianAttachment storageGuardianAttachment = randomGuardianAttachment;
+            GuardianAttachment expectedGuardianAttachment = storageGuardianAttachment;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGuardianAttachmentByIdAsync
+                (randomGuardianAttachment.GuardianId, randomGuardianAttachment.AttachmentId))
+                    .Returns(new ValueTask<GuardianAttachment>(randomGuardianAttachment));
+
+            // when
+            GuardianAttachment actualGuardianAttachment = await
+                this.guardianAttachmentService.RetrieveGuardianAttachmentByIdAsync(
+                    randomGuardianAttachment.GuardianId, randomGuardianAttachment.AttachmentId);
+
+            // then
+            actualGuardianAttachment.Should().BeEquivalentTo(expectedGuardianAttachment);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuardianAttachmentByIdAsync
+                (randomGuardianAttachment.GuardianId, randomGuardianAttachment.AttachmentId),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
