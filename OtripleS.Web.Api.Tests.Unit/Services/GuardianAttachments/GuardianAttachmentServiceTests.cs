3988ab5e268538b7eade27e89cf39f9158e6c04f
eris.adhami <eri.adhami@gmail.com>
2021-01-13 23:01:40 +0100

    ShouldThrowValidatonExceptionOnRetrieveWhenGuardianIdIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/GuardianAttachments/GuardianAttachmentServiceTests.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/GuardianAttachments/GuardianAttachmentServiceTests.cs
@@ -4,6 +4,7 @@
 //----------------------------------------------------------------
 
 using System;
+using System.Linq.Expressions;
 using Moq;
 using OtripleS.Web.Api.Brokers.DateTimes;
 using OtripleS.Web.Api.Brokers.Loggings;
@@ -46,6 +47,13 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.GuardianAttachments
                 .OnProperty(guardianAttachment => guardianAttachment.Attachment).IgnoreIt();
 
             return filler;
+        }
+
+        private static Expression<Func<Exception, bool>> SameExceptionAs(Exception expectedException)
+        {
+            return actualException =>
+                expectedException.Message == actualException.Message
+                && expectedException.InnerException.Message == actualException.InnerException.Message;
         }
     }
 }
