469d4c3630b8cc3ccde72bbe0082253a7d90955c
Stanislaw Nowak <stanislaw@nowakpost.com>
2021-01-14 08:54:44 +0100

    ShouldThrowDependencyExceptionOnRetrieveAllGuardianAttachmentsWhenDbExceptionOccursAndLogIt => FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/GuardianAttachments/GuardianAttachmentServiceTests.Exceptions.RetrieveAll.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/GuardianAttachments/GuardianAttachmentServiceTests.Exceptions.RetrieveAll.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using OtripleS.Web.Api.Models.GuardianAttachments.Exceptions;
 using Xunit;
@@ -30,6 +31,36 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.GuardianAttachments
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(expectedGuardianAttachmentDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllGuardianAttachments(),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public void ShouldThrowDependencyExceptionOnRetrieveAllGuardianAttachmentsWhenDbExceptionOccursAndLogIt()
+        {
+            // given
+            var databaseUpdateException = new DbUpdateException();
+
+            var expectedGuardianAttachmentDependencyException =
+                new GuardianAttachmentDependencyException(databaseUpdateException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllGuardianAttachments())
+                    .Throws(databaseUpdateException);
+
+            // when . then
+            Assert.Throws<GuardianAttachmentDependencyException>(() =>
+                this.guardianAttachmentService.RetrieveAllGuardianAttachments());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedGuardianAttachmentDependencyException))),
                     Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
