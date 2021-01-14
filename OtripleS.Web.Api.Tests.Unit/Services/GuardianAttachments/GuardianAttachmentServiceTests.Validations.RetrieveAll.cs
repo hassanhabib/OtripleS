e75624726a111df9c3051c86b232f1552065cd0e
Stanislaw Nowak <stanislaw@nowakpost.com>
2021-01-14 08:40:01 +0100

    ShouldLogWarningOnRetrieveAllWhenGuardianAttachmentsWereEmptyAndLogIt => FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/GuardianAttachments/GuardianAttachmentServiceTests.Validations.RetrieveAll.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.GuardianAttachments;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.GuardianAttachments
+{
+    public partial class GuardianAttachmentServiceTests
+    {
+        [Fact]
+        public void ShouldLogWarningOnRetrieveAllWhenGuardianAttachmentsWereEmptyAndLogIt()
+        {
+            // given
+            IQueryable<GuardianAttachment> emptyStorageGuardianAttachments = new List<GuardianAttachment>().AsQueryable();
+            IQueryable<GuardianAttachment> expectedGuardianAttachments = emptyStorageGuardianAttachments;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllGuardianAttachments())
+                    .Returns(expectedGuardianAttachments);
+
+            // when
+            IQueryable<GuardianAttachment> actualGuardianAttachments =
+                this.guardianAttachmentService.RetrieveAllGuardianAttachments();
+
+            // then
+            actualGuardianAttachments.Should().BeEquivalentTo(emptyStorageGuardianAttachments);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllGuardianAttachments(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogWarning("No guardian attachments found in storage."),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
