b013e6bb32c1cf2f30adfbb435584f22e346bdcc
Viral Pandya <virav.pandya@gmail.com>
2021-01-14 20:21:09 -0800

    ShouldThrowValidationExceptionOnAddWhenGuardianAttachmentIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/GuardianAttachments/GuardianAttachmentServiceTests.Validations.Add.cs
@@ -0,0 +1,49 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
+using Moq;
+using OtripleS.Web.Api.Models.GuardianAttachments;
+using OtripleS.Web.Api.Models.GuardianAttachments.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.GuardianAttachments
+{
+    public partial class GuardianAttachmentServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddWhenGuardianAttachmentIsNullAndLogItAsync()
+        {
+            // given
+            GuardianAttachment randomGuardianAttachment = default;
+            GuardianAttachment nullGuardianAttachment = randomGuardianAttachment;
+            var nullGuardianAttachmentException = new NullGuardianAttachmentException();
+
+            var expectedGuardianAttachmentValidationException =
+                new GuardianAttachmentValidationException(nullGuardianAttachmentException);
+
+            // when
+            ValueTask<GuardianAttachment> addGuardianAttachmentTask =
+                this.guardianAttachmentService.AddGuardianAttachmentAsync(nullGuardianAttachment);
+
+            // then
+            await Assert.ThrowsAsync<GuardianAttachmentValidationException>(() =>
+                addGuardianAttachmentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedGuardianAttachmentValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertGuardianAttachmentAsync(It.IsAny<GuardianAttachment>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
