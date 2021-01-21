0ecf5fb3b1781e7bc6adb93b335e674a8c23079b
Viral Pandya <virav.pandya@gmail.com>
2021-01-20 20:12:19 -0800

    ShouldThrowValidatonExceptionOnRemoveWhenTeacherIdIsInvalidAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/TeacherAttachments/TeacherAttachmentServiceTests.Validations.Remove.cs
@@ -0,0 +1,59 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.TeacherAttachments;
+using OtripleS.Web.Api.Models.TeacherAttachments.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.TeacherAttachments
+{
+    public partial class TeacherAttachmentServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidatonExceptionOnRemoveWhenTeacherIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid randomAttachmentId = Guid.NewGuid();
+            Guid randomTeacherId = default;
+            Guid inputAttachmentId = randomAttachmentId;
+            Guid inputTeacherId = randomTeacherId;
+
+            var invalidTeacherAttachmentInputException = new InvalidTeacherAttachmentException(
+                parameterName: nameof(TeacherAttachment.TeacherId),
+                parameterValue: inputTeacherId);
+
+            var expectedTeacherAttachmentValidationException =
+                new TeacherAttachmentValidationException(invalidTeacherAttachmentInputException);
+
+            // when
+            ValueTask<TeacherAttachment> removeTeacherAttachmentTask =
+                this.teacherAttachmentService.RemoveTeacherAttachmentByIdAsync(inputTeacherId, inputAttachmentId);
+
+            // then
+            await Assert.ThrowsAsync<TeacherAttachmentValidationException>(() => removeTeacherAttachmentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedTeacherAttachmentValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTeacherAttachmentByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteTeacherAttachmentAsync(It.IsAny<TeacherAttachment>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        
+    }
+}
