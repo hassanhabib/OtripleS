2497d25fcbb7d3f5a565305d5257ecda69e86a59
Viral Pandya <virav.pandya@gmail.com>
2021-01-20 19:58:42 -0800

    ShouldThrowDependencyExceptionOnRetrieveAllTeacherAttachmentsWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/TeacherAttachments/TeacherAttachmentServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,46 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using OtripleS.Web.Api.Models.TeacherAttachments.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.TeacherAttachments
+{
+    public partial class TeacherAttachmentServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDependencyExceptionOnRetrieveAllTeacherAttachmentsWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            var sqlException = GetSqlException();
+
+            var expectedTeacherAttachmentDependencyException =
+                new TeacherAttachmentDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllTeacherAttachments())
+                    .Throws(sqlException);
+
+            // when . then
+            Assert.Throws<TeacherAttachmentDependencyException>(() =>
+                this.teacherAttachmentService.RetrieveAllTeacherAttachments());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedTeacherAttachmentDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllTeacherAttachments(),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }        
+    }
+}
