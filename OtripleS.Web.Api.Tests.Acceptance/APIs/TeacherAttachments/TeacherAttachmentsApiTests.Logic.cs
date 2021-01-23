60c441c50dba83591cc14e4f6e2ae8d2e41e5280
Viral Pandya <virav.pandya@gmail.com>
2021-01-22 21:50:06 -0800

    ShouldPostTeacherAttachmentAsync -> PASS

---
+++ OtripleS.Web.Api.Tests.Acceptance/APIs/TeacherAttachments/TeacherAttachmentsApiTests.Logic.cs
@@ -0,0 +1,39 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using OtripleS.Web.Api.Tests.Acceptance.Models.TeacherAttachments;
+using RESTFulSense.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Acceptance.APIs.TeacherAttachments
+{
+    public partial class TeacherAttachmentsApiTests
+    {
+        [Fact]
+        public async Task ShouldPostTeacherAttachmentAsync()
+        {
+            // given
+            TeacherAttachment randomTeacherAttachment = await CreateRandomTeacherAttachment();
+            TeacherAttachment inputTeacherAttachment = randomTeacherAttachment;
+            TeacherAttachment expectedTeacherAttachment = inputTeacherAttachment;
+
+            // when             
+            await this.otripleSApiBroker.PostTeacherAttachmentAsync(inputTeacherAttachment);
+
+            TeacherAttachment actualTeacherAttachment =
+                await this.otripleSApiBroker.GetTeacherAttachmentByIdsAsync(
+                    inputTeacherAttachment.TeacherId,
+                    inputTeacherAttachment.AttachmentId);
+
+            // then
+            actualTeacherAttachment.Should().BeEquivalentTo(expectedTeacherAttachment);
+            await DeleteTeacherAttachmentAsync(actualTeacherAttachment);
+        }
+    }
+}
