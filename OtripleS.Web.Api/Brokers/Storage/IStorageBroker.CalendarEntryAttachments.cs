42829abf754d1389a31401f9efb8079a504234f8
Viral Pandya <virav.pandya@gmail.com>
2021-01-26 20:24:08 -0800

    CalendarEntryAttachment broker files

---
+++ OtripleS.Web.Api/Brokers/Storage/IStorageBroker.CalendarEntryAttachments.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Models.CalendarEntryAttachments;
+
+namespace OtripleS.Web.Api.Brokers.Storage
+{
+    public partial interface IStorageBroker
+    {
+        public ValueTask<CalendarEntryAttachment> InsertCalendarEntryAttachmentAsync(
+           CalendarEntryAttachment calendarEntryAttachment);
+
+        public IQueryable<CalendarEntryAttachment> SelectAllCalendarEntryAttachments();
+
+        public ValueTask<CalendarEntryAttachment> SelectCalendarEntryAttachmentByIdAsync(
+            Guid calendarEntryId,
+            Guid attachmentId);
+
+        public ValueTask<CalendarEntryAttachment> UpdateCalendarEntryAttachmentAsync(
+            CalendarEntryAttachment calendarEntryAttachment);
+
+        public ValueTask<CalendarEntryAttachment> DeleteCalendarEntryAttachmentAsync(
+            CalendarEntryAttachment calendarEntryAttachment);
+    }
+}
