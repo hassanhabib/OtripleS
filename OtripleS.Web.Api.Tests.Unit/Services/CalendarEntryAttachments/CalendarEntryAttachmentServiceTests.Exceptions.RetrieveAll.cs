ae01753c621c7c9191eca8544362cb6c8a01cf2d
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-01-29 14:43:45 +0500

    ShouldThrowDependencyExceptionOnRetrieveAllCalendarEntryAttachmentsWhenSqlExceptionOccursAndLogIt  =>  FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/CalendarEntryAttachments/CalendarEntryAttachmentServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,49 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using OtripleS.Web.Api.Models.CalendarEntryAttachments;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.CalendarEntryAttachments
+{
+    public partial class CalendarEntryAttachmentServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDependencyExceptionOnRetrieveAllCalendarEntryAttachmentsWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            var sqlException = GetSqlException();
+
+            var expectedCalendarEntryAttachmentDependencyException =
+                new CalendarEntryAttachmentDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                    broker.SelectAllCalendarEntryAttachments())
+                .Throws(sqlException);
+
+            // when . then
+            Assert.Throws<CalendarEntryAttachmentDependencyException>(() =>
+                this.calendarEntryAttachmentService.RetrieveAllCalendarEntryAttachments());
+
+            this.loggingBrokerMock.Verify(broker =>
+                    broker.LogCritical(It.Is(SameExceptionAs(
+                        expectedCalendarEntryAttachmentDependencyException))),
+                Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                    broker.SelectAllCalendarEntryAttachments(),
+                Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
