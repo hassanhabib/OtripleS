d3b0fb63b83de4a278fc64beb07e49e8620c8d0d
Viral Pandya <virav.pandya@gmail.com>
2021-01-30 21:37:39 -0800

    ShouldThrowDependencyExceptionOnRetrieveAllCalendarEntryAttachmentsWhenSqlExceptionOccursAndLogIt -> PASS

---
+++ OtripleS.Web.Api.Tests.Unit/Services/CalendarEntryAttachments/CalendarEntryAttachmentServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,48 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using OtripleS.Web.Api.Models.CalendarEntryAttachments.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.CalendarEntryAttachments
+{
+    public partial class CalendarEntryAttachmentServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDependencyExceptionOnRetrieveAllCalendarEntryAttachmentsWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            var sqlException = GetSqlException();
+            
+            var expectedCalendarEntryAttachmentDependencyException = 
+                new CalendarEntryAttachmentDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllCalendarEntryAttachments())
+                    .Throws(sqlException);
+
+            // when . then
+            Assert.Throws<CalendarEntryAttachmentDependencyException>(() =>
+                this.calendarEntryAttachmentService.RetrieveAllCalendarEntryAttachments());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedCalendarEntryAttachmentDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllCalendarEntryAttachments(),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        
+    }
+}
