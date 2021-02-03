473960c719a22a8d736ed5f02939d2ec77f22041
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-02-03 10:46:26 +0500

    Model files added

---
+++ OtripleS.Web.Api/Models/CourseAttachments/CourseAttachment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OtripleS.Web.Api.Models.Attachments;
+using OtripleS.Web.Api.Models.Courses;
+
+namespace OtripleS.Web.Api.Models.CourseAttachments
+{
+    public class CourseAttachment
+    {
+        public Guid CourseId { get; set; }
+        public Course Course { get; set; }
+        public Guid AttachmentId { get; set; }
+        public Attachment Attachment { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
