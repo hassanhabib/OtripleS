eacc59db05a3128eecbfa816b39a45c12b659a6b
Stanislaw Nowak <stanislaw@nowakpost.com>
2021-02-04 03:42:59 +0100

    Code clean up

--- OtripleS.Web.Api.Tests.Unit/Services/CourseAttachments/CourseAttachmentServiceTests.Validations.RetrieveAll.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/CourseAttachments/CourseAttachmentServiceTests.Validations.RetrieveAll.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FluentAssertions;
-using Microsoft.EntityFrameworkCore;
 using Moq;
 using OtripleS.Web.Api.Models.CourseAttachments;
-using OtripleS.Web.Api.Models.CourseAttachments.Exceptions;
 using Xunit;
 
 namespace OtripleS.Web.Api.Tests.Unit.Services.CourseAttachments
