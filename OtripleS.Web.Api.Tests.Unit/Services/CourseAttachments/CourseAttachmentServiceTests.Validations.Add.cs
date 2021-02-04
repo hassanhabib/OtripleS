b7e4f27a448a59a12ee0893a460cd254d2767f01
Stanislaw Nowak <stanislaw@nowakpost.com>
2021-02-04 02:37:10 +0100

    ShouldThrowValidationExceptionOnAddWhenCourseAttachmentAlreadyExistsAndLogItAsync => FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/CourseAttachments/CourseAttachmentServiceTests.Validations.Add.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/CourseAttachments/CourseAttachmentServiceTests.Validations.Add.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using Moq;
 using OtripleS.Web.Api.Models.CourseAttachments;
 using OtripleS.Web.Api.Models.CourseAttachments.Exceptions;
@@ -109,6 +110,47 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.CourseAttachments
             this.storageBrokerMock.Verify(broker =>
                 broker.InsertCourseAttachmentAsync(It.IsAny<CourseAttachment>()),
                     Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddWhenCourseAttachmentAlreadyExistsAndLogItAsync()
+        {
+            // given
+            CourseAttachment randomCourseAttachment = CreateRandomCourseAttachment();
+            CourseAttachment alreadyExistsCourseAttachment = randomCourseAttachment;
+            string randomMessage = GetRandomMessage();
+            string exceptionMessage = randomMessage;
+            var duplicateKeyException = new DuplicateKeyException(exceptionMessage);
+
+            var alreadyExistsCourseAttachmentException =
+                new AlreadyExistsCourseAttachmentException(duplicateKeyException);
+
+            var expectedCourseAttachmentValidationException =
+                new CourseAttachmentValidationException(alreadyExistsCourseAttachmentException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertCourseAttachmentAsync(alreadyExistsCourseAttachment))
+                    .ThrowsAsync(duplicateKeyException);
+
+            // when
+            ValueTask<CourseAttachment> addCourseAttachmentTask =
+                this.courseAttachmentService.AddCourseAttachmentAsync(alreadyExistsCourseAttachment);
+
+            // then
+            await Assert.ThrowsAsync<CourseAttachmentValidationException>(() =>
+                addCourseAttachmentTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertCourseAttachmentAsync(It.IsAny<CourseAttachment>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(expectedCourseAttachmentValidationException))),
+                    Times.Once);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
---
+++ OtripleS.Web.Api/Models/CourseAttachments/Exceptions/AlreadyExistsCourseAttachmentException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtripleS.Web.Api.Models.CourseAttachments.Exceptions
+{
+    public class AlreadyExistsCourseAttachmentException : Exception
+    {
+        public AlreadyExistsCourseAttachmentException(Exception innerException)
+            : base("Course attachment with the same id already exists.", innerException) { }
+    }
+}
