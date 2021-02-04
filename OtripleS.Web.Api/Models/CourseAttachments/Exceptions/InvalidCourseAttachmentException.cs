553485d3e59577aebb004f7b75cd3f9baa46b5df
Stanislaw Nowak <stanislaw@nowakpost.com>
2021-02-04 02:25:37 +0100

    ShouldThrowValidationExceptionOnAddWhenCourseIdIsInvalidAndLogItAsync => FAIL

---
+++ OtripleS.Web.Api/Models/CourseAttachments/Exceptions/InvalidCourseAttachmentException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtripleS.Web.Api.Models.CourseAttachments.Exceptions
+{
+    public class InvalidCourseAttachmentException : Exception
+    {
+        public InvalidCourseAttachmentException(string parameterName, object parameterValue)
+            : base($"Invalid course attachment, " +
+                  $"ParameterName: {parameterName}, " +
+                  $"ParameterValue: {parameterValue}.")
+        { }
+    }
+}
