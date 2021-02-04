5eff58f6319beaa282c96650218f5baa49dc77d8
Viral Pandya <virav.pandya@gmail.com>
2021-02-03 17:06:15 -0800

    ShouldThrowValidatonExceptionOnRetrieveWhenCourseIdIsInvalidAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api/Models/CourseAttachments/Exceptions/InvalidCourseAttachmentException.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+
+namespace OtripleS.Web.Api.Models.CourseAttachments.Exceptions
+{
+    public class InvalidCourseAttachmentException : Exception
+    {
+        public InvalidCourseAttachmentException(string parameterName, object parameterValue)
+            : base($"Invalid CourseAttachment, " +
+                  $"ParameterName: {parameterName}, " +
+                  $"ParameterValue: {parameterValue}.")
+        { }
+    }
+}
