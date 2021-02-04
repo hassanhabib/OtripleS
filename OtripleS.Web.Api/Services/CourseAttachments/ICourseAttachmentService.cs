4d3a7e2799487760d55d4c22bf3f600e9306899b
Viral Pandya <virav.pandya@gmail.com>
2021-02-03 17:01:07 -0800

    ShouldRetrieveCourseAttachmentById -> FAIL

---
+++ OtripleS.Web.Api/Services/CourseAttachments/ICourseAttachmentService.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Models.CourseAttachments;
+
+namespace OtripleS.Web.Api.Services.CourseAttachments
+{
+    public interface ICourseAttachmentService
+    {
+        ValueTask<CourseAttachment> RetrieveCourseAttachmentByIdAsync(
+            Guid courseId,
+            Guid attachmentId);
+    }
+}
