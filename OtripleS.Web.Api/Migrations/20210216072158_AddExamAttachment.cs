a8980dc764fdf93ac3e4728f054bfb1ed02905d9
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-02-16 12:22:47 +0500

    ExamAttachment migration files added

---
+++ OtripleS.Web.Api/Migrations/20210216072158_AddExamAttachment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OtripleS.Web.Api.Migrations
+{
+    public partial class AddExamAttachment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExamAttachments",
+                columns: table => new
+                {
+                    ExamId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AttachmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExamAttachments", x => new { x.ExamId, x.AttachmentId });
+                    table.ForeignKey(
+                        name: "FK_ExamAttachments_Attachments_AttachmentId",
+                        column: x => x.AttachmentId,
+                        principalTable: "Attachments",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ExamAttachments_Exams_ExamId",
+                        column: x => x.ExamId,
+                        principalTable: "Exams",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExamAttachments_AttachmentId",
+                table: "ExamAttachments",
+                column: "AttachmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExamAttachments");
+        }
+    }
+}
