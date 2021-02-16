383d5064ecc99bf65a74396aa7f12839abbaa4fe
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-02-16 12:13:31 +0500

    ExamAttachment broker files

---
+++ OtripleS.Web.Api/Brokers/Storage/StorageBroker.ExamAttachments.References.cs
@@ -0,0 +1,32 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+using OtripleS.Web.Api.Models.ExamAttachments;
+
+namespace OtripleS.Web.Api.Brokers.Storage
+{
+    public partial class StorageBroker
+    {
+        private void AddExamAttachmentReferences(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ExamAttachment>()
+                .HasKey(examAttachment =>
+                    new { examAttachment.ExamId, examAttachment.AttachmentId });
+
+            modelBuilder.Entity<ExamAttachment>()
+                .HasOne(examAttachment => examAttachment.Attachment)
+                .WithMany(attachment => attachment.ExamAttachments)
+                .HasForeignKey(examAttachment => examAttachment.AttachmentId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<ExamAttachment>()
+                .HasOne(examAttachment => examAttachment.Exam)
+                .WithMany(exam => exam.ExamAttachments)
+                .HasForeignKey(examAttachment => examAttachment.ExamId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
