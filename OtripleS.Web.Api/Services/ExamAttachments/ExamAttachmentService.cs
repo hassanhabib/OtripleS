9598688de12f7354254f645d1dc7f1ec1b7195e6
Alagbala Damilola <zionlloyd05@gmail.com>
2021-02-18 20:35:29 +0100

    ShouldRemoveExamAttachmentAsync -> FAIL

---
+++ OtripleS.Web.Api/Services/ExamAttachments/ExamAttachmentService.cs
@@ -0,0 +1,38 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Brokers.DateTimes;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.ExamAttachments;
+
+namespace OtripleS.Web.Api.Services.ExamAttachments
+{
+    public class ExamAttachmentService : IExamAttachmentService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+
+        public ExamAttachmentService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker,
+            IDateTimeBroker dateTimeBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+            this.dateTimeBroker = dateTimeBroker;
+        }
+
+        public ValueTask<ExamAttachment> RemoveExamAttachmentByIdAsync(Guid examId, Guid attachmentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OtripleS.Web.Api/Services/ExamAttachments/IExamAttachmentService.cs
@@ -0,0 +1,20 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Models.ExamAttachments;
+
+namespace OtripleS.Web.Api.Services.ExamAttachments
+{
+    public interface IExamAttachmentService
+    {
+        ValueTask<ExamAttachment> RemoveExamAttachmentByIdAsync(
+          Guid examId,
+          Guid attachmentId);
+    }
+}
