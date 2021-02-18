1b077a840d80289ea8300d7f90f01d949d1ce732
Alagbala Damilola <zionlloyd05@gmail.com>
2021-02-18 22:01:44 +0100

    ShouldThrowDependencyExceptionOnRemoveWhenDbUpdateConcurrencyExceptionOccursAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/ExamAttachments/ExamAttachmentServiceTests.Exceptions.Remove.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/ExamAttachments/ExamAttachmentServiceTests.Exceptions.Remove.cs
@@ -102,6 +102,48 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.ExamAttachments
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
 
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRemoveWhenDbUpdateConcurrencyExceptionOccursAndLogItAsync()
+        {
+            // given
+            Guid someAttachmentId = Guid.NewGuid();
+            Guid someExamId = Guid.NewGuid();
+            var databaseUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedAttachmentException =
+                new LockedExamAttachmentException(databaseUpdateConcurrencyException);
+
+            var expectedExamAttachmentException =
+                new ExamAttachmentDependencyException(lockedAttachmentException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectExamAttachmentByIdAsync(someExamId, someAttachmentId))
+                    .ThrowsAsync(databaseUpdateConcurrencyException);
+
+            // when
+            ValueTask<ExamAttachment> removeExamAttachmentTask =
+                this.ExamAttachmentService.RemoveExamAttachmentByIdAsync(someExamId, someAttachmentId);
+
+            // then
+            await Assert.ThrowsAsync<ExamAttachmentDependencyException>(() =>
+                removeExamAttachmentTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectExamAttachmentByIdAsync(someExamId, someAttachmentId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedExamAttachmentException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteExamAttachmentAsync(It.IsAny<ExamAttachment>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
 
     }
 }
