f5365186297961a493098db596a793bd02252e16
Viral Pandya <virav.pandya@gmail.com>
2021-02-18 23:11:08 -0800

    ShouldThrowValidatonExceptionOnRetrieveWhenAttachmentIdIsInvalidAndLogItAsync -> PASS

--- OtripleS.Web.Api.Tests.Unit/Services/ExamAttachments/ExamAttachmentServiceTests.Validations.RetrieveById.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/ExamAttachments/ExamAttachmentServiceTests.Validations.RetrieveById.cs
@@ -53,6 +53,43 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.ExamAttachments
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
 
-        
+        [Fact]
+        public async Task ShouldThrowValidatonExceptionOnRetrieveWhenAttachmentIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid randomAttachmentId = default;
+            Guid randomExamId = Guid.NewGuid();
+            Guid invalidAttachmentId = randomAttachmentId;
+            Guid inputExamId = randomExamId;
+
+            var invalidExamAttachmentInputException = new InvalidExamAttachmentException(
+                parameterName: nameof(ExamAttachment.AttachmentId),
+                parameterValue: invalidAttachmentId);
+
+            var expectedExamAttachmentValidationException =
+                new ExamAttachmentValidationException(invalidExamAttachmentInputException);
+
+            // when
+            ValueTask<ExamAttachment> retrieveExamAttachmentTask =
+                this.examAttachmentService.RetrieveExamAttachmentByIdAsync(
+                    inputExamId,
+                    invalidAttachmentId);
+
+            // then
+            await Assert.ThrowsAsync<ExamAttachmentValidationException>(() =>
+                retrieveExamAttachmentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedExamAttachmentValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectExamAttachmentByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
