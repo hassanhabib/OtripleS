70d9a0233bfcbc267e68b0b6c1e16eb43ff3f410
Viral Pandya <virav.pandya@gmail.com>
2021-02-18 23:13:02 -0800

    ShouldThrowValidationExceptionOnRetrieveWhenStorageExamAttachmentIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/ExamAttachments/ExamAttachmentServiceTests.Validations.RetrieveById.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/ExamAttachments/ExamAttachmentServiceTests.Validations.RetrieveById.cs
@@ -91,5 +91,47 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.ExamAttachments
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveWhenStorageExamAttachmentIsInvalidAndLogItAsync()
+        {
+            // given
+            ExamAttachment randomExamAttachment = CreateRandomExamAttachment();
+            Guid inputAttachmentId = randomExamAttachment.AttachmentId;
+            Guid inputExamId = randomExamAttachment.ExamId;
+            ExamAttachment nullStorageExamAttachment = null;
+
+            var notFoundExamAttachmentException =
+                new NotFoundExamAttachmentException(inputExamId, inputAttachmentId);
+
+            var expectedAttachmentValidationException =
+                new ExamAttachmentValidationException(notFoundExamAttachmentException);
+
+            this.storageBrokerMock.Setup(broker =>
+                 broker.SelectExamAttachmentByIdAsync(inputExamId, inputAttachmentId))
+                    .ReturnsAsync(nullStorageExamAttachment);
+
+            // when
+            ValueTask<ExamAttachment> retrieveExamAttachmentTask =
+                this.examAttachmentService.RetrieveExamAttachmentByIdAsync(
+                    inputExamId,
+                    inputAttachmentId);
+
+            // then
+            await Assert.ThrowsAsync<ExamAttachmentValidationException>(() =>
+                retrieveExamAttachmentTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectExamAttachmentByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedAttachmentValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ OtripleS.Web.Api/Models/ExamAttachments/Exceptions/NotFoundExamAttachmentException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+
+namespace OtripleS.Web.Api.Models.ExamAttachments.Exceptions
+{
+    public class NotFoundExamAttachmentException : Exception
+    {
+        public NotFoundExamAttachmentException(Guid guardianId, Guid attachmentId)
+           : base($"Couldn't find ExamAttachment with examId: {guardianId} " +
+                  $"and attachmentId: {attachmentId}.")
+        { }
+    }
+}
