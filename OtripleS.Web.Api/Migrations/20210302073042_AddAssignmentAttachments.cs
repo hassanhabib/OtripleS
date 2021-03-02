6b23d5bde304620d25d8ecc99da6a9f2e8cb6764
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-03-02 12:31:59 +0500

    AssignmentAttachment migration files added

---
+++ OtripleS.Web.Api/Migrations/20210302073042_AddAssignmentAttachments.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OtripleS.Web.Api.Migrations
+{
+    public partial class AddAssignmentAttachments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AssignmentAttachments",
+                columns: table => new
+                {
+                    AssignmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AttachmentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssignmentAttachments", x => new { x.AssignmentId, x.AttachmentId });
+                    table.ForeignKey(
+                        name: "FK_AssignmentAttachments_Assignments_AttachmentId",
+                        column: x => x.AttachmentId,
+                        principalTable: "Assignments",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_AssignmentAttachments_Attachments_AttachmentId",
+                        column: x => x.AttachmentId,
+                        principalTable: "Attachments",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssignmentAttachments_AttachmentId",
+                table: "AssignmentAttachments",
+                column: "AttachmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AssignmentAttachments");
+        }
+    }
+}
