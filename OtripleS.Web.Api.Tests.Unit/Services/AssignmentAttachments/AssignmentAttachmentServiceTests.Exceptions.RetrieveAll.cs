7cd12577d34d00a77ee4dd58ee71ea8d128808cc
Stanislaw Nowak <stanislaw@nowakpost.com>
2021-03-04 08:59:15 +0100

    ShouldThrowDependencyExceptionOnRetrieveAllAssignmentAttachmentsWhenSqlExceptionOccursAndLogIt => FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/AssignmentAttachments/AssignmentAttachmentServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using OtripleS.Web.Api.Models.AssignmentAttachments.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.AssignmentAttachments
+{
+    public partial class AssignmentAttachmentServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDependencyExceptionOnRetrieveAllAssignmentAttachmentsWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            var sqlException = GetSqlException();
+
+            var expectedAssignmentAttachmentDependencyException =
+                new AssignmentAttachmentDependencyException(sqlException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllAssignmentAttachments())
+                    .Throws(sqlException);
+
+            // when . then
+            Assert.Throws<AssignmentAttachmentDependencyException>(() =>
+                this.assignmentAttachmentService.RetrieveAllAssignmentAttachments());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllAssignmentAttachments(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(expectedAssignmentAttachmentDependencyException))),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
