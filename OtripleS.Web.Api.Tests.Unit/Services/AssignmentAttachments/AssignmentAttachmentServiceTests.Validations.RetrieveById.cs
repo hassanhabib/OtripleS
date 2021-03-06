c364fed486264587a12e8102d373489416a12218
Viral Pandya <virav.pandya@gmail.com>
2021-03-05 17:01:43 -0800

    ShouldThrowValidatonExceptionOnRetrieveWhenAttachmentIdIsInvalidAndLogItAsync => PASS

--- OtripleS.Web.Api.Tests.Unit/Services/AssignmentAttachments/AssignmentAttachmentServiceTests.Validations.RetrieveById.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/AssignmentAttachments/AssignmentAttachmentServiceTests.Validations.RetrieveById.cs
@@ -51,6 +51,45 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.AssignmentAttachments
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }        
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidatonExceptionOnRetrieveWhenAttachmentIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid randomAttachmentId = default;
+            Guid randomAssignmentId = Guid.NewGuid();
+            Guid invalidAttachmentId = randomAttachmentId;
+            Guid inputAssignmentId = randomAssignmentId;
+
+            var invalidAssignmentAttachmentInputException = new InvalidAssignmentAttachmentException(
+                parameterName: nameof(AssignmentAttachment.AttachmentId),
+                parameterValue: invalidAttachmentId);
+
+            var expectedAssignmentAttachmentValidationException =
+                new AssignmentAttachmentValidationException(invalidAssignmentAttachmentInputException);
+
+            // when
+            ValueTask<AssignmentAttachment> retrieveAssignmentAttachmentTask =
+                this.assignmentAttachmentService.RetrieveAssignmentAttachmentByIdAsync(
+                    inputAssignmentId,
+                    invalidAttachmentId);
+
+            // then
+            await Assert.ThrowsAsync<AssignmentAttachmentValidationException>(() =>
+                retrieveAssignmentAttachmentTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedAssignmentAttachmentValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAssignmentAttachmentByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
