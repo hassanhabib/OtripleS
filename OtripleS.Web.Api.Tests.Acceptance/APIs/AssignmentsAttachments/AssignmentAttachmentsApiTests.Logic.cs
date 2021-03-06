60d3368785935e6c8b3cc0ec188dee77c0381648
Viral Pandya <virav.pandya@gmail.com>
2021-03-06 11:48:30 -0800

    ShouldPostAssignmentAttachmentAsync -> PASS

---
+++ OtripleS.Web.Api.Tests.Acceptance/APIs/AssignmentsAttachments/AssignmentAttachmentsApiTests.Logic.cs
@@ -0,0 +1,42 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using OtripleS.Web.Api.Tests.Acceptance.Models.AssignmentsAttachments;
+using RESTFulSense.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Acceptance.APIs.AssignmentsAttachments
+{
+    public partial class AssignmentAttachmentsApiTests
+    {
+        [Fact]
+        public async Task ShouldPostAssignmentAttachmentAsync()
+        {
+            // given
+            AssignmentAttachment randomAssignmentAttachment = await CreateRandomAssignmentAttachment();
+            AssignmentAttachment inputAssignmentAttachment = randomAssignmentAttachment;
+            AssignmentAttachment expectedAssignmentAttachment = inputAssignmentAttachment;
+
+            // when             
+            AssignmentAttachment actualAssignmentAttachment =
+                await this.otripleSApiBroker.PostAssignmentAttachmentAsync(inputAssignmentAttachment);
+
+            AssignmentAttachment retrievedAssignmentAttachment =
+                await this.otripleSApiBroker.GetAssignmentAttachmentByIdsAsync(
+                    inputAssignmentAttachment.AssignmentId,
+                    inputAssignmentAttachment.AttachmentId);
+
+            // then
+            actualAssignmentAttachment.Should().BeEquivalentTo(expectedAssignmentAttachment);
+            retrievedAssignmentAttachment.Should().BeEquivalentTo(expectedAssignmentAttachment);
+            await DeleteAssignmentAttachmentAsync(actualAssignmentAttachment);
+        }
+
+    }
+}
