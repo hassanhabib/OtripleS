3ec9381b39b040ed3550173b5cb380234e52cb69
eris.adhami <eri.adhami@gmail.com>
2021-03-24 14:45:14 +0100

    ShouldThrowValidationExceptionOnAddWhenFeeIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Fees/FeeServiceTests.Validations.Add.cs
@@ -0,0 +1,49 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.Fees;
+using OtripleS.Web.Api.Models.Fees.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Fees
+{
+    public partial class FeeServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddWhenFeeIsNullAndLogItAsync()
+        {
+            // given
+            Fee randomFee = default;
+            Fee nullFee = randomFee;
+            var nullFeeException = new NullFeeException();
+
+            var expectedFeeValidationException =
+                new FeeValidationException(nullFeeException);
+
+            // when
+            ValueTask<Fee> createFeeTask =
+                this.feeService.AddFeeAsync(nullFee);
+
+            // then
+            await Assert.ThrowsAsync<FeeValidationException>(() =>
+                createFeeTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedFeeValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertFeeAsync(It.IsAny<Fee>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
