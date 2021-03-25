bb48b1f44e86c9efa0daea8fc6d68b229b41700b
Stanislaw Nowak <stanislaw@nowakpost.com>
2021-03-25 07:53:29 +0100

    ShouldRetrieveAllFees => FAIL

---
+++ OtripleS.Web.Api/Services/Fees/FeeService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Brokers.DateTimes;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.Fees;
+
+namespace OtripleS.Web.Api.Services.Fees
+{
+    public partial class FeeService : IFeeService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+
+        public FeeService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker,
+            IDateTimeBroker dateTimeBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+            this.dateTimeBroker = dateTimeBroker;
+        }
+
+        public IQueryable<Fee> RetrieveAllFees()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OtripleS.Web.Api/Services/Fees/IFeeService.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using OtripleS.Web.Api.Models.Fees;
+
+namespace OtripleS.Web.Api.Services.Fees
+{
+    public interface IFeeService
+    {
+        IQueryable<Fee> RetrieveAllFees();
+    }
+}
