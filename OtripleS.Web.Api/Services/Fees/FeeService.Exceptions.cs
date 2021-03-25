7092604f440d55ba2bdb2e40bf8741b4aadc997d
eris.adhami <eri.adhami@gmail.com>
2021-03-25 12:56:38 +0100

    ShouldThrowServiceExceptionOnAddWhenExceptionOccursAndLogItAsync -> PASS

--- OtripleS.Web.Api/Services/Fees/FeeService.Exceptions.cs
+++ OtripleS.Web.Api/Services/Fees/FeeService.Exceptions.cs
@@ -46,6 +46,10 @@ namespace OtripleS.Web.Api.Services.Fees
             {
                 throw CreateAndLogDependencyException(dbUpdateException);
             }
+            catch (Exception exception)
+            {
+                throw CreateAndLogServiceException(exception);
+            }
         }
 
         private FeeValidationException CreateAndLogValidationException(Exception exception)
@@ -70,6 +74,14 @@ namespace OtripleS.Web.Api.Services.Fees
             this.loggingBroker.LogError(feeDependencyException);
 
             return feeDependencyException;
+        }
+
+        private FeeServiceException CreateAndLogServiceException(Exception exception)
+        {
+            var feeServiceException = new FeeServiceException(exception);
+            this.loggingBroker.LogError(feeServiceException);
+
+            return feeServiceException;
         }
     }
 }
