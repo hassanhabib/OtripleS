04ad03cc65c18b863c342896de45029802cba5c0
Stanislaw Nowak <stanislaw@nowakpost.com>
2021-03-25 08:05:40 +0100

    ShouldThrowDependencyExceptionOnRetrieveAllFeesWhenDbExceptionOccursAndLogIt => FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/Fees/FeeServiceTests.Exceptions.RetrieveAll.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Fees/FeeServiceTests.Exceptions.RetrieveAll.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using OtripleS.Web.Api.Models.Fees.Exceptions;
 using Xunit;
@@ -40,5 +41,36 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Fees
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public void ShouldThrowDependencyExceptionOnRetrieveAllFeesWhenDbExceptionOccursAndLogIt()
+        {
+            // given
+            var databaseUpdateException = new DbUpdateException();
+
+            var expectedAttachmentDependencyException =
+                new FeeDependencyException(databaseUpdateException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllFees())
+                    .Throws(databaseUpdateException);
+
+            // when . then
+            Assert.Throws<FeeDependencyException>(() =>
+                this.feeService.RetrieveAllFees());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllFees(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedAttachmentDependencyException))),
+                    Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
