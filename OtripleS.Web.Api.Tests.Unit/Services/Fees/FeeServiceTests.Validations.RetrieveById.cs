e303f0f5360d6aa3f6ee90284e984d4d1a1f3fba
Viral Pandya <virav.pandya@gmail.com>
2021-03-25 16:47:25 -0700

    ShouldThrowValidationExceptionOnRetrieveByIdWhenIdIsInvalidAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Fees/FeeServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,57 @@
+// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.Fees;
+using OtripleS.Web.Api.Models.Fees.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Fees
+{
+    public partial class FeeServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnRetrieveByIdWhenIdIsInvalidAndLogItAsync()
+        {
+            //given
+            Guid randomFeeId = default;
+            Guid inputFeeId = randomFeeId;
+
+            var invalidFeeInputException = new InvalidFeeInputException(
+                parameterName: nameof(Fee.Id),
+                parameterValue: inputFeeId);
+
+            var expectedFeeValidationException =
+                new FeeValidationException(invalidFeeInputException);
+
+            //when
+            ValueTask<Fee> retrieveFeeByIdTask =
+                this.feeService.RetrieveFeeByIdAsync(inputFeeId);
+
+            //then
+            await Assert.ThrowsAsync<FeeValidationException>(() => retrieveFeeByIdTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedFeeValidationException))),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker => 
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectFeeByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        
+    }
+}
