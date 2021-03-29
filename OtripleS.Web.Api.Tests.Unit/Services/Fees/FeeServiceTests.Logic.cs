32828e21659f20cf0a4cf86cb8963de9903316ca
Viral Pandya <virav.pandya@gmail.com>
2021-03-29 16:03:52 -0700

    ShouldModifyFeeAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/Fees/FeeServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Fees/FeeServiceTests.Logic.cs
@@ -11,6 +11,7 @@ using FluentAssertions;
 using Moq;
 using OtripleS.Web.Api.Models.Fees;
 using Xunit;
+using Force.DeepCloner;
 
 namespace OtripleS.Web.Api.Tests.Unit.Services.Fees
 {
@@ -56,6 +57,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Fees
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
 
+        [Fact]
         public void ShouldRetrieveAllFees()
         {
             // given
@@ -118,6 +120,58 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Fees
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldModifyFeeAsync()
+        {
+            // given
+            int randomNumber = GetRandomNumber();
+            int randomDays = randomNumber;
+            DateTimeOffset randomDate = GetRandomDateTime();
+            DateTimeOffset randomInputDate = GetRandomDateTime();
+            Fee randomFee = CreateRandomFee(randomInputDate);
+            Fee inputFee = randomFee;
+            Fee afterUpdateStorageFee = inputFee;
+            Fee expectedFee = afterUpdateStorageFee;
+            Fee beforeUpdateStorageFee = randomFee.DeepClone();
+            inputFee.UpdatedDate = randomDate;
+            Guid feeId = inputFee.Id;
+
+            this.dateTimeBrokerMock.Setup(broker =>
+               broker.GetCurrentDateTime())
+                   .Returns(randomDate);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectFeeByIdAsync(feeId))
+                    .ReturnsAsync(beforeUpdateStorageFee);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.UpdateFeeAsync(inputFee))
+                    .ReturnsAsync(afterUpdateStorageFee);
+
+            // when
+            Fee actualFee =
+                await this.feeService.ModifyFeeAsync(inputFee);
+
+            // then
+            actualFee.Should().BeEquivalentTo(expectedFee);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectFeeByIdAsync(feeId),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateFeeAsync(inputFee),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();            
         }
     }
 }
