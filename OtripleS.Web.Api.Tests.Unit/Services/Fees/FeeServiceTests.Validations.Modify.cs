bcea3ce02e6541e86fb87d17ee096e61d731bad8
Viral Pandya <virav.pandya@gmail.com>
2021-03-29 16:22:00 -0700

    ShouldThrowValidationExceptionOnModifyWhenFeeIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Fees/FeeServiceTests.Validations.Modify.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using Moq;
+using OtripleS.Web.Api.Models.Fees;
+using OtripleS.Web.Api.Models.Fees.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Fees
+{
+    public partial class FeeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyWhenFeeIsNullAndLogItAsync()
+        {
+            //given
+            Fee invalidFee = null;
+            var nullFeeException = new NullFeeException();
+
+            var expectedFeeValidationException =
+                new FeeValidationException(nullFeeException);
+
+            //when
+            ValueTask<Fee> modifyFeeTask =
+                this.feeService.ModifyFeeAsync(invalidFee);
+
+            //then
+            await Assert.ThrowsAsync<FeeValidationException>(() =>
+                modifyFeeTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedFeeValidationException))),
+                Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
