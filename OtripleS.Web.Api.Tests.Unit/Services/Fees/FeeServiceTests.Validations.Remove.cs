cc55af6e5be3b3b0a75db69c6f7378c2b0c2d10d
Viral Pandya <virav.pandya@gmail.com>
2021-03-30 07:17:54 -0700

    ShouldThrowValidatonExceptionOnDeleteWhenIdIsInvalidAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Fees/FeeServiceTests.Validations.Remove.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.Fees;
+using OtripleS.Web.Api.Models.Fees.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Fees
+{
+    public partial class FeeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidatonExceptionOnDeleteWhenIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid randomFeeId = default;
+            Guid inputFeeId = randomFeeId;
+
+            var invalidFeeInputException = new InvalidFeeException(
+                parameterName: nameof(Fee.Id),
+                parameterValue: inputFeeId);
+
+            var expectedFeeValidationException =
+                new FeeValidationException(invalidFeeInputException);
+
+            // when
+            ValueTask<Fee> actualFeeTask =
+                this.feeService.RemoveFeeAsync(inputFeeId);
+
+            // then
+            await Assert.ThrowsAsync<FeeValidationException>(() => actualFeeTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedFeeValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectFeeByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteFeeAsync(It.IsAny<Fee>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();            
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
