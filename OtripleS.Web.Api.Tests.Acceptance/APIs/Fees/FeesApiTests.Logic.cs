2104d4de30e2bf43c61bbd1fa9c5c31abcee0b3c
Viral Pandya <virav.pandya@gmail.com>
2021-03-31 20:11:55 -0700

    ShouldPostFeeAsync -> PASS

---
+++ OtripleS.Web.Api.Tests.Acceptance/APIs/Fees/FeesApiTests.Logic.cs
@@ -0,0 +1,39 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using OtripleS.Web.Api.Tests.Acceptance.Models.Fees;
+using RESTFulSense.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Acceptance.APIs.Fees
+{
+    public partial class FeesApiTests
+    {
+        [Fact]
+        public async Task ShouldPostFeeAsync()
+        {
+            // given
+            Fee randomFee = await CreateRandomFeeAsync();
+            Fee inputFee = randomFee;
+            Fee expectedFee = inputFee;
+
+            // when 
+            await this.otripleSApiBroker.PostFeeAsync(inputFee);
+
+            Fee actualFee =
+               await this.otripleSApiBroker.GetFeeByIdAsync(inputFee.Id);
+
+            // then
+            actualFee.Should().BeEquivalentTo(expectedFee);
+            await DeleteFeeAsync(actualFee);
+        }
+
+        
+    }
+}
