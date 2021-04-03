4013b35106f433e036c42c7250006b8f97c47825
Viral Pandya <virav.pandya@gmail.com>
2021-04-03 12:59:42 -0700

    Code Rub

--- OtripleS.Web.Api.Tests.Unit/Services/Assignments/AssignmentServiceTests.Exceptions.RetrieveAll.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Assignments/AssignmentServiceTests.Exceptions.RetrieveAll.cs
@@ -48,40 +48,6 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Assignments
         }
 
         [Fact]
-        public void ShouldThrowDependencyExceptionOnRetrieveAllWhenDbExceptionOccursAndLogIt()
-        {
-            // given
-            var databaseUpdateException = new DbUpdateException();
-
-            var expectedAssignmentDependencyException =
-                new AssignmentDependencyException(databaseUpdateException);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectAllAssignments())
-                    .Throws(databaseUpdateException);
-
-            // when . then
-            Assert.Throws<AssignmentDependencyException>(() =>
-                this.assignmentService.RetrieveAllAssignments());
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectAllAssignments(),
-                    Times.Once);
-
-            this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(expectedAssignmentDependencyException))),
-                    Times.Once);
-
-            this.dateTimeBrokerMock.Verify(broker =>
-                broker.GetCurrentDateTime(),
-                    Times.Never);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
         public void ShouldThrowServiceExceptionOnRetrieveAllWhenExceptionOccursAndLogIt()
         {
             // given
