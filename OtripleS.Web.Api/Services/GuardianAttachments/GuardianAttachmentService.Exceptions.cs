93bb50ba998ba55ecb1e8462414d1ba692f0cab2
eris.adhami <eri.adhami@gmail.com>
2021-04-10 13:29:48 +0200

    Code rub GuardianAttachment

--- OtripleS.Web.Api/Services/GuardianAttachments/GuardianAttachmentService.Exceptions.cs
+++ OtripleS.Web.Api/Services/GuardianAttachments/GuardianAttachmentService.Exceptions.cs
@@ -83,10 +83,6 @@ namespace OtripleS.Web.Api.Services.GuardianAttachments
             {
                 throw CreateAndLogCriticalDependencyException(sqlException);
             }
-            catch (DbUpdateException dbUpdateException)
-            {
-                throw CreateAndLogDependencyException(dbUpdateException);
-            }
             catch (Exception exception)
             {
                 throw CreateAndLogServiceException(exception);
