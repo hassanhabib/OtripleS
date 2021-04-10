ec8e91caadf04f0c6a76af2a3e7db2333e34963f
eris.adhami <eri.adhami@gmail.com>
2021-04-10 13:36:30 +0200

    Code rub StudentAttachment

--- OtripleS.Web.Api.Tests.Unit/Services/StudentAttachments/StudentAttachmentServiceTests.Exceptions.RetrieveAll.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentAttachments/StudentAttachmentServiceTests.Exceptions.RetrieveAll.cs
@@ -44,36 +44,6 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.StudentAttachments
         }
 
         [Fact]
-        public void ShouldThrowDependencyExceptionOnRetrieveAllStudentAttachmentsWhenDbExceptionOccursAndLogIt()
-        {
-            // given
-            var databaseUpdateException = new DbUpdateException();
-
-            var expectedAttachmentDependencyException =
-                new StudentAttachmentDependencyException(databaseUpdateException);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectAllStudentAttachments())
-                    .Throws(databaseUpdateException);
-
-            // when . then
-            Assert.Throws<StudentAttachmentDependencyException>(() =>
-                this.studentAttachmentService.RetrieveAllStudentAttachments());
-
-            this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(expectedAttachmentDependencyException))),
-                    Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectAllStudentAttachments(),
-                    Times.Once);
-
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
         public void ShouldThrowServiceExceptionOnRetrieveAllStudentAttachmentsWhenExceptionOccursAndLogIt()
         {
             // given
