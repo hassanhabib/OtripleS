7638770236056ee40c589774de48c47ec980b125
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-04-13 16:18:14 +0500

    Exam Fee Broker Files Added

---
+++ OtripleS.Web.Api/Brokers/Storage/IStorageBroker.ExamFees.cs
@@ -0,0 +1,21 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Models.ExamFees;
+
+namespace OtripleS.Web.Api.Brokers.Storage
+{
+    public partial interface IStorageBroker
+    {
+        public ValueTask<ExamFee> InsertExamFeeAsync(ExamFee examFee);
+        public IQueryable<ExamFee> SelectAllExamFees();
+        public ValueTask<ExamFee> SelectExamFeeByIdAsync(Guid id);
+        public ValueTask<ExamFee> UpdateExamFeeAsync(ExamFee examFee);
+        public ValueTask<ExamFee> DeleteExamFeeAsync(ExamFee examFee);
+    }
+}
---
+++ OtripleS.Web.Api/Brokers/Storage/StorageBroker.ExamFees.References.cs
@@ -0,0 +1,28 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+using OtripleS.Web.Api.Models.ExamFees;
+
+namespace OtripleS.Web.Api.Brokers.Storage
+{
+    public partial class StorageBroker
+    {
+        private void AddExamFeeReferences(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ExamFee>()
+                .HasOne(examFee => examFee.CreatedByUser)
+                .WithMany(examFeeCreatedByUser => examFeeCreatedByUser.ExamFeesUpdatedByUser)
+                .HasForeignKey(examfee => examfee.CreatedBy)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<ExamFee>()
+                .HasOne(examFee => examFee.UpdatedByUser)
+                .WithMany(examFeeUpdatedByUser => examFeeUpdatedByUser.ExamFeesUpdatedByUser)
+                .HasForeignKey(examfee => examfee.FeeId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
