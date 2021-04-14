53d8e46e0516a9b83014c9fbe3745440a5d82eea
eris.adhami <eri.adhami@gmail.com>
2021-04-14 17:08:43 +0200

    ShouldThrowValidationExceptionOnAddWhenExamIdIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/ExamFees/ExamFeeServiceTests.Validation.Add.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/ExamFees/ExamFeeServiceTests.Validation.Add.cs
@@ -43,5 +43,40 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.ExamFees
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddWhenExamIdIsInvalidAndLogItAsync()
+        {
+            // given
+            ExamFee randomExamFee = CreateRandomExamFee();
+            ExamFee inputExamFee = randomExamFee;
+            inputExamFee.ExamId = default;
+
+            var invalidExamFeeInputException = new InvalidExamFeeException(
+                parameterName: nameof(ExamFee.ExamId),
+                parameterValue: inputExamFee.ExamId);
+
+            var expectedExamFeeValidationException =
+                new ExamFeeValidationException(invalidExamFeeInputException);
+
+            // when
+            ValueTask<ExamFee> addExamFeeTask =
+                this.examFeeService.AddExamFeeAsync(inputExamFee);
+
+            // then
+            await Assert.ThrowsAsync<ExamFeeValidationException>(() =>
+                addExamFeeTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedExamFeeValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertExamFeeAsync(It.IsAny<ExamFee>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
