458ae550f03728e0eb26ff0526b8d68341511aee
Viral Pandya <virav.pandya@gmail.com>
2021-04-16 07:36:04 -0700

    Code Rub

--- OtripleS.Web.Api.Tests.Unit/Services/ExamFees/ExamFeeServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/ExamFees/ExamFeeServiceTests.Logic.cs
@@ -161,7 +161,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.ExamFees
 
             this.dateTimeBrokerMock.Verify(broker =>
                 broker.GetCurrentDateTime(),
-                    Times.Never);
+                    Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
                 broker.SelectExamFeeByIdAsync(examExamFeeId),
