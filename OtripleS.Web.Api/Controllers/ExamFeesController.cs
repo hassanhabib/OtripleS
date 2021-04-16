8eb9515c7329f2f8e0a1fd19c5ff872ecb7e9675
Viral Pandya <virav.pandya@gmail.com>
2021-04-15 21:10:50 -0700

    PostExamFeeAsync -> PASS

---
+++ OtripleS.Web.Api/Controllers/ExamFeesController.cs
@@ -0,0 +1,64 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OtripleS.Web.Api.Models.ExamFees;
+using OtripleS.Web.Api.Models.ExamFees.Exceptions;
+using OtripleS.Web.Api.Services.ExamFees;
+using RESTFulSense.Controllers;
+
+namespace OtripleS.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamFeesController : RESTFulController
+    {
+        private readonly IExamFeeService examFeeService;
+
+        public ExamFeesController(IExamFeeService examFeeService) =>
+            this.examFeeService = examFeeService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<ExamFee>> PostExamFeeAsync(ExamFee examFee)
+        {
+            try
+            {
+                ExamFee persistedExamFee =
+                    await this.examFeeService.AddExamFeeAsync(examFee);
+
+                return Ok(persistedExamFee);
+            }
+            catch (ExamFeeValidationException examFeeValidationException)
+                when (examFeeValidationException.InnerException is AlreadyExistsExamFeeException)
+            {
+                string innerMessage = GetInnerMessage(examFeeValidationException);
+
+                return Conflict(innerMessage);
+            }
+            catch (ExamFeeValidationException examFeeValidationException)
+            {
+                string innerMessage = GetInnerMessage(examFeeValidationException);
+
+                return BadRequest(innerMessage);
+            }
+            catch (ExamFeeDependencyException examFeeDependencyException)
+            {
+                return Problem(examFeeDependencyException.Message);
+            }
+            catch (ExamFeeServiceException examFeeServiceException)
+            {
+                return Problem(examFeeServiceException.Message);
+            }
+        }
+
+        
+
+        private static string GetInnerMessage(Exception exception) =>
+            exception.InnerException.Message;
+    }
+}
