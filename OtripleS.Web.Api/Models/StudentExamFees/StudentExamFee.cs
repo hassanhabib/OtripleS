72618c71363132d5853bb75c90a36ad16c675041
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-04-20 14:52:19 +0500

    Model Files Added

---
+++ OtripleS.Web.Api/Models/StudentExamFees/StudentExamFee.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+using System;
+using OtripleS.Web.Api.Models.ExamFees;
+using OtripleS.Web.Api.Models.Students;
+using OtripleS.Web.Api.Models.Users;
+
+namespace OtripleS.Web.Api.Models.StudentExamFees
+{
+    public class StudentExamFee : IAuditable
+    {
+        public Guid ExamFeeId {get; set;}
+        public ExamFee ExamFee {get; set;}
+        public Guid StudentId {get; set;}
+        public Student Student {get; set;}
+        public StudentExamFeeStatus Status {get; set;}
+        public DateTimeOffset CreatedDate {get; set;}
+        public DateTimeOffset UpdatedDate {get; set;}
+        public Guid CreatedBy {get; set;}
+        public User CreatedByUser {get; set;}
+        public Guid UpdatedBy {get; set;}
+        public User UpdatedByUser {get; set;}
+    }
+}
---
+++ OtripleS.Web.Api/Models/StudentExamFees/StudentExamFeeStatus.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+namespace OtripleS.Web.Api.Models.StudentExamFees
+{
+    public enum StudentExamFeeStatus
+    {
+        Pending,
+        Paid,
+        Deferred,
+        Waived,
+        Cancelled
+    }
+}
