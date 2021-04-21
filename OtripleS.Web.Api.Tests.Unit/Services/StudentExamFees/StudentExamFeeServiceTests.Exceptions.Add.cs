3a30244b723a2e17cb6341f7a5421a565416e2c9
eris.adhami <eri.adhami@gmail.com>
2021-04-21 17:18:54 +0200

    ShouldThrowDependencyExceptionOnCreateWhenDbExceptionOccursAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/StudentExamFees/StudentExamFeeServiceTests.Exceptions.Add.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentExamFees/StudentExamFeeServiceTests.Exceptions.Add.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using OtripleS.Web.Api.Models.StudentExamFees;
 using OtripleS.Web.Api.Models.StudentExamFees.Exceptions;
@@ -46,6 +47,53 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.StudentExamFees
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(expectedStudentExamFeeDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertStudentExamFeeAsync(inputStudentExamFee),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnCreateWhenDbExceptionOccursAndLogItAsync()
+        {
+            // given
+            DateTimeOffset dateTime = GetRandomDateTime();
+            StudentExamFee randomStudentExamFee = CreateRandomStudentExamFee(dateTime);
+            StudentExamFee inputStudentExamFee = randomStudentExamFee;
+            inputStudentExamFee.UpdatedBy = inputStudentExamFee.CreatedBy;
+            inputStudentExamFee.UpdatedDate = inputStudentExamFee.CreatedDate;
+            var databaseUpdateException = new DbUpdateException();
+
+            var expectedStudentExamFeeDependencyException =
+                new StudentExamFeeDependencyException(databaseUpdateException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Returns(dateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertStudentExamFeeAsync(inputStudentExamFee))
+                    .ThrowsAsync(databaseUpdateException);
+
+            // when
+            ValueTask<StudentExamFee> createStudentExamFeeTask =
+                this.studentExamFeeService.AddStudentExamFeeAsync(inputStudentExamFee);
+
+            // then
+            await Assert.ThrowsAsync<StudentExamFeeDependencyException>(() =>
+                createStudentExamFeeTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentExamFeeDependencyException))),
                     Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
