e869b9999a1407057b1f522782dc1b491629f89e
eris.adhami <eri.adhami@gmail.com>
2021-04-21 13:39:12 +0200

    ShouldAddStudentExamFeeAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentExamFees/StudentExamFeeServiceTests.cs
@@ -0,0 +1,53 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System;
+using Moq;
+using OtripleS.Web.Api.Brokers.DateTimes;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.StudentExamFees;
+using OtripleS.Web.Api.Services.StudentExamFees;
+using OtripleS.Web.Api.Services.StudentStudentExamFees;
+using Tynamix.ObjectFiller;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.StudentExamFees
+{
+    public partial class StudentExamFeeServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly IStudentExamFeeService studentExamFeeService;
+
+        public StudentExamFeeServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+
+            this.studentExamFeeService = new StudentExamFeeService(
+                storageBroker: this.storageBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object);
+        }
+
+        private StudentExamFee CreateRandomStudentExamFee() =>
+            CreateStudentExamFeeFiller(DateTimeOffset.UtcNow).Create();
+
+        private static Filler<StudentExamFee> CreateStudentExamFeeFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<StudentExamFee>();
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates)
+                .OnProperty(studentExamFee => studentExamFee.CreatedByUser).IgnoreIt()
+                .OnProperty(studentExamFee => studentExamFee.UpdatedByUser).IgnoreIt()
+                .OnProperty(studentExamFee => studentExamFee.Student).IgnoreIt()
+                .OnProperty(studentExamFee => studentExamFee.ExamFee).IgnoreIt();
+
+            return filler;
+        }
+    }
+}
---
+++ OtripleS.Web.Api/Services/StudentExamFees/StudentExamFeeService.cs
@@ -0,0 +1,36 @@
+﻿//---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+//----------------------------------------------------------------
+
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Brokers.DateTimes;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storage;
+using OtripleS.Web.Api.Models.StudentExamFees;
+using OtripleS.Web.Api.Services.StudentStudentExamFees;
+
+namespace OtripleS.Web.Api.Services.StudentExamFees
+{
+    public class StudentExamFeeService : IStudentExamFeeService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+
+        public StudentExamFeeService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker,
+            IDateTimeBroker dateTimeBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+            this.dateTimeBroker = dateTimeBroker;
+        }
+
+        public ValueTask<StudentExamFee> AddStudentExamFeeAsync(StudentExamFee studentExamFee)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
