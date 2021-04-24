14f88f9d66c282240335493306f8e4de562195e6
Viral Pandya <virav.pandya@gmail.com>
2021-04-23 21:03:20 -0700

    ShouldThrowValidationExceptionOnModifyWhenStudentExamFeeIsNullAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentExamFees/StudentExamFeeServiceTests.Validations.Modify.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Force.DeepCloner;
+using Moq;
+using OtripleS.Web.Api.Models.StudentExamFees;
+using OtripleS.Web.Api.Models.StudentExamFees.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.StudentExamFees
+{
+    public partial class StudentExamFeeServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnModifyWhenStudentExamFeeIsNullAndLogItAsync()
+        {
+            //given
+            StudentExamFee invalidStudentExamFee = null;
+            var nullStudentExamFeeException = new NullStudentExamFeeException();
+
+            var expectedStudentExamFeeValidationException =
+                new StudentExamFeeValidationException(nullStudentExamFeeException);
+
+            //when
+            ValueTask<StudentExamFee> modifyStudentExamFeeTask =
+                this.studentExamFeeService.ModifyStudentExamFeeAsync(invalidStudentExamFee);
+
+            //then
+            await Assert.ThrowsAsync<StudentExamFeeValidationException>(() =>
+                modifyStudentExamFeeTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentExamFeeValidationException))),
+                Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        
+    }
+}
