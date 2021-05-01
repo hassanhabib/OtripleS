25ddb66bc7078980b289583a724ca0b68baf2d25
Viral Pandya <virav.pandya@gmail.com>
2021-04-30 17:08:43 -0700

    Code Rub

--- OtripleS.Web.Api/Services/StudentExamFees/StudentExamFeeService.Validations.cs
+++ OtripleS.Web.Api/Services/StudentExamFees/StudentExamFeeService.Validations.cs
@@ -11,17 +11,7 @@ using OtripleS.Web.Api.Models.StudentExamFees.Exceptions;
 namespace OtripleS.Web.Api.Services.StudentExamFees
 {
     public partial class StudentExamFeeService
-    {
-        //private void ValidateStudentExamFeeId(Guid studentExamFeeId)
-        //{
-        //    if (studentExamFeeId == default)
-        //    {
-        //        throw new InvalidStudentExamFeeException(
-        //            parameterName: nameof(StudentExamFee.Id),
-        //            parameterValue: studentExamFeeId);
-        //    }
-        //}
-
+    {        
         private static void ValidateStorageStudentExamFee(
           StudentExamFee storageStudentExamFee,
           Guid studentId,
