0a7cb104de24af4655b87eb293910799009e33fc
eris.adhami <eri.adhami@gmail.com>
2021-05-12 14:38:42 +0200

    ShouldRetrieveRegistrationByIdAsync -> PASS

--- OtripleS.Web.Api/Services/Registrations/RegistrationService.cs
+++ OtripleS.Web.Api/Services/Registrations/RegistrationService.cs
@@ -29,7 +29,7 @@ namespace OtripleS.Web.Api.Services.Registrations
         }
         public ValueTask<Registration> RetrieveRegistrationByIdAsync(Guid registrationId)
         {
-            throw new NotImplementedException();
+            return this.storageBroker.SelectRegistrationByIdAsync(registrationId);
         }
     }
 }
