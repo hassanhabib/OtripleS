1e9a835ccafea33b04a79ccf89c1b9229ae5ee9b
eris.adhami <eri.adhami@gmail.com>
2021-05-12 14:12:24 +0200

    Registration Broker Select By Id function

--- OtripleS.Web.Api/Brokers/Storages/StorageBroker.Registrations.cs
+++ OtripleS.Web.Api/Brokers/Storages/StorageBroker.Registrations.cs
@@ -4,6 +4,7 @@
 // ---------------------------------------------------------------
 
 using System;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using OtripleS.Web.Api.Models.Registrations;
 
@@ -12,5 +13,13 @@ namespace OtripleS.Web.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Registration> Registrations { get; set; }
+
+        public async ValueTask<Registration> SelectRegistrationByIdAsync(Guid RegistrationId)
+        {
+            using var broker = new StorageBroker(this.configuration);
+            this.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+
+            return await broker.Registrations.FindAsync(RegistrationId);
+        }
     }
 }
