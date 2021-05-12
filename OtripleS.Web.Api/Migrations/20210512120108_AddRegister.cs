2f2cf2da6e2b3d2bee475c3cb99113690563021c
eris.adhami <eri.adhami@gmail.com>
2021-05-12 14:04:01 +0200

    Registration Model Setup & Migrations with EF Core

---
+++ OtripleS.Web.Api/Migrations/20210512120108_AddRegister.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OtripleS.Web.Api.Migrations
+{
+    public partial class AddRegister : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Registrations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    SubmitterName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SubmitterPhone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SubmitterEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentPhone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AdditionalDetails = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registrations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Registrations_AspNetUsers_CreatedBy",
+                        column: x => x.CreatedBy,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Registrations_AspNetUsers_UpdatedBy",
+                        column: x => x.UpdatedBy,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_CreatedBy",
+                table: "Registrations",
+                column: "CreatedBy");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_UpdatedBy",
+                table: "Registrations",
+                column: "UpdatedBy");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registrations");
+        }
+    }
+}
---
+++ OtripleS.Web.Api/Models/Registrations/Registration.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using OtripleS.Web.Api.Models.Users;
+
+namespace OtripleS.Web.Api.Models.Registrations
+{
+    public class Registration : IAuditable
+    {
+        public Guid Id { get; set; }
+        public RegistrationStatus Status { get; set; }
+        public string SubmitterName { get; set; }
+        public string SubmitterPhone { get; set; }
+        public string SubmitterEmail { get; set; }
+        public string StudentName { get; set; }
+        public string StudentPhone { get; set; }
+        public string StudentEmail { get; set; }
+        public string AdditionalDetails { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+
+        public Guid CreatedBy { get; set; }
+        public User CreatedByUser { get; set; }
+
+        public Guid UpdatedBy { get; set; }
+        public User UpdatedByUser { get; set; }
+    }
+}
