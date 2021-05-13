020a083402f0e82bc13c4036a95049811d6ca45d
Stanislaw Nowak <stanislaw@nowakpost.com>
2021-05-13 08:40:50 +0200

    ShouldThrowValidationExceptionOnAddWhenRegistrationIsNullAndLogItAsync => FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Registrations/RegistrationServiceTests.Validations.Add.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using OtripleS.Web.Api.Models.Registrations;
+using OtripleS.Web.Api.Models.Registrations.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Registrations
+{
+    public partial class RegistrationServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddWhenRegistrationIsNullAndLogItAsync()
+        {
+            // given
+            Registration randomRegistration = default;
+            Registration nullRegistration = randomRegistration;
+            var nullRegistrationException = new NullRegistrationException();
+
+            var expectedRegistrationValidationException =
+                new RegistrationValidationException(nullRegistrationException);
+
+            // when
+            ValueTask<Registration> createRegistrationTask =
+                this.registrationService.AddRegistrationAsync(nullRegistration);
+
+            // then
+            await Assert.ThrowsAsync<RegistrationValidationException>(() =>
+                createRegistrationTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedRegistrationValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertRegistrationAsync(It.IsAny<Registration>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
