b7d739172060dc2bc7c68b138c2b0a771875b430
Stanislaw Nowak <stanislaw@nowakpost.com>
2021-05-13 08:34:14 +0200

    ShouldThrowDependencyExceptionOnAddWhenDbExceptionOccursAndLogItAsync => PASS

--- OtripleS.Web.Api.Tests.Unit/Services/Registrations/RegistrationServiceTests.Exceptions.Add.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Registrations/RegistrationServiceTests.Exceptions.Add.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
 using OtripleS.Web.Api.Models.Registrations;
 using OtripleS.Web.Api.Models.Registrations.Exceptions;
 using System;
@@ -57,5 +58,52 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Registrations
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddWhenDbExceptionOccursAndLogItAsync()
+        {
+            // given
+            DateTimeOffset dateTime = GetRandomDateTime();
+            Registration randomRegistration = CreateRandomRegistration(dateTime);
+            Registration inputRegistration = randomRegistration;
+            inputRegistration.UpdatedBy = inputRegistration.CreatedBy;
+            var databaseUpdateException = new DbUpdateException();
+
+            var expectedRegistrationDependencyException =
+                new RegistrationDependencyException(databaseUpdateException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Returns(dateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertRegistrationAsync(inputRegistration))
+                    .ThrowsAsync(databaseUpdateException);
+
+            // when
+            ValueTask<Registration> createRegistrationTask =
+                this.registrationService.AddRegistrationAsync(inputRegistration);
+
+            // then
+            await Assert.ThrowsAsync<RegistrationDependencyException>(() =>
+                createRegistrationTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedRegistrationDependencyException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertRegistrationAsync(inputRegistration),
+                    Times.Once);
+
+            //this.dateTimeBrokerMock.Verify(broker =>
+            //    broker.GetCurrentDateTime(),
+            //        Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
