83dd434a8a3ae4c1b42a62fc945eb6604104bae1
Viral Pandya <virav.pandya@gmail.com>
2021-05-15 08:31:34 -0700

    ShouldThrowValidationExceptionOnRemoveIfRegistrationIsNotFoundAndLogItAsync -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/Registrations/RegistrationServiceTests.Validations.Remove.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Registrations/RegistrationServiceTests.Validations.Remove.cs
@@ -48,10 +48,52 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Registrations
                     Times.Never);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls(); 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
-            this.storageBrokerMock.VerifyNoOtherCalls();
         }
 
-        
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfRegistrationIsNotFoundAndLogItAsync()
+        {
+            // given
+            DateTimeOffset randomDateTime = GetRandomDateTime();
+            Registration randomRegistration = CreateRandomRegistration(dateTime: randomDateTime);
+            Guid actualRegistrationId = randomRegistration.Id;
+            Registration noRegistration = null;
+
+            var notFoundRegistrationException =
+                new NotFoundRegistrationException(randomRegistration.Id);
+
+            var expectedRegistrationValidationException =
+                new RegistrationValidationException(notFoundRegistrationException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectRegistrationByIdAsync(actualRegistrationId)).
+                    ReturnsAsync(noRegistration);
+
+            // when
+            ValueTask<Registration> removeRegistrationByIdTask =
+                this.registrationService.RemoveRegistrationByIdAsync(actualRegistrationId);
+
+            // then
+            await Assert.ThrowsAsync<RegistrationValidationException>(() =>
+                removeRegistrationByIdTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectRegistrationByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteRegistrationAsync(It.IsAny<Registration>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedRegistrationValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();            
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
