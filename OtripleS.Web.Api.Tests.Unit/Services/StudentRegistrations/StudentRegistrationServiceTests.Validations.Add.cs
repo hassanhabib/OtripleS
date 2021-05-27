e4236cafac5a7cfec6a52f7651498548c1159cc2
Stanislaw Nowak <stanislaw@nowakpost.com>
2021-05-27 07:49:26 +0200

    ShouldThrowValidationExceptionOnAddWhenStudentIdIsInvalidAndLogItAsync = FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/StudentRegistrations/StudentRegistrationServiceTests.Validations.Add.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentRegistrations/StudentRegistrationServiceTests.Validations.Add.cs
@@ -38,5 +38,41 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.StudentRegistrations
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddWhenStudentIdIsInvalidAndLogItAsync()
+        {
+            // given
+            StudentRegistration randomStudentRegistration = CreateRandomStudentRegistration();
+            StudentRegistration inputStudentRegistration = randomStudentRegistration;
+            inputStudentRegistration.StudentId = default;
+
+            var invalidStudentRegistrationInputException = new InvalidStudentRegistrationException(
+                parameterName: nameof(StudentRegistration.StudentId),
+                parameterValue: inputStudentRegistration.StudentId);
+
+            var expectedStudentRegistrationValidationException =
+                new StudentRegistrationValidationException(invalidStudentRegistrationInputException);
+
+            // when
+            ValueTask<StudentRegistration> addStudentRegistrationTask =
+                this.studentRegistrationService.AddStudentRegistrationAsync(inputStudentRegistration);
+
+            // then
+            await Assert.ThrowsAsync<StudentRegistrationValidationException>(() =>
+                addStudentRegistrationTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentRegistrationValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertStudentRegistrationAsync(It.IsAny<StudentRegistration>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ OtripleS.Web.Api/Models/StudentRegistrations/Exceptions/InvalidStudentRegistrationException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtripleS.Web.Api.Models.StudentRegistrations.Exceptions
+{
+    public class InvalidStudentRegistrationException : Exception
+    {
+        public InvalidStudentRegistrationException(string parameterName, object parameterValue)
+            : base($"Invalid StudentRegistration, " +
+                  $"ParameterNmae: {parameterName}, " +
+                  $"ParameterValue: {parameterValue}.")
+        { }
+    }
+}
