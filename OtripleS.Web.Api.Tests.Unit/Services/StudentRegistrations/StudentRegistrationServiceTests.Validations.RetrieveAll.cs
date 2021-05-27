a31b2aa5d77cf7fc4c7b6286ae268421099f51e1
Alagbala Damilola <zionlloyd05@gmail.com>
2021-05-27 16:03:41 +0100

    ShouldLogWarningOnRetrieveAllWhenStudentRegistrationsWasEmptyAndLogIt -> FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentRegistrations/StudentRegistrationServiceTests.Validations.RetrieveAll.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.StudentRegistrations;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.StudentRegistrations
+{
+    public partial class StudentRegistrationServiceTests
+    {
+        [Fact]
+        public void ShouldLogWarningOnRetrieveAllWhenStudentRegistrationsWasEmptyAndLogIt()
+        {
+            // given
+            IQueryable<StudentRegistration> emptyStorageStudentRegistrations = new List<StudentRegistration>().AsQueryable();
+            IQueryable<StudentRegistration> expectedStudentRegistrations = emptyStorageStudentRegistrations;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllStudentRegistrations())
+                    .Returns(expectedStudentRegistrations);
+
+            // when
+            IQueryable<StudentRegistration> actualStudentRegistrations =
+                this.studentRegistrationService.RetrieveAllStudentRegistrations();
+
+            // then
+            actualStudentRegistrations.Should().BeEquivalentTo(emptyStorageStudentRegistrations);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogWarning("No studentRegistrations found in storage."),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllStudentRegistrations(),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
