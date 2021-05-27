26bcf6e4c4ed3ae9a7acf777b02d8e254fbcbe9a
Stanislaw Nowak <stanislaw@nowakpost.com>
2021-05-27 07:42:32 +0200

    ShouldThrowValidationExceptionOnAddWhenStudentRegistrationIsNullAndLogItAsync => FAIL

---
+++ OtripleS.Web.Api.Tests.Unit/Services/StudentRegistrations/StudentRegistrationServiceTests.Validations.Add.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using OtripleS.Web.Api.Models.StudentRegistrations;
+using OtripleS.Web.Api.Models.StudentRegistrations.Exceptions;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.StudentRegistrations
+{
+    public partial class StudentRegistrationServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnAddWhenStudentRegistrationIsNullAndLogItAsync()
+        {
+            // given
+            StudentRegistration nullStudentRegistration = default;
+            var nullStudentRegistrationException = new NullStudentRegistrationException();
+
+            var expectedStudentRegistrationValidationException =
+                new StudentRegistrationValidationException(nullStudentRegistrationException);
+
+            // when
+            ValueTask<StudentRegistration> addStudentRegistrationTask =
+                this.studentRegistrationService.AddStudentRegistrationAsync(nullStudentRegistration);
+
+            // then
+            await Assert.ThrowsAsync<StudentRegistrationValidationException>(() =>
+                addStudentRegistrationTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedStudentRegistrationValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertStudentRegistrationAsync(It.IsAny<StudentRegistration>()),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ OtripleS.Web.Api/Models/StudentRegistrations/Exceptions/NullStudentRegistrationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtripleS.Web.Api.Models.StudentRegistrations.Exceptions
+{
+    public class NullStudentRegistrationException : Exception
+    {
+        public NullStudentRegistrationException() : base("The StudentRegistration is null.") { }
+    }
+}
