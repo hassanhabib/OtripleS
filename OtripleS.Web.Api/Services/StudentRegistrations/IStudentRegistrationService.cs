a7be8bdf2b89591664e243bdf12056b4630ad3c6
Alagbala Damilola <zionlloyd05@gmail.com>
2021-05-27 14:44:02 +0100

    ShouldRetrieveAllStudentRegistrations -> FAIL

---
+++ OtripleS.Web.Api/Services/StudentRegistrations/IStudentRegistrationService.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Models.StudentRegistrations;
+
+namespace OtripleS.Web.Api.Services.StudentRegistrations
+{
+    public interface IStudentRegistrationService
+    {
+        IQueryable<StudentRegistration> RetrieveAllStudentRegistrations();
+    }
+}
---
+++ OtripleS.Web.Api/Services/StudentRegistrations/StudentRegistrationService.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OtripleS.Web.Api.Brokers.DateTimes;
+using OtripleS.Web.Api.Brokers.Loggings;
+using OtripleS.Web.Api.Brokers.Storages;
+using OtripleS.Web.Api.Models.StudentRegistrations;
+
+namespace OtripleS.Web.Api.Services.StudentRegistrations
+{
+    public class StudentRegistrationService : IStudentRegistrationService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+
+        public StudentRegistrationService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker,
+            IDateTimeBroker dateTimeBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+            this.dateTimeBroker = dateTimeBroker;
+        }
+
+        public IQueryable<StudentRegistration> RetrieveAllStudentRegistrations()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
