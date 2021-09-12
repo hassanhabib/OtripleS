480cdc8f45dd4d805c7238990619d9cd315069bf
Hassan Habib <hassanhabib@live.com>
2021-09-11 21:47:48 -0500

    CODE RUB: Fix Dependency Order

--- OtripleS.Web.Api/Services/Foundations/Students/StudentService.cs
+++ OtripleS.Web.Api/Services/Foundations/Students/StudentService.cs
@@ -16,17 +16,17 @@ namespace OtripleS.Web.Api.Services.Foundations.Students
     public partial class StudentService : IStudentService
     {
         private readonly IStorageBroker storageBroker;
-        private readonly ILoggingBroker loggingBroker;
         private readonly IDateTimeBroker dateTimeBroker;
+        private readonly ILoggingBroker loggingBroker;
 
         public StudentService(
             IStorageBroker storageBroker,
-            ILoggingBroker loggingBroker,
-            IDateTimeBroker dateTimeBroker)
+            IDateTimeBroker dateTimeBroker,
+            ILoggingBroker loggingBroker)
         {
             this.storageBroker = storageBroker;
-            this.loggingBroker = loggingBroker;
             this.dateTimeBroker = dateTimeBroker;
+            this.loggingBroker = loggingBroker;
         }
 
         public ValueTask<Student> RegisterStudentAsync(Student student) =>
