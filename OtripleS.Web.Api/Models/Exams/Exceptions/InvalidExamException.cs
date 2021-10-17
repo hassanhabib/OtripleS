33f1ecbf931526b7b1b032d69523cac079e73eee
Emily Bettencourt <emmicourt@gmail.com>
2021-10-16 20:39:51 -0700

    CODE RUB: remove old exception constructor

--- OtripleS.Web.Api/Models/Exams/Exceptions/InvalidExamException.cs
+++ OtripleS.Web.Api/Models/Exams/Exceptions/InvalidExamException.cs
@@ -9,12 +9,6 @@ namespace OtripleS.Web.Api.Models.Exams.Exceptions
 {
     public class InvalidExamException : Xeption
     {
-        public InvalidExamException(string parameterName, object parameterValue)
-            : base(message: $"Invalid exam, " +
-                  $"parameter name: {parameterName}, " +
-                  $"parameter value: {parameterValue}.")
-        { }
-
         public InvalidExamException()
             : base(message: "Invalid exam. Please fix the errors and try again.")
         { }
