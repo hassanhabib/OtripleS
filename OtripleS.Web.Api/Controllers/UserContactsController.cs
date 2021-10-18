c3aebf012f5c5a019f8edb4eb216127d079976b7
Milan Toncic <milan.toncic@zuehlke.com>
2021-10-18 08:27:18 +0200

    CODE RUB: Route goes after ApiController

--- OtripleS.Web.Api/Controllers/UserContactsController.cs
+++ OtripleS.Web.Api/Controllers/UserContactsController.cs
@@ -14,8 +14,8 @@ using RESTFulSense.Controllers;
 
 namespace OtripleS.Web.Api.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class UserContactsController : RESTFulController
     {
         private readonly IUserContactService userContactService;
