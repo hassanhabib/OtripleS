0b4d2fcdd46307787cc22269799f211be094e154
j-bedford <j-bedford@users.noreply.github.com>
2021-10-23 09:23:49 +0200

    CODE RUB: parameter aliasing

--- OtripleS.Web.Api/Brokers/Storages/StorageBroker.Students.cs
+++ OtripleS.Web.Api/Brokers/Storages/StorageBroker.Students.cs
@@ -19,7 +19,7 @@ namespace OtripleS.Web.Api.Brokers.Storages
         public async ValueTask<Student> InsertStudentAsync(Student student)
         {
             using var broker = new StorageBroker(this.configuration);
-            EntityEntry<Student> studentEntityEntry = await broker.Students.AddAsync(student);
+            EntityEntry<Student> studentEntityEntry = await broker.Students.AddAsync(entity: student);
             await broker.SaveChangesAsync();
 
             return studentEntityEntry.Entity;
@@ -38,7 +38,7 @@ namespace OtripleS.Web.Api.Brokers.Storages
         public async ValueTask<Student> UpdateStudentAsync(Student student)
         {
             using var broker = new StorageBroker(this.configuration);
-            EntityEntry<Student> studentEntityEntry = broker.Students.Update(student);
+            EntityEntry<Student> studentEntityEntry = broker.Students.Update(entity: student);
             await broker.SaveChangesAsync();
 
             return studentEntityEntry.Entity;
@@ -47,7 +47,7 @@ namespace OtripleS.Web.Api.Brokers.Storages
         public async ValueTask<Student> DeleteStudentAsync(Student student)
         {
             using var broker = new StorageBroker(this.configuration);
-            EntityEntry<Student> studentEntityEntry = broker.Students.Remove(student);
+            EntityEntry<Student> studentEntityEntry = broker.Students.Remove(entity: student);
             await broker.SaveChangesAsync();
 
             return studentEntityEntry.Entity;
