c44914e4fdf1c4dda003a2305d21b9efb41f3a1c
Emily Bettencourt <emmicourt@gmail.com>
2021-10-24 14:14:49 -0700

    CODE RUB: Remove old invalid exception constructor

--- OtripleS.Web.Api/Models/Fees/Exceptions/InvalidFeeException.cs
+++ OtripleS.Web.Api/Models/Fees/Exceptions/InvalidFeeException.cs
@@ -9,12 +9,6 @@ namespace OtripleS.Web.Api.Models.Fees.Exceptions
 {
     public class InvalidFeeException : Xeption
     {
-        public InvalidFeeException(string parameterName, object parameterValue)
-            : base(message: $"Invalid fee, " +
-                  $"parameter name: {parameterName}, " +
-                  $"parameter value: {parameterValue}.")
-        { }
-
         public InvalidFeeException()
             : base("Fee is invalid. Please fix the errors and try again.")
         { }
