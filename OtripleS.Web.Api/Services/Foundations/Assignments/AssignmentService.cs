2b0094f24dc6de5f9b17bd069bf0a924723d8e3c
Milan Toncic <milantoncic@outlook.com>
2021-10-28 07:19:50 +0200

    CODE RUB: To prefered one-liner form

--- OtripleS.Web.Api/Services/Foundations/Assignments/AssignmentService.cs
+++ OtripleS.Web.Api/Services/Foundations/Assignments/AssignmentService.cs
@@ -36,11 +36,9 @@ namespace OtripleS.Web.Api.Services.Foundations.Assignments
             return await this.storageBroker.InsertAssignmentAsync(assignment);
         });
 
-        public IQueryable<Assignment> RetrieveAllAssignments() =>
-        TryCatch(() =>
-        {
-            return this.storageBroker.SelectAllAssignments();
-        });
+        public IQueryable<Assignment> RetrieveAllAssignments() => 
+        TryCatch(() => 
+            this.storageBroker.SelectAllAssignments());
 
         public ValueTask<Assignment> RetrieveAssignmentByIdAsync(Guid assignmentId) =>
         TryCatch(async () =>
