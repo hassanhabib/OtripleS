357362e2a66614015bcbc00addd8d732cc9da181
MD YASIR ARAFAT <icsm.arafat@gmail.com>
2021-11-01 01:37:34 +0600

    Empty Lines 135, 276 -> GONE

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Validations.Modify.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Students/StudentServiceTests.Validations.Modify.cs
@@ -133,7 +133,6 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Students
             invalidStudent.CreatedDate = sameDate;
             invalidStudent.UpdatedDate = sameDate;
 
-
             var invalidStudentException = new InvalidStudentException();
 
             invalidStudentException.AddData(
@@ -273,7 +272,6 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Students
             invalidStudent.UpdatedDate = storageStudent.UpdatedDate;
             invalidStudent.CreatedBy = invalidCreatedBy;
             Guid studentId = invalidStudent.Id;
-
 
             var invalidStudentException = new InvalidStudentException();
 
