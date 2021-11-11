a1bf2c3df86572d0ba375aa98bcd9c905c854ea0
MD YASIR ARAFAT <ping@arafat.pro>
2021-11-11 17:54:48 +0600

    CODE RUB: Moved Property to Next line following Standard Consistancy

--- OtripleS.Web.Api.Tests.Acceptance/APIs/TeacherContacts/TeacherContactsApiTests.cs
+++ OtripleS.Web.Api.Tests.Acceptance/APIs/TeacherContacts/TeacherContactsApiTests.cs
@@ -114,7 +114,8 @@ namespace OtripleS.Web.Api.Tests.Acceptance.APIs.TeacherContacts
 
             var filler = new Filler<Teacher>();
 
-            filler.Setup().OnProperty(teacher => teacher.Status).Use(TeacherStatus.Active)
+            filler.Setup()
+                .OnProperty(teacher => teacher.Status).Use(TeacherStatus.Active)
                 .OnProperty(teacher => teacher.CreatedBy).Use(posterId)
                 .OnProperty(teacher => teacher.UpdatedBy).Use(posterId)
                 .OnProperty(teacher => teacher.CreatedDate).Use(now)
