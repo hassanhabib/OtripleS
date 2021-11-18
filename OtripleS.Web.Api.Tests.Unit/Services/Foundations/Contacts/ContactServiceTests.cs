d61d3c3b82ad57b9246004134d18b3bf93aadae0
MD YASIR ARAFAT <ping@arafat.pro>
2021-11-19 03:42:16 +0600

    CODE RUB: Removed Commented Code & Whiitespace

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Contacts/ContactServiceTests.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Contacts/ContactServiceTests.cs
@@ -77,9 +77,6 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Contacts
 
         private static Expression<Func<Exception, bool>> SameExceptionAs(Exception expectedException)
         {
-            //return actualException =>
-            //    expectedException.Message == actualException.Message
-            //    && expectedException.InnerException.Message == actualException.InnerException.Message;
             return actualException =>
                 actualException.Message == expectedException.Message
                 && actualException.InnerException.Message == expectedException.InnerException.Message;
