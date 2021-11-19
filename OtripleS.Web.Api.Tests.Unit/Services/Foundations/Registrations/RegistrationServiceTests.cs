80edba24ea592a15287f5d6576336e10f62d98aa
juturul <jlavanyareddy@gmail.com>
2021-11-18 19:10:56 -0800

    CODE RUB: Moved && To Next Line

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Registrations/RegistrationServiceTests.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Registrations/RegistrationServiceTests.cs
@@ -81,8 +81,8 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Registrations
         private static Expression<Func<Exception, bool>> SameExceptionAs(Exception expectedException)
         {
             return actualException =>
-                actualException.Message == expectedException.Message &&
-                actualException.InnerException.Message == expectedException.InnerException.Message;
+                actualException.Message == expectedException.Message
+                && actualException.InnerException.Message == expectedException.InnerException.Message;
         }
 
         private static SqlException GetSqlException() =>
