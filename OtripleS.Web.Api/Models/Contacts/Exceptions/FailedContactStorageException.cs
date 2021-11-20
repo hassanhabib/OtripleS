c1120d4d0f8464e6bc68be74168c57468762ad49
MD YASIR ARAFAT <ping@arafat.pro>
2021-11-20 18:34:21 +0600

    CODE RUB: Added copyright and Parameterised alias

--- OtripleS.Web.Api/Models/Contacts/Exceptions/FailedContactStorageException.cs
+++ OtripleS.Web.Api/Models/Contacts/Exceptions/FailedContactStorageException.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
 using Xeptions;
 
 namespace OtripleS.Web.Api.Models.Contacts.Exceptions
@@ -6,7 +11,7 @@ namespace OtripleS.Web.Api.Models.Contacts.Exceptions
     public class FailedContactStorageException : Xeption
     {
         public FailedContactStorageException(Exception innerException)
-            :base("Failed contact storage error occurred, contact support.", innerException)
+            :base(message:"Failed contact storage error occurred, contact support.", innerException)
         { }
     }
 }
