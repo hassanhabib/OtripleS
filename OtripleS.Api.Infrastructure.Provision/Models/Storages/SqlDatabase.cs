526306547eba5d4f56901e788f7a6e4060f67d08
Ricky <rcruzfreelance@outlook.com>
2021-11-26 18:24:50 -0400

    DATA: Created Models for storages

---
+++ OtripleS.Api.Infrastructure.Provision/Models/Storages/SqlDatabase.cs
@@ -0,0 +1,13 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+namespace OtripleS.Web.Api.Infrastructure.Provision.Models.Storages
+{
+    public class SqlDatabase
+    {
+        public string AdminName { get; set; }
+        public string AdminAccess { get; set; }
+    }
+}
