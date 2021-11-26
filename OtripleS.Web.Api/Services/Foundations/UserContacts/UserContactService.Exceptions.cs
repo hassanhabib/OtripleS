77505265f859f83a7a2f6c857989951b44dfd9f9
Ricky <rcruzfreelance@outlook.com>
2021-11-26 17:37:33 -0400

    ShouldThrowDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> PASS

--- OtripleS.Web.Api/Services/Foundations/UserContacts/UserContactService.Exceptions.cs
+++ OtripleS.Web.Api/Services/Foundations/UserContacts/UserContactService.Exceptions.cs
@@ -87,7 +87,10 @@ namespace OtripleS.Web.Api.Services.Foundations.UserContacts
             }
             catch (SqlException sqlException)
             {
-                throw CreateAndLogCriticalDependencyException(sqlException);
+                var failedUserContactStorageException =
+                    new FailedUserContactStorageException(sqlException);
+
+                throw CreateAndLogCriticalDependencyException(failedUserContactStorageException);
             }
             catch (Exception exception)
             {
