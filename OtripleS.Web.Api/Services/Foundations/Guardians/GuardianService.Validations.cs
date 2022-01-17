e61fd5af45fbc83ca841682070696fc330187b67
KFitz114 <flipperly@protonmail.com>
2022-01-17 16:36:59 -0500

    CODE RUB: Use is null instead of default

--- OtripleS.Web.Api/Services/Foundations/Guardians/GuardianService.Validations.cs
+++ OtripleS.Web.Api/Services/Foundations/Guardians/GuardianService.Validations.cs
@@ -91,7 +91,7 @@ namespace OtripleS.Web.Api.Services.Foundations.Guardians
 
         private static void ValidateGuardianIdIsNotNull(Guardian guardian)
         {
-            if (guardian == default)
+            if (guardian is null)
             {
                 throw new NullGuardianException();
             }
