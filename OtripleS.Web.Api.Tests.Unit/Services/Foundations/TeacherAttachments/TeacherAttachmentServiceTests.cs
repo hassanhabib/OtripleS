f05c95deb8f8083ff7079c58ee12e63c6b53476b
KFitz114 <flipperly@protonmail.com>
2022-01-28 21:14:41 -0500

    CODE RUB: Replace DateTimeBroker order with LoggingBroker

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherAttachments/TeacherAttachmentServiceTests.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherAttachments/TeacherAttachmentServiceTests.cs
@@ -21,20 +21,20 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.TeacherAttachments
     public partial class TeacherAttachmentServiceTests
     {
         private readonly Mock<IStorageBroker> storageBrokerMock;
-        private readonly Mock<ILoggingBroker> loggingBrokerMock;
         private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
         private readonly ITeacherAttachmentService teacherAttachmentService;
 
         public TeacherAttachmentServiceTests()
         {
             this.storageBrokerMock = new Mock<IStorageBroker>();
-            this.loggingBrokerMock = new Mock<ILoggingBroker>();
             this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
 
             this.teacherAttachmentService = new TeacherAttachmentService(
                 storageBroker: this.storageBrokerMock.Object,
-                loggingBroker: this.loggingBrokerMock.Object,
-                dateTimeBroker: this.dateTimeBrokerMock.Object);
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
         }
 
         private static IQueryable<TeacherAttachment> CreateRandomTeacherAttachments() =>
