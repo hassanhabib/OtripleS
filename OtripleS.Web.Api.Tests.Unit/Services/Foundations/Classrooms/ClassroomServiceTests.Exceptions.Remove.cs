2fab0c2a29b0a22e0e02724693e31dafd67e2173
evangelinedrink <Evangelinedrink@gmail.com>
2022-02-02 23:15:36 -0800

    CODE RUB: Fix Indentation for Classrooms Exception Remove

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Classrooms/ClassroomServiceTests.Exceptions.Remove.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Classrooms/ClassroomServiceTests.Exceptions.Remove.cs
@@ -41,8 +41,9 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Classrooms
                 deleteClassroomTask.AsTask());
 
             this.loggingBrokerMock.Verify(broker =>
-                broker.LogCritical(It.Is(SameExceptionAs(expectedClassroomDependencyException))),
-                    Times.Once);
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedClassroomDependencyException))),
+                        Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
                 broker.SelectClassroomByIdAsync(inputClassroomId),
@@ -85,7 +86,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Classrooms
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogError(It.Is(SameExceptionAs(
                     expectedClassroomDependencyException))),
-                    Times.Once);
+                        Times.Once);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
@@ -153,8 +154,9 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Classrooms
                 deleteClassroomTask.AsTask());
 
             this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(SameExceptionAs(expectedClassroomServiceException))),
-                    Times.Once);
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedClassroomServiceException))),
+                        Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
                 broker.SelectClassroomByIdAsync(inputClassroomId),
