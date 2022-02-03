ea50d53263b815e88f8a3f26b5f59a03b3c621f1
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-02-03 11:16:11 -0800

    CODE RUB: changed action variable in retrieveall exam attachments

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/ExamAttachments/ExamAttachmentServiceTests.Exceptions.RetrieveAll.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/ExamAttachments/ExamAttachmentServiceTests.Exceptions.RetrieveAll.cs
@@ -25,9 +25,14 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.ExamAttachments
                 broker.SelectAllExamAttachments())
                     .Throws(sqlException);
 
-            // when . then
-            Assert.Throws<ExamAttachmentDependencyException>(() =>
-                this.examAttachmentService.RetrieveAllExamAttachments());
+            // when
+            Action retrieveAllExamAttachmentsAction = () =>
+                this.examAttachmentService.RetrieveAllExamAttachments();
+
+            // then
+            Assert.Throws<ExamAttachmentDependencyException>(
+                retrieveAllExamAttachmentsAction);
+
 
             this.storageBrokerMock.Verify(broker =>
                 broker.SelectAllExamAttachments(),
@@ -55,9 +60,13 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.ExamAttachments
                 broker.SelectAllExamAttachments())
                     .Throws(exception);
 
-            // when . then
-            Assert.Throws<ExamAttachmentServiceException>(() =>
-                this.examAttachmentService.RetrieveAllExamAttachments());
+            // when
+            Action retrieveAllExamAttachmentsAction = () =>
+                this.examAttachmentService.RetrieveAllExamAttachments();
+
+            // then
+            Assert.Throws<ExamAttachmentServiceException>(
+                retrieveAllExamAttachmentsAction);
 
             this.storageBrokerMock.Verify(broker =>
                 broker.SelectAllExamAttachments(),
