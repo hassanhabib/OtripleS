563fb324d52de1b11945781a38e0155355606167
Janane <imjanane@gmail.com>
2022-02-10 15:57:52 -0600

    ShouldThrowServiceExceptionOnCreateWhenExceptionOccursAndLogItAsyn -> FAIL

---
+++ OtripleS.Web.Api/Models/Assignments/Exceptions/FailedAssignmentServiceException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xeptions;
+
+namespace OtripleS.Web.Api.Models.Assignments.Exceptions
+{
+    public class FailedAssignmentServiceException :Xeption
+    {
+        public FailedAssignmentServiceException(Exception innerException)
+             : base("Failed Assignment service error occured, contact support", innerException)
+        {}
+    }
+}
