3fb93fd2077b0f4927ca31c0d4d0c098e3b14201
Tharani <tharanisantosh@outlook.com>
2022-02-10 04:19:34 +0200

    ShouldThrowServiceExceptionOnAddWhenExceptionOccursAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api/Models/UserContacts/Exceptions/FailedUserContactServiceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xeptions;
+
+namespace OtripleS.Web.Api.Models.UserContacts.Exceptions
+{
+    public class FailedUserContactServiceException : Xeption
+    {
+        public FailedUserContactServiceException(Exception innerException)
+            : base(message: "Failed user contact service error occured.", innerException)
+        { 
+        }
+    }
+}
