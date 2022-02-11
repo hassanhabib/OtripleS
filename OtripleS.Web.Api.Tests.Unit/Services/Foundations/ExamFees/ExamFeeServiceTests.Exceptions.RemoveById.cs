61656b8bc5da4562db3464de77894453fe80475e
Hassan Habib <hassanhabib@live.com>
2022-02-10 16:50:21 -0800

    CODE RUB: Enforce One-Liner Rule for Exam Fees

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/ExamFees/ExamFeeServiceTests.Exceptions.RemoveById.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/ExamFees/ExamFeeServiceTests.Exceptions.RemoveById.cs
@@ -24,7 +24,8 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.ExamFees
             Guid inputExamFeeId = randomExamFeeId;
             SqlException sqlException = GetSqlException();
 
-            var expectedExamFeeDependencyException = new ExamFeeDependencyException(sqlException);
+            var expectedExamFeeDependencyException =
+                new ExamFeeDependencyException(sqlException);
 
             this.storageBrokerMock.Setup(broker =>
                  broker.SelectExamFeeByIdAsync(inputExamFeeId))
