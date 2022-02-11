d7a22b1e18b99d466d437cafb00fbfeb9180a419
Tharani <tharanisantosh@outlook.com>
2022-02-11 08:29:28 +0200

    ShouldThrowServiceExceptionOnAddWhenExceptionOccursAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api/Models/TeacherContacts/Exceptions/FailedTeacherContactServiceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xeptions;
+
+namespace OtripleS.Web.Api.Models.TeacherContacts.Exceptions
+{
+    public class FailedTeacherContactServiceException : Xeption
+    {
+        public FailedTeacherContactServiceException(Exception innerException)
+            : base(message: "Failed teacher contact service error occured.", innerException)
+        { 
+        }
+    }
+}
