cd66898d44a1090bba19a72172f9b16e1e1167c7
Janane <imjanane@gmail.com>
2022-02-12 15:49:07 -0600

    ShouldThrowServiceExceptionOnCreateWhenExceptionOccursAndLogItAsync - > FAIL

---
+++ OtripleS.Web.Api/Models/Attendances/Exceptions/FailedAttendanceServiceException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xeptions;
+
+namespace OtripleS.Web.Api.Models.Attendances.Exceptions
+{
+    public class FailedAttendanceServiceException :Xeption
+    {
+        public FailedAttendanceServiceException(Exception innerException)
+            : base("Failed Attendance Service Exception, contact support", innerException)
+        {}
+    }
+}
