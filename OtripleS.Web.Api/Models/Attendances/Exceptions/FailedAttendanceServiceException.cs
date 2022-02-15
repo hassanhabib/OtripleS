37eb63e008edae0e77e6727510e24b1cfd214a0f
Janane <imjanane@gmail.com>
2022-02-15 15:40:26 -0600

    CODE RUB : Code CleanUp

--- OtripleS.Web.Api/Models/Attendances/Exceptions/FailedAttendanceServiceException.cs
+++ OtripleS.Web.Api/Models/Attendances/Exceptions/FailedAttendanceServiceException.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
 using Xeptions;
 
 namespace OtripleS.Web.Api.Models.Attendances.Exceptions
 {
-    public class FailedAttendanceServiceException :Xeption
+    public class FailedAttendanceServiceException : Xeption
     {
         public FailedAttendanceServiceException(Exception innerException)
-            : base("Failed Attendance Service Exception, contact support", innerException)
-        {}
+            : base(message: "Failed attendance service exception, contact support.", innerException)
+        { }
     }
 }
