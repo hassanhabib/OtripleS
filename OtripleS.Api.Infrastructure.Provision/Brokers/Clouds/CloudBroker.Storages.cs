8f225aab79a3213fe772095256e555dd45d68b2b
Tharani <tharanisantosh@outlook.com>
2022-02-15 20:02:31 +0200

    CODE RUB: Code Cleanup

--- OtripleS.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.Storages.cs
+++ OtripleS.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.Storages.cs
@@ -1,6 +1,6 @@
 ﻿// ---------------------------------------------------------------
-//  Copyright (c) Coalition of the Good-Hearted Engineers 
-// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR 
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
 
 using System.Threading.Tasks;
