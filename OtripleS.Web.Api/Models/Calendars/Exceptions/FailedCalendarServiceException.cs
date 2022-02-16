e3540948af5b418b53a4675675aa38d535fd1ca0
Janane <imjanane@gmail.com>
2022-02-16 02:48:39 -0600

    ShouldThrowServiceExceptionOnAddWhenExceptionOccursAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api/Models/Calendars/Exceptions/FailedCalendarServiceException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace OtripleS.Web.Api.Models.Calendars.Exceptions
+{
+    public class FailedCalendarServiceException : Xeption
+    {
+        public FailedCalendarServiceException(Exception innerException)
+            : base(message: "Failed calendar service error occured, contact support", innerException)
+             { }
+    }
+}
