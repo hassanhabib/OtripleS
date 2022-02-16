028eb26e6415e0d0ebe335dc7fb7cec76d879c11
Janane <imjanane@gmail.com>
2022-02-15 19:22:55 -0600

    ShouldThrowServiceExceptionOnAddWhenExceptionOccursAndLogItAsync ->FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/CalendarEntryAttachments/CalendarEntryAttachmentServiceTests.Exceptions.Add.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/CalendarEntryAttachments/CalendarEntryAttachmentServiceTests.Exceptions.Add.cs
@@ -94,8 +94,11 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.CalendarEntryAttachme
             CalendarEntryAttachment someCalendarEntryAttachment = CreateRandomCalendarEntryAttachment();
             var serviceException = new Exception();
 
+            var failedCalendarEntryAttachment =
+                new FailedCalendarEntryAttachment(serviceException);
+
             var expectedCalendarEntryAttachmentServiceException =
-                new CalendarEntryAttachmentServiceException(serviceException);
+                new CalendarEntryAttachmentServiceException(failedCalendarEntryAttachment);
 
             this.storageBrokerMock.Setup(broker =>
                 broker.InsertCalendarEntryAttachmentAsync(someCalendarEntryAttachment))
