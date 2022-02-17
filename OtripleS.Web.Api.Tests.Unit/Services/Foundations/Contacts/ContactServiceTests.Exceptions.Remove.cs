26d6a977c43ece51c2c00f39298802a5a3d4c07c
Janane <imjanane@gmail.com>
2022-02-17 00:04:46 -0600

    ShouldThrowServiceExceptionOnDeleteWhenExceptionOccursAndLogItAsync -> PASS

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Contacts/ContactServiceTests.Exceptions.Remove.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Contacts/ContactServiceTests.Exceptions.Remove.cs
@@ -151,8 +151,11 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Contacts
             Guid inputContactId = randomContactId;
             var serviceException = new Exception();
 
+            var failedContactServiceException =
+                new FailedContactServiceException(serviceException);
+
             var expectedContactServiceException =
-                new ContactServiceException(serviceException);
+                new ContactServiceException(failedContactServiceException);
 
             this.storageBrokerMock.Setup(broker =>
                 broker.SelectContactByIdAsync(inputContactId))
