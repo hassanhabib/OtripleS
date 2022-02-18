9222f0417522017080030898ed9cff77dfd6e880
Janane <imjanane@gmail.com>
2022-02-18 02:15:53 -0600

    ShouldThrowServiceExceptionOnRetrieveAllWhenExceptionOccursAndLogIt -> FAIL

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Classrooms/ClassroomServiceTests.Exceptions.RetrieveAll.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Classrooms/ClassroomServiceTests.Exceptions.RetrieveAll.cs
@@ -59,8 +59,11 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Classrooms
             // given
             var serviceException = new Exception();
 
+            var failedClassroomServiceException =
+                new FailedClassroomServiceException(serviceException);
+
             var expectedClassroomServiceException =
-                new ClassroomServiceException(serviceException);
+                new ClassroomServiceException(failedClassroomServiceException);
 
             this.storageBrokerMock.Setup(broker =>
                 broker.SelectAllClassrooms())
