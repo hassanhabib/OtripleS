be6d879dc917a326241cc2d8f37d88986a61882d
Janane <imjanane@gmail.com>
2022-02-19 16:50:46 -0600

    ShouldThrowServiceExceptionOnCreateWhenExceptionOccursAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api/Models/ExamFees/Exceptions/FailedExamFeeServiceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xeptions;
+
+namespace OtripleS.Web.Api.Models.ExamFees.Exceptions
+{
+    public class FailedExamFeeServiceException : Xeption
+    {
+        public FailedExamFeeServiceException(Exception innerException)
+            : base(message: " Failed exam fee service error occured, contact support",
+                  innerException)
+        { }
+    }
+}
