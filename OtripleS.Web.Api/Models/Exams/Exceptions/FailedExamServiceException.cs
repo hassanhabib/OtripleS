67ba8467ee6abdb046e473838699164da9b3a5f8
Janane <imjanane@gmail.com>
2022-02-19 17:16:20 -0600

    ShouldThrowServiceExceptionOnAddWhenExceptionOccursAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api/Models/Exams/Exceptions/FailedExamServiceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xeptions;
+
+namespace OtripleS.Web.Api.Models.Exams.Exceptions
+{
+    public class FailedExamServiceException : Xeption
+    {
+        public FailedExamServiceException(Exception innerException)
+            : base(message:"Failed exam service error occured, contact support",
+                  innerException)
+        { }
+    }
+}
