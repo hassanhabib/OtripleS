eb8827e2fc7d1ec32ff7b378d7e8130e56c422c8
Janane <imjanane@gmail.com>
2022-02-19 17:30:04 -0600

    CODE RUB : Code Cleanup

--- OtripleS.Web.Api/Models/Exams/Exceptions/FailedExamServiceException.cs
+++ OtripleS.Web.Api/Models/Exams/Exceptions/FailedExamServiceException.cs
@@ -6,7 +6,7 @@ namespace OtripleS.Web.Api.Models.Exams.Exceptions
     public class FailedExamServiceException : Xeption
     {
         public FailedExamServiceException(Exception innerException)
-            : base(message:"Failed exam service error occured, contact support",
+            : base(message: "Failed exam service error occured, contact support",
                   innerException)
         { }
     }
