a5fee04cf87cf9e4d1c268983ac318765923dfa0
Janane <imjanane@gmail.com>
2022-02-19 16:36:04 -0600

    ShouldThrowServiceExceptionOnAddWhenExceptionOccursAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api/Models/ExamAttachments/Exceptions/FailedExamAttachmentServiceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xeptions;
+
+namespace OtripleS.Web.Api.Models.ExamAttachments.Exceptions
+{
+    public class FailedExamAttachmentServiceException: Xeption
+    {
+        public FailedExamAttachmentServiceException(Exception innerException)
+            : base(message: "Failed exam attachment service error occured, contact support",
+                  innerException)
+        { }
+    }
+}
