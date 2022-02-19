9e7bb2a0e16f1ee3306f1c4a38c7e8c1c8393074
Janane <imjanane@gmail.com>
2022-02-19 16:13:51 -0600

    ShouldThrowServiceExceptionOnCreateWhenExceptionOccursAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api/Models/Courses/Exceptions/FailedCourseServiceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xeptions;
+
+namespace OtripleS.Web.Api.Models.Courses.Exceptions
+{
+    public class FailedCourseServiceException:Xeption
+    {
+        public FailedCourseServiceException(Exception innerException)
+            : base(message: " Failed course service error occured, contact support.",
+                  innerException)
+        { }
+    }
+}
