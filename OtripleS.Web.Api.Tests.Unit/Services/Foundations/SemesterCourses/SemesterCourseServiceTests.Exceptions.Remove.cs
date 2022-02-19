a162330282dcf59104e03b83ad306d115275b419
Tharani <tharanisantosh@outlook.com>
2022-02-19 06:40:01 +0200

    CODE RUB : Comment Resolved

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/SemesterCourses/SemesterCourseServiceTests.Exceptions.Remove.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/SemesterCourses/SemesterCourseServiceTests.Exceptions.Remove.cs
@@ -135,7 +135,8 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.SemesterCourses
             var failedSemesterCourseServiceException =
                 new FailedSemesterCourseServiceException(serviceException);
 
-            var expectedSemesterCourseServiceException = new SemesterCourseServiceException(failedSemesterCourseServiceException);
+            var expectedSemesterCourseServiceException = 
+                new SemesterCourseServiceException(failedSemesterCourseServiceException);
 
             this.storageBrokerMock.Setup(broker =>
                  broker.SelectSemesterCourseByIdAsync(inputSemesterCourseId))
