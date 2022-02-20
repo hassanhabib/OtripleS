2c1b684027213ea3fb7bf43b8aa5493747279aff
Janane <imjanane@gmail.com>
2022-02-19 22:19:08 -0600

    CODE RUB : Added copyrights header

--- OtripleS.Web.Api/Models/ExamAttachments/Exceptions/FailedExamAttachmentServiceException.cs
+++ OtripleS.Web.Api/Models/ExamAttachments/Exceptions/FailedExamAttachmentServiceException.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
 using Xeptions;
 
 namespace OtripleS.Web.Api.Models.ExamAttachments.Exceptions
