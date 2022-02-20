8ff9d312224d3d14ceb2936fe7792102ea63a48f
Tharani <tharanisantosh@outlook.com>
2022-02-20 06:44:46 +0200

    CODE RUB : Comments Resolved

--- OtripleS.Web.Api/Models/Guardians/Exceptions/FailedGuardianServiceException.cs
+++ OtripleS.Web.Api/Models/Guardians/Exceptions/FailedGuardianServiceException.cs
@@ -11,9 +11,8 @@ namespace OtripleS.Web.Api.Models.Guardians.Exceptions
     public class FailedGuardianServiceException : Xeption
     {
         public FailedGuardianServiceException(Exception innerException)
-            : base(message: "Failed Guardian service error occurred, contact support.", innerException)
+            : base(message: "Failed guardian service error occurred, contact support.", innerException)
         {
         }
-
     }
 }
