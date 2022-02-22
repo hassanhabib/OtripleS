3f5aba90d536def15ac208a30bd745a5abfb7560
Janane <imjanane@gmail.com>
2022-02-22 17:49:41 -0600

    ShouldThrowServiceExceptionOnAddWhenExceptionOccursAndLogItAsync -> FAIL

---
+++ OtripleS.Web.Api/Models/Fees/Exceptions/FailedFeeServiceException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+using System;
+using Xeptions;
+
+namespace OtripleS.Web.Api.Models.Fees.Exceptions
+{
+    public class FailedFeeServiceException : Xeption
+    {
+        public FailedFeeServiceException(Exception innerException)
+            : base(message: " Failed fee service error occurred, contact support",
+                  innerException)
+        { }
+    }
+}
