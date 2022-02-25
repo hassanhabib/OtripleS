2730abf384cfb38082d4b76d36fc31e30b2b3cce
Janane <imjanane@gmail.com>
2022-02-25 13:11:38 -0600

    CODE RUB : Separte Logic Tests for RetrievelAll

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/AssignmentAttachments/AssignmentAttachmentServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,52 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.AssignmentAttachments;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.AssignmentAttachments
+{
+    public partial class AssignmentAttachmentServiceTests
+    {
+        [Fact]
+        public void ShouldRetrieveAllAssignmentAttachments()
+        {
+            // given
+            IQueryable<AssignmentAttachment> randomAssignmentAttachments =
+                CreateRandomAssignmentAttachments();
+
+            IQueryable<AssignmentAttachment> storageAssignmentAttachments =
+                randomAssignmentAttachments;
+
+            IQueryable<AssignmentAttachment> expectedAssignmentAttachments =
+                storageAssignmentAttachments;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllAssignmentAttachments())
+                    .Returns(storageAssignmentAttachments);
+
+            // when
+            IQueryable<AssignmentAttachment> actualAssignmentAttachments =
+                this.assignmentAttachmentService.RetrieveAllAssignmentAttachments();
+
+            // then
+            actualAssignmentAttachments.Should().BeEquivalentTo(expectedAssignmentAttachments);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllAssignmentAttachments(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+       
+    }
+}
