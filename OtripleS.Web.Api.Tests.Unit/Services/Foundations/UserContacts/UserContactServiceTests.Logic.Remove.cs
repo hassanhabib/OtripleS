8e4b1fe6865dbec52a6b7a4b68e84df59ac3c683
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-03 11:15:15 -0800

    CODE RUB: Split Logic files for Remove User Contacts operations.

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/UserContacts/UserContactServiceTests.Logic.Remove.cs
@@ -0,0 +1,62 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.UserContacts;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.UserContacts
+{
+    public partial class UserContactServiceTests
+    {
+        [Fact]
+        public async Task ShouldRemoveUserContactAsync()
+        {
+            // given
+            var randomUserId = Guid.NewGuid();
+            var randomContactId = Guid.NewGuid();
+            Guid inputUserId = randomUserId;
+            Guid inputContactId = randomContactId;
+            UserContact randomUserContact = CreateRandomUserContact();
+            randomUserContact.UserId = inputUserId;
+            randomUserContact.ContactId = inputContactId;
+            UserContact storageUserContact = randomUserContact;
+            UserContact expectedUserContact = storageUserContact;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectUserContactByIdAsync(inputUserId, inputContactId))
+                    .ReturnsAsync(storageUserContact);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.DeleteUserContactAsync(storageUserContact))
+                    .ReturnsAsync(expectedUserContact);
+
+            // when
+            UserContact actualUserContact =
+                await this.userContactService.RemoveUserContactByIdAsync(inputUserId, inputContactId);
+
+            // then
+            actualUserContact.Should().BeEquivalentTo(expectedUserContact);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectUserContactByIdAsync(inputUserId, inputContactId),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteUserContactAsync(storageUserContact),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/UserContacts/UserContactServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/UserContacts/UserContactServiceTests.Logic.cs
@@ -42,48 +42,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.UserContacts
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
-
-        [Fact]
-        public async Task ShouldRemoveUserContactAsync()
-        {
-            // given
-            var randomUserId = Guid.NewGuid();
-            var randomContactId = Guid.NewGuid();
-            Guid inputUserId = randomUserId;
-            Guid inputContactId = randomContactId;
-            UserContact randomUserContact = CreateRandomUserContact();
-            randomUserContact.UserId = inputUserId;
-            randomUserContact.ContactId = inputContactId;
-            UserContact storageUserContact = randomUserContact;
-            UserContact expectedUserContact = storageUserContact;
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectUserContactByIdAsync(inputUserId, inputContactId))
-                    .ReturnsAsync(storageUserContact);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.DeleteUserContactAsync(storageUserContact))
-                    .ReturnsAsync(expectedUserContact);
-
-            // when
-            UserContact actualUserContact =
-                await this.userContactService.RemoveUserContactByIdAsync(inputUserId, inputContactId);
-
-            // then
-            actualUserContact.Should().BeEquivalentTo(expectedUserContact);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectUserContactByIdAsync(inputUserId, inputContactId),
-                    Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.DeleteUserContactAsync(storageUserContact),
-                    Times.Once);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
-
+        
         [Fact]
         public void ShouldRetrieveAllUserContacts()
         {
