6878bd862f3ea9d9d5f945ed3a06b0d3d757cc64
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-03 15:27:19 -0800

    CODE RUB: Split Logic files for RetrieveBy Id teacher Contacts operations

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherContacts/TeacherContactServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherContacts/TeacherContactServiceTests.Logic.cs
@@ -107,35 +107,6 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.TeacherContacts
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public async Task ShouldRetrieveTeacherContactById()
-        {
-            // given
-            DateTimeOffset inputDateTime = GetRandomDateTime();
-            TeacherContact randomTeacherContact = CreateRandomTeacherContact(inputDateTime);
-            TeacherContact storageTeacherContact = randomTeacherContact;
-            TeacherContact expectedTeacherContact = storageTeacherContact;
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectTeacherContactByIdAsync(randomTeacherContact.TeacherId, randomTeacherContact.ContactId))
-                    .ReturnsAsync(randomTeacherContact);
-
-            // when
-            TeacherContact actualTeacherContact = await
-                this.teacherContactService.RetrieveTeacherContactByIdAsync(
-                    randomTeacherContact.TeacherId, randomTeacherContact.ContactId);
-
-            // then
-            actualTeacherContact.Should().BeEquivalentTo(expectedTeacherContact);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectTeacherContactByIdAsync(randomTeacherContact.TeacherId, randomTeacherContact.ContactId),
-                    Times.Once);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
+        }        
     }
 }
