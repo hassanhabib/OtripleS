3af0b12037da8308bae65f6870f34f6525f7c8f9
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-03 17:57:23 -0800

    CODE RUB: Split logic files for Retrieve all Teacher Attachment operations

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherAttachments/TeacherAttachmentServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherAttachments/TeacherAttachmentServiceTests.Logic.cs
@@ -42,35 +42,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.TeacherAttachments
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public void ShouldRetrieveAllTeacherAttachments()
-        {
-            // given
-            IQueryable<TeacherAttachment> randomTeacherAttachments = CreateRandomTeacherAttachments();
-            IQueryable<TeacherAttachment> storageTeacherAttachments = randomTeacherAttachments;
-            IQueryable<TeacherAttachment> expectedTeacherAttachments = storageTeacherAttachments;
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectAllTeacherAttachments())
-                    .Returns(storageTeacherAttachments);
-
-            // when
-            IQueryable<TeacherAttachment> actualTeacherAttachments =
-                this.teacherAttachmentService.RetrieveAllTeacherAttachments();
-
-            // then
-            actualTeacherAttachments.Should().BeEquivalentTo(expectedTeacherAttachments);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectAllTeacherAttachments(),
-                    Times.Once);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
+        }        
 
         [Fact]
         public async Task ShouldRetrieveTeacherAttachmentByIdAsync()
