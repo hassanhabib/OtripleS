7ee5aeca1069c77667955aec3d582e8e8c24c80e
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-03 18:26:21 -0800

    CODE RUB: Split logic files for retrieve by id teacher attachment operations

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherAttachments/TeacherAttachmentServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherAttachments/TeacherAttachmentServiceTests.Logic.cs
@@ -70,38 +70,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.TeacherAttachments
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public async Task ShouldRetrieveTeacherAttachmentByIdAsync()
-        {
-            // given
-            TeacherAttachment randomTeacherAttachment = CreateRandomTeacherAttachment();
-            TeacherAttachment storageTeacherAttachment = randomTeacherAttachment;
-            TeacherAttachment expectedTeacherAttachment = storageTeacherAttachment;
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectTeacherAttachmentByIdAsync
-                (randomTeacherAttachment.TeacherId, randomTeacherAttachment.AttachmentId))
-                    .ReturnsAsync(storageTeacherAttachment);
-
-            // when
-            TeacherAttachment actualTeacherAttachment = await
-                this.teacherAttachmentService.RetrieveTeacherAttachmentByIdAsync(
-                    randomTeacherAttachment.TeacherId, randomTeacherAttachment.AttachmentId);
-
-            // then
-            actualTeacherAttachment.Should().BeEquivalentTo(expectedTeacherAttachment);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectTeacherAttachmentByIdAsync
-                (randomTeacherAttachment.TeacherId, randomTeacherAttachment.AttachmentId),
-                    Times.Once);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
+        }        
 
         [Fact]
         public async Task ShouldRemoveTeacherAttachmentAsync()
