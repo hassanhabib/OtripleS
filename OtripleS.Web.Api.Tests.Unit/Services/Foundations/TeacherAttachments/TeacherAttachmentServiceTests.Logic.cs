0373d2d2dd3b6618f232dd43059a50bb474e3deb
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-03 17:49:22 -0800

    CODE RUB: Split logic files for remove teacher attachments operations

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherAttachments/TeacherAttachmentServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherAttachments/TeacherAttachmentServiceTests.Logic.cs
@@ -102,48 +102,6 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.TeacherAttachments
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
-
-        [Fact]
-        public async Task ShouldRemoveTeacherAttachmentAsync()
-        {
-            // given
-            var randomTeacherId = Guid.NewGuid();
-            var randomAttachmentId = Guid.NewGuid();
-            Guid inputTeacherId = randomTeacherId;
-            Guid inputAttachmentId = randomAttachmentId;
-            TeacherAttachment randomTeacherAttachment = CreateRandomTeacherAttachment();
-            randomTeacherAttachment.TeacherId = inputTeacherId;
-            randomTeacherAttachment.AttachmentId = inputAttachmentId;
-            TeacherAttachment storageTeacherAttachment = randomTeacherAttachment;
-            TeacherAttachment expectedTeacherAttachment = storageTeacherAttachment;
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectTeacherAttachmentByIdAsync(inputTeacherId, inputAttachmentId))
-                    .ReturnsAsync(storageTeacherAttachment);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.DeleteTeacherAttachmentAsync(storageTeacherAttachment))
-                    .ReturnsAsync(expectedTeacherAttachment);
-
-            // when
-            TeacherAttachment actualTeacherAttachment =
-                await this.teacherAttachmentService.RemoveTeacherAttachmentByIdAsync(
-                    inputTeacherId, inputAttachmentId);
-
-            // then
-            actualTeacherAttachment.Should().BeEquivalentTo(expectedTeacherAttachment);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectTeacherAttachmentByIdAsync(inputTeacherId, inputAttachmentId),
-                    Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.DeleteTeacherAttachmentAsync(storageTeacherAttachment),
-                    Times.Once);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
+        
     }
 }
