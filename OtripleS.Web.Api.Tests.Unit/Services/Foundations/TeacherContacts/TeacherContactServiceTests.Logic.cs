dab868104b2bc67efebb97ba3defda8abb5a009b
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-06 14:15:53 -0800

    CODE RUB: Split logic files for teacher contacts retrieve all operations

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherContacts/TeacherContactServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherContacts/TeacherContactServiceTests.Logic.cs
@@ -41,30 +41,6 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.TeacherContacts
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public void ShouldRetrieveAllTeacherContacts()
-        {
-            //given
-            IQueryable<TeacherContact> randomSemesterCourses = CreateRandomTeacherContacts();
-            IQueryable<TeacherContact> storageTeacherContacts = randomSemesterCourses;
-            IQueryable<TeacherContact> expectedTeacherContacts = storageTeacherContacts;
-
-            this.storageBrokerMock.Setup(broker => broker.SelectAllTeacherContacts())
-                .Returns(storageTeacherContacts);
-
-            // when
-            IQueryable<TeacherContact> actualTeacherContacts =
-                this.teacherContactService.RetrieveAllTeacherContacts();
-
-            actualTeacherContacts.Should().BeEquivalentTo(expectedTeacherContacts);
-
-            this.storageBrokerMock.Verify(broker => broker.SelectAllTeacherContacts(),
-                Times.Once);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
         }        
 
         [Fact]
