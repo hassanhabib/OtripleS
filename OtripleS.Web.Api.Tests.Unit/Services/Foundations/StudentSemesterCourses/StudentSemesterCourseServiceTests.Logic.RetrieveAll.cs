e6b6838191678d55f7eb722d419717399acdd5f4
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-06 15:20:32 -0800

    CODE RUB: Split logic files for student semester courses Retrieve all operations

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/StudentSemesterCourses/StudentSemesterCourseServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.StudentSemesterCourses;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.StudentSemesterCourses
+{
+    public partial class StudentSemesterCourseServiceTests
+    {
+        [Fact]
+        public void ShouldRetrieveAllStudentSemesterCourses()
+        {
+            //given
+            IQueryable<StudentSemesterCourse> randomSemesterCourses =
+                CreateRandomStudentSemesterCourses();
+
+            IQueryable<StudentSemesterCourse> storageStudentSemesterCourses = randomSemesterCourses;
+            IQueryable<StudentSemesterCourse> expectedStudentSemesterCourses = storageStudentSemesterCourses;
+
+            this.storageBrokerMock.Setup(broker => broker.SelectAllStudentSemesterCourses())
+                .Returns(storageStudentSemesterCourses);
+
+            // when
+            IQueryable<StudentSemesterCourse> actualStudentSemesterCourses =
+                this.studentSemesterCourseService.RetrieveAllStudentSemesterCourses();
+
+            actualStudentSemesterCourses.Should().BeEquivalentTo(expectedStudentSemesterCourses);
+
+            this.storageBrokerMock.Verify(broker => broker.SelectAllStudentSemesterCourses(),
+                Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
