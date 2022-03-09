877e049e0517f1381c42386fb1e0f42ee55d8f95
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-08 18:57:01 -0800

    CODE RUB: Split logic files for Create Assignment Operations

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Assignments/AssignmentServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Assignments/AssignmentServiceTests.Logic.cs
@@ -15,48 +15,7 @@ using Xunit;
 namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Assignments
 {
     public partial class AssignmentServiceTests
-    {
-        [Fact]
-        public async Task ShouldCreateAssignmentAsync()
-        {
-            // given
-            DateTimeOffset randomDateTime = GetRandomDateTime();
-            DateTimeOffset dateTime = randomDateTime;
-            Assignment randomAssignment = CreateRandomAssignment(randomDateTime);
-            randomAssignment.UpdatedBy = randomAssignment.CreatedBy;
-            randomAssignment.UpdatedDate = randomAssignment.CreatedDate;
-            Assignment inputAssignment = randomAssignment;
-            Assignment storageAssignment = randomAssignment;
-            Assignment expectedAssignment = storageAssignment;
-
-            this.dateTimeBrokerMock.Setup(broker =>
-                broker.GetCurrentDateTime())
-                    .Returns(dateTime);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.InsertAssignmentAsync(inputAssignment))
-                    .ReturnsAsync(storageAssignment);
-
-            // when
-            Assignment actualAssignment =
-                await this.assignmentService.CreateAssignmentAsync(inputAssignment);
-
-            // then
-            actualAssignment.Should().BeEquivalentTo(expectedAssignment);
-
-            this.dateTimeBrokerMock.Verify(broker =>
-                broker.GetCurrentDateTime(),
-                    Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.InsertAssignmentAsync(inputAssignment),
-                    Times.Once);
-
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
-
+    {       
         [Fact]
         public async Task ShouldModifyAssignmentAsync()
         {
