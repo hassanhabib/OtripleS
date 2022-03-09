1bd1811b9973cf6ea87113d894d25aeaa2f8862b
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-09 10:24:55 -0800

    CODE RUB: Split logic files for retrieveall teachers operations

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Teachers/TeacherServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Teachers/TeacherServiceTests.Logic.cs
@@ -83,39 +83,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Teachers
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public void ShouldRetrieveAllTeachers()
-        {
-            // given
-            IQueryable<Teacher> randomTeachers = CreateRandomTeachers();
-            IQueryable<Teacher> storageTeachers = randomTeachers;
-            IQueryable<Teacher> expectedTeachers = storageTeachers;
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectAllTeachers())
-                    .Returns(storageTeachers);
-
-            // when
-            IQueryable<Teacher> actualTeachers =
-                this.teacherService.RetrieveAllTeachers();
-
-            // then
-            actualTeachers.Should().BeEquivalentTo(expectedTeachers);
-
-            this.dateTimeBrokerMock.Verify(broker =>
-                broker.GetCurrentDateTime(),
-                    Times.Never);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectAllTeachers(),
-                    Times.Once);
-
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
+        }        
 
         [Fact]
         public async Task ShouldCreateTeacherAsync()
