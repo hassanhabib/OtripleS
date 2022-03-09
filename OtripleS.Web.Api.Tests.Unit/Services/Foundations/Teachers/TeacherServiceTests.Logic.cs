bca39cf442a85b53a4b1ecba5ba72f3a248f0568
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-09 09:57:59 -0800

    CODE RUB: Split logic files for delete teachers operations

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Teachers/TeacherServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Teachers/TeacherServiceTests.Logic.cs
@@ -15,45 +15,7 @@ using Xunit;
 namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Teachers
 {
     public partial class TeacherServiceTests
-    {
-        [Fact]
-        public async Task ShouldDeleteTeacherAsync()
-        {
-            // given
-            DateTimeOffset dateTime = GetRandomDateTime();
-            Teacher randomTeacher = CreateRandomTeacher(dateTime);
-            Guid inputTeacherId = randomTeacher.Id;
-            Teacher inputTeacher = randomTeacher;
-            Teacher storageTeacher = randomTeacher;
-            Teacher expectedTeacher = randomTeacher;
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectTeacherByIdAsync(inputTeacherId))
-                    .ReturnsAsync(inputTeacher);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.DeleteTeacherAsync(inputTeacher))
-                    .ReturnsAsync(storageTeacher);
-
-            // when
-            Teacher actualTeacher =
-                await this.teacherService.RemoveTeacherByIdAsync(inputTeacherId);
-
-            // then
-            actualTeacher.Should().BeEquivalentTo(expectedTeacher);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectTeacherByIdAsync(inputTeacherId),
-                    Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.DeleteTeacherAsync(inputTeacher),
-                    Times.Once);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
+    {        
 
         [Fact]
         public async Task ShouldRetrieveTeacherByIdAsync()
