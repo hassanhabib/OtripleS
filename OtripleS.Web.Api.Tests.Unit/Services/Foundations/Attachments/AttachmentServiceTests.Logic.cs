06a98fc18f5759029cc19c75637649052176e3ba
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-09 13:52:42 -0800

    CODe RUB: Split Logic files for Modify attachments Operations

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Attachments/AttachmentServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Attachments/AttachmentServiceTests.Logic.cs
@@ -58,65 +58,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Attachments
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public async Task ShouldModifyAttachmentAsync()
-        {
-            // given
-            int randomNumber = GetRandomNumber();
-            int randomDays = randomNumber;
-            DateTimeOffset randomDate = GetRandomDateTime();
-            DateTimeOffset randomInputDate = GetRandomDateTime();
-
-            Attachment randomAttachment =
-                CreateRandomAttachment(randomInputDate);
-
-            Attachment inputAttachment = randomAttachment;
-            Attachment afterUpdateStorageAttachment = inputAttachment;
-            Attachment expectedAttachment = afterUpdateStorageAttachment;
-
-            Attachment beforeUpdateStorageAttachment =
-                randomAttachment.DeepClone();
-
-            inputAttachment.UpdatedDate = randomDate;
-            Guid attachmentId = inputAttachment.Id;
-
-            this.dateTimeBrokerMock.Setup(broker =>
-                broker.GetCurrentDateTime())
-                    .Returns(randomDate);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectAttachmentByIdAsync(attachmentId))
-                    .ReturnsAsync(beforeUpdateStorageAttachment);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.UpdateAttachmentAsync(inputAttachment))
-                    .ReturnsAsync(afterUpdateStorageAttachment);
-
-            // when
-            Attachment actualAttachment =
-                await this.attachmentService.ModifyAttachmentAsync(inputAttachment);
-
-            // then
-            actualAttachment.Should().BeEquivalentTo(expectedAttachment);
-
-            this.dateTimeBrokerMock.Verify(broker =>
-                broker.GetCurrentDateTime(),
-                    Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectAttachmentByIdAsync(attachmentId),
-                    Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.UpdateAttachmentAsync(inputAttachment),
-                    Times.Once);
-
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
+        }        
 
         [Fact]
         public void ShouldRetrieveAllAttachments()
