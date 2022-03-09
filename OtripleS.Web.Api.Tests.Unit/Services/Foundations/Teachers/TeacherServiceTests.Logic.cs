0e423387ff82f11a17514f6aef8f7e4408804293
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-09 11:00:21 -0800

    CODE RUB: Split logic files for Create Teacher

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Teachers/TeacherServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Teachers/TeacherServiceTests.Logic.cs
@@ -115,47 +115,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Teachers
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public async Task ShouldCreateTeacherAsync()
-        {
-            // given
-            DateTimeOffset dateTime = DateTimeOffset.UtcNow;
-            Teacher randomTeacher = CreateRandomTeacher(dateTime);
-            randomTeacher.UpdatedBy = randomTeacher.CreatedBy;
-            randomTeacher.UpdatedDate = randomTeacher.CreatedDate;
-            Teacher inputTeacher = randomTeacher;
-            Teacher storageTeacher = randomTeacher;
-            Teacher expectedTeacher = randomTeacher;
-
-            this.dateTimeBrokerMock.Setup(broker =>
-               broker.GetCurrentDateTime())
-                   .Returns(dateTime);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.InsertTeacherAsync(inputTeacher))
-                    .ReturnsAsync(storageTeacher);
-
-            // when
-            Teacher actualTeacher =
-                await this.teacherService.CreateTeacherAsync(inputTeacher);
-
-            // then
-            actualTeacher.Should().BeEquivalentTo(expectedTeacher);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.InsertTeacherAsync(inputTeacher),
-                    Times.Once);
-
-            this.dateTimeBrokerMock.Verify(broker =>
-                broker.GetCurrentDateTime(),
-                    Times.Once);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
+        }        
 
         [Fact]
         public async Task ShouldModifyTeacherAsync()
