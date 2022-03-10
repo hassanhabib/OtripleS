2f5bfc716a48bb2e033ee532cbaa673f26f01624
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-10 13:39:36 -0800

    CODE RUB: Split logic files for Remove Calendar Entry Attachment Operations

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/CalendarEntryAttachments/CalendarEntryAttachmentServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/CalendarEntryAttachments/CalendarEntryAttachmentServiceTests.Logic.cs
@@ -106,49 +106,6 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.CalendarEntryAttachme
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public async Task ShouldRemoveCalendarEntryAttachmentAsync()
-        {
-            // given
-            var randomCalendarEntryId = Guid.NewGuid();
-            var randomAttachmentId = Guid.NewGuid();
-            Guid inputCalendarEntryId = randomCalendarEntryId;
-            Guid inputAttachmentId = randomAttachmentId;
-            CalendarEntryAttachment randomCalendarEntryAttachment = CreateRandomCalendarEntryAttachment();
-            randomCalendarEntryAttachment.CalendarEntryId = inputCalendarEntryId;
-            randomCalendarEntryAttachment.AttachmentId = inputAttachmentId;
-            CalendarEntryAttachment storageCalendarEntryAttachment = randomCalendarEntryAttachment;
-            CalendarEntryAttachment expectedCalendarEntryAttachment = storageCalendarEntryAttachment;
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectCalendarEntryAttachmentByIdAsync(inputCalendarEntryId, inputAttachmentId))
-                    .ReturnsAsync(storageCalendarEntryAttachment);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.DeleteCalendarEntryAttachmentAsync(storageCalendarEntryAttachment))
-                    .ReturnsAsync(expectedCalendarEntryAttachment);
-
-            // when
-            CalendarEntryAttachment actualCalendarEntryAttachment =
-                await this.calendarEntryAttachmentService.RemoveCalendarEntryAttachmentByIdAsync(
-                    inputCalendarEntryId, inputAttachmentId);
-
-            // then
-            actualCalendarEntryAttachment.Should().BeEquivalentTo(expectedCalendarEntryAttachment);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectCalendarEntryAttachmentByIdAsync(inputCalendarEntryId, inputAttachmentId),
-                    Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.DeleteCalendarEntryAttachmentAsync(storageCalendarEntryAttachment),
-                    Times.Once);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
+        }        
     }
 }
