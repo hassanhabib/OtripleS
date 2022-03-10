a9587a01428ca6f1ffc677eb3a181bb39ccb494d
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-10 12:18:28 -0800

    CODE RUB: Split logic files for Delete Calendar Entry Operations.

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/CalendarEntries/CalendarEntryServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/CalendarEntries/CalendarEntryServiceTests.Logic.cs
@@ -173,45 +173,6 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.CalendarEntries
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public async Task ShouldDeleteCalendarEntryByIdAsync()
-        {
-            // given
-            DateTimeOffset dateTime = GetRandomDateTime();
-            CalendarEntry randomCalendarEntry = CreateRandomCalendarEntry(dateTime);
-            Guid inputCalendarEntryId = randomCalendarEntry.Id;
-            CalendarEntry inputCalendarEntry = randomCalendarEntry;
-            CalendarEntry storageCalendarEntry = randomCalendarEntry;
-            CalendarEntry expectedCalendarEntry = randomCalendarEntry;
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectCalendarEntryByIdAsync(inputCalendarEntryId))
-                    .ReturnsAsync(inputCalendarEntry);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.DeleteCalendarEntryAsync(inputCalendarEntry))
-                    .ReturnsAsync(storageCalendarEntry);
-
-            // when
-            CalendarEntry actualCalendarEntry =
-                await this.calendarEntryService.RemoveCalendarEntryByIdAsync(inputCalendarEntryId);
-
-            // then
-            actualCalendarEntry.Should().BeEquivalentTo(expectedCalendarEntry);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectCalendarEntryByIdAsync(inputCalendarEntryId),
-                    Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.DeleteCalendarEntryAsync(inputCalendarEntry),
-                    Times.Once);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
+        }        
     }
 }
