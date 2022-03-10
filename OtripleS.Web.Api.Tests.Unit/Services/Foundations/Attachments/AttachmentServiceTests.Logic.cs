3570393da037612e77ac9ea72c68973be6a9788d
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-09 17:26:57 -0800

    CODE RUB: Split Logic files for RetrieveAll Attachments Operations

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Attachments/AttachmentServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Attachments/AttachmentServiceTests.Logic.cs
@@ -116,35 +116,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Attachments
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
-        public void ShouldRetrieveAllAttachments()
-        {
-            // given
-            IQueryable<Attachment> randomAttachments = CreateRandomAttachments();
-            IQueryable<Attachment> storageAttachments = randomAttachments;
-            IQueryable<Attachment> expectedAttachments = storageAttachments;
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectAllAttachments())
-                    .Returns(storageAttachments);
-
-            // when
-            IQueryable<Attachment> actualAttachments =
-                this.attachmentService.RetrieveAllAttachments();
-
-            // then
-            actualAttachments.Should().BeEquivalentTo(expectedAttachments);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectAllAttachments(),
-                    Times.Once);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }
+        }        
 
         [Fact]
         public async Task ShouldRetrieveAttachmentByIdAsync()
