1adc0deb9cb74bb0b79760fd234292c44bc22c98
Tharani <tharanisantosh@outlook.com>
2022-03-10 10:08:58 +0200

    CODERUB: Separate Logic Test For Create Operation - Teachers

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Teachers/TeacherServiceTests.Logic.Create.cs
@@ -0,0 +1,60 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.Teachers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Teachers
+{
+    public partial class TeacherServiceTests
+    {
+        [Fact]
+        public async Task ShouldCreateTeacherAsync()
+        {
+            // given
+            DateTimeOffset dateTime = DateTimeOffset.UtcNow;
+            Teacher randomTeacher = CreateRandomTeacher(dateTime);
+            randomTeacher.UpdatedBy = randomTeacher.CreatedBy;
+            randomTeacher.UpdatedDate = randomTeacher.CreatedDate;
+            Teacher inputTeacher = randomTeacher;
+            Teacher storageTeacher = randomTeacher;
+            Teacher expectedTeacher = randomTeacher;
+
+            this.dateTimeBrokerMock.Setup(broker =>
+               broker.GetCurrentDateTime())
+                   .Returns(dateTime);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertTeacherAsync(inputTeacher))
+                    .ReturnsAsync(storageTeacher);
+
+            // when
+            Teacher actualTeacher =
+                await this.teacherService.CreateTeacherAsync(inputTeacher);
+
+            // then
+            actualTeacher.Should().BeEquivalentTo(expectedTeacher);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertTeacherAsync(inputTeacher),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
