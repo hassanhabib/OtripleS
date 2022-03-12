5c6c27f6f774260d31be38b022bdaf9377ace830
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-12 11:45:57 -0800

    CODE RUB: Split logic files for Retrieve By Id Attendances

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Attendances/AttendanceServiceTests.Logic.RetreieveById.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Attendances/AttendanceServiceTests.Logic.RetreieveById.cs
@@ -4,10 +4,11 @@
 // ---------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Force.DeepCloner;
 using Moq;
 using OtripleS.Web.Api.Models.Attendances;
 using Xunit;
@@ -15,29 +16,26 @@ using Xunit;
 namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Attendances
 {
     public partial class AttendanceServiceTests
-    {           
+    {
         [Fact]
-        public async Task ShouldDeleteAttendanceAsync()
+        public async Task ShouldRetrieveAttendanceByIdAsync()
         {
             // given
-            DateTimeOffset dateTime = GetRandomDateTime();
-            Attendance randomAttendance = CreateRandomAttendance(dateTime: dateTime);
-            Guid inputAttendanceId = randomAttendance.Id;
-            Attendance inputAttendance = randomAttendance;
-            Attendance storageAttendance = inputAttendance;
-            Attendance expectedAttendance = storageAttendance;
+            DateTimeOffset dateTimeOffset = GetRandomDateTime();
+            Guid randomAttendanceId = Guid.NewGuid();
+            Guid inputAttendanceId = randomAttendanceId;
+            Attendance randomAttendance = CreateRandomAttendance(dateTime: dateTimeOffset);
+            randomAttendance.Id = randomAttendanceId;
+            Attendance storageAttendance = randomAttendance;
+            Attendance expectedAttendance = randomAttendance;
 
             this.storageBrokerMock.Setup(broker =>
                 broker.SelectAttendanceByIdAsync(inputAttendanceId))
-                    .ReturnsAsync(inputAttendance);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.DeleteAttendanceAsync(inputAttendance))
                     .ReturnsAsync(storageAttendance);
 
             // when
             Attendance actualAttendance =
-                await this.attendanceService.RemoveAttendanceByIdAsync(inputAttendanceId);
+                await this.attendanceService.RetrieveAttendanceByIdAsync(inputAttendanceId);
 
             // then
             actualAttendance.Should().BeEquivalentTo(expectedAttendance);
@@ -46,13 +44,13 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Attendances
                 broker.SelectAttendanceByIdAsync(inputAttendanceId),
                     Times.Once);
 
-            this.storageBrokerMock.Verify(broker =>
-                broker.DeleteAttendanceAsync(inputAttendance),
-                    Times.Once);
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
-        }        
+        }
     }
 }
