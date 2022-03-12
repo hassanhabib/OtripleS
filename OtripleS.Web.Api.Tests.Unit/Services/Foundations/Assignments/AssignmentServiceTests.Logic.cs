6d1c6857ff0a01ac80c61f510c2dde4997c7f000
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-11 16:23:24 -0800

    CODERUB: Split logic files for Modify assignments

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Assignments/AssignmentServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Assignments/AssignmentServiceTests.Logic.cs
@@ -15,59 +15,7 @@ using Xunit;
 namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Assignments
 {
     public partial class AssignmentServiceTests
-    {        
-        [Fact]
-        public async Task ShouldModifyAssignmentAsync()
-        {
-            // given
-            int randomNumber = GetRandomNumber();
-            int randomDays = randomNumber;
-            DateTimeOffset randomDate = GetRandomDateTime();
-            DateTimeOffset randomInputDate = GetRandomDateTime();
-            Assignment randomAssignment = CreateRandomAssignment(randomInputDate);
-            Assignment inputAssignment = randomAssignment;
-            Assignment afterUpdateStorageAssignment = inputAssignment;
-            Assignment expectedAssignment = afterUpdateStorageAssignment;
-            Assignment beforeUpdateStorageAssignment = randomAssignment.DeepClone();
-            inputAssignment.UpdatedDate = randomDate;
-            Guid assignmentId = inputAssignment.Id;
-
-            this.dateTimeBrokerMock.Setup(broker =>
-               broker.GetCurrentDateTime())
-                   .Returns(randomDate);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectAssignmentByIdAsync(assignmentId))
-                    .ReturnsAsync(beforeUpdateStorageAssignment);
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.UpdateAssignmentAsync(inputAssignment))
-                    .ReturnsAsync(afterUpdateStorageAssignment);
-
-            // when
-            Assignment actualAssignment =
-                await this.assignmentService.ModifyAssignmentAsync(inputAssignment);
-
-            // then
-            actualAssignment.Should().BeEquivalentTo(expectedAssignment);
-
-            this.dateTimeBrokerMock.Verify(broker =>
-                broker.GetCurrentDateTime(),
-                    Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectAssignmentByIdAsync(assignmentId),
-                    Times.Once);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.UpdateAssignmentAsync(inputAssignment),
-                    Times.Once);
-
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-        }
-
+    {                
         [Fact]
         public void ShouldRetrieveAllAssignments()
         {
