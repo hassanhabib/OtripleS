d7abddd2e96292c4ffaf26a202e9bbeeccf333ef
Janane <imjanane@gmail.com>
2022-03-12 15:53:21 -0600

    CODE RUB : Separate Logictes for each operation - Course Attachments

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/CourseAttachments/CourseAttachmentServiceTests.Logic.Add.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using OtripleS.Web.Api.Models.CourseAttachments;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.CourseAttachments
+{
+    public partial class CourseAttachmentServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddCourseAttachmentAsync()
+        {
+            // given
+            CourseAttachment randomCourseAttachment = CreateRandomCourseAttachment();
+            CourseAttachment inputCourseAttachment = randomCourseAttachment;
+            CourseAttachment storageCourseAttachment = randomCourseAttachment;
+            CourseAttachment expectedCourseAttachment = storageCourseAttachment;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertCourseAttachmentAsync(inputCourseAttachment))
+                    .ReturnsAsync(storageCourseAttachment);
+
+            // when
+            CourseAttachment actualCourseAttachment =
+                await this.courseAttachmentService.AddCourseAttachmentAsync(inputCourseAttachment);
+
+            // then
+            actualCourseAttachment.Should().BeEquivalentTo(expectedCourseAttachment);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertCourseAttachmentAsync(inputCourseAttachment),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
