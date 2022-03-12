4a71d94992a944eb0ba1d2fdd2789298d8011246
Janane <imjanane@gmail.com>
2022-03-12 15:47:35 -0600

    CODE RUB : Separate Logic tests for each operation - Contacts

---
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Contacts/ContactServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,52 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using OtripleS.Web.Api.Models.Contacts;
+using Xunit;
+
+namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Contacts
+{
+    public partial class ContactServiceTests
+    {
+        [Fact]
+        public void ShouldRetrieveAllContacts()
+        {
+            // given
+            DateTimeOffset randomDateTime = GetRandomDateTime();
+            IQueryable<Contact> randomContacts = CreateRandomContacts(randomDateTime);
+            IQueryable<Contact> storageContacts = randomContacts;
+            IQueryable<Contact> expectedContacts = storageContacts;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllContacts())
+                    .Returns(storageContacts);
+
+            // when
+            IQueryable<Contact> actualContacts =
+                this.contactService.RetrieveAllContacts();
+
+            // then
+            actualContacts.Should().BeEquivalentTo(expectedContacts);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllContacts(),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
