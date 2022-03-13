0030a89ba544483d1d01a327794631bf8156b8c5
Deepali Shrivastava <deepali.shrivastava7@gmail.com>
2022-03-13 12:13:14 -0700

    CODE RUB: Split logic files for Retrieve All Assignments

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Assignments/AssignmentServiceTests.Logic.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Assignments/AssignmentServiceTests.Logic.cs
@@ -15,38 +15,6 @@ using Xunit;
 namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Assignments
 {
     public partial class AssignmentServiceTests
-    {        
-        [Fact]
-        public void ShouldRetrieveAllAssignments()
-        {
-            // given
-            DateTimeOffset randomDateTime = GetRandomDateTime();
-            IQueryable<Assignment> randomAssignments = CreateRandomAssignments(randomDateTime);
-            IQueryable<Assignment> storageAssignments = randomAssignments;
-            IQueryable<Assignment> expectedAssignments = storageAssignments;
-
-            this.storageBrokerMock.Setup(broker =>
-                broker.SelectAllAssignments())
-                    .Returns(storageAssignments);
-
-            // when
-            IQueryable<Assignment> actualAssignments =
-                this.assignmentService.RetrieveAllAssignments();
-
-            // then
-            actualAssignments.Should().BeEquivalentTo(expectedAssignments);
-
-            this.storageBrokerMock.Verify(broker =>
-                broker.SelectAllAssignments(),
-                    Times.Once);
-
-            this.dateTimeBrokerMock.Verify(broker =>
-                broker.GetCurrentDateTime(),
-                    Times.Never);
-
-            this.storageBrokerMock.VerifyNoOtherCalls();
-            this.dateTimeBrokerMock.VerifyNoOtherCalls();
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-        }        
+    {                
     }
 }
