6bad566ba3b263f3120f7b14c8af56391ebc413f
Tharani <tharanisantosh@outlook.com>
2022-03-28 11:06:38 +0200

    CODE RUB : Teacher Contacts - RetrieveById - DbUpdateException

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherContacts/TeacherContactServiceTests.Exceptions.RetrieveById.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherContacts/TeacherContactServiceTests.Exceptions.RetrieveById.cs
@@ -63,7 +63,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.TeacherContacts
                 new TeacherContactDependencyException(databaseUpdateException);
 
             this.storageBrokerMock.Setup(broker =>
-                broker.SelectTeacherContactByIdAsync(someTeacherId, someContactId))
+                broker.SelectTeacherContactByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
                     .ThrowsAsync(databaseUpdateException);
 
             // when
@@ -79,7 +79,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.TeacherContacts
                         Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
-                broker.SelectTeacherContactByIdAsync(someTeacherId, someContactId),
+                broker.SelectTeacherContactByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()),
                     Times.Once);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
