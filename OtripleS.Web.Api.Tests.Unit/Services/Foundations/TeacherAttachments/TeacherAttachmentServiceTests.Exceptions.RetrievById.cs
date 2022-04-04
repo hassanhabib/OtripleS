290c1ae67604c8a47bfd2788aeb8b158f19ae761
Tharani <tharanisantosh@outlook.com>
2022-04-04 15:54:38 +0200

    CODE RUB : Teacher Attachments - Retrieve By Id - Sql Exception

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherAttachments/TeacherAttachmentServiceTests.Exceptions.RetrievById.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/TeacherAttachments/TeacherAttachmentServiceTests.Exceptions.RetrievById.cs
@@ -26,7 +26,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.TeacherAttachments
             var expectedTeacherAttachmentDependencyException = new TeacherAttachmentDependencyException(sqlException);
 
             this.storageBrokerMock.Setup(broker =>
-                 broker.SelectTeacherAttachmentByIdAsync(someTeacherId, someAttachmentId))
+                 broker.SelectTeacherAttachmentByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
                     .ThrowsAsync(sqlException);
 
             // when
@@ -43,7 +43,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.TeacherAttachments
                         Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
-                broker.SelectTeacherAttachmentByIdAsync(someTeacherId, someAttachmentId),
+                broker.SelectTeacherAttachmentByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>()),
                     Times.Once);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
