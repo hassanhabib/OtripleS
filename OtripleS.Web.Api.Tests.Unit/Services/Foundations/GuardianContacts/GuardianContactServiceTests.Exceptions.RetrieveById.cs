01b87f95fd4b28df0048056c0875523f3bf0663b
Tharani <tharanisantosh@outlook.com>
2022-05-07 07:59:26 +0200

    CODE RUB: GuardianContacts-RetrieveById-Exception-SqlException-InputToSome

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/GuardianContacts/GuardianContactServiceTests.Exceptions.RetrieveById.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/GuardianContacts/GuardianContactServiceTests.Exceptions.RetrieveById.cs
@@ -20,24 +20,21 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.GuardianContacts
         public async Task ShouldThrowDependencyExceptionOnRetrieveWhenSqlExceptionOccursAndLogItAsync()
         {
             // given
-            var randomContactId = Guid.NewGuid();
-            var inputContactId = randomContactId;
-            Guid randomGuardianId = Guid.NewGuid();
-            Guid inputGuardianId = randomGuardianId;
+            Guid someContactId = Guid.NewGuid();
+            Guid someGuardianId = Guid.NewGuid();
             SqlException sqlException = GetSqlException();
 
             var expectedGuardianContactDependencyException =
                 new GuardianContactDependencyException(sqlException);
 
             this.storageBrokerMock.Setup(broker =>
-                 broker.SelectGuardianContactByIdAsync(inputGuardianId, inputContactId))
+                 broker.SelectGuardianContactByIdAsync(someGuardianId, someContactId))
                     .ThrowsAsync(sqlException);
 
             // when
             ValueTask<GuardianContact> retrieveGuardianContactTask =
                 this.guardianContactService.RetrieveGuardianContactByIdAsync(
-                    inputGuardianId,
-                    inputContactId);
+                        someGuardianId, someContactId);
 
             // then
             await Assert.ThrowsAsync<GuardianContactDependencyException>(() =>
@@ -49,7 +46,7 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.GuardianContacts
                         Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
-                broker.SelectGuardianContactByIdAsync(inputGuardianId, inputContactId),
+                broker.SelectGuardianContactByIdAsync(someGuardianId, someContactId),
                     Times.Once);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
