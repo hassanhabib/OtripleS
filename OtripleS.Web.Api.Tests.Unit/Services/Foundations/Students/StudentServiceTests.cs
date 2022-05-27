13c8b36e99a8992be853f2d2fe4e1beb507e93df
Mahder Awod <mahinoah_2019@yahoo.com>
2022-05-27 12:26:40 -0700

    CODE RUB: Student SameExceptionAs correction

--- OtripleS.Web.Api.Tests.Unit/Services/Foundations/Students/StudentServiceTests.cs
+++ OtripleS.Web.Api.Tests.Unit/Services/Foundations/Students/StudentServiceTests.cs
@@ -68,8 +68,8 @@ namespace OtripleS.Web.Api.Tests.Unit.Services.Foundations.Students
         private static Expression<Func<Exception, bool>> SameExceptionAs(Exception expectedException)
         {
             return actualException =>
-                  actualException.Message == expectedException.Message
-                  && actualException.InnerException.Message == expectedException.InnerException.Message;
+                actualException.Message == expectedException.Message
+                && actualException.InnerException.Message == expectedException.InnerException.Message;
         }
 
         private static SqlException GetSqlException() =>
