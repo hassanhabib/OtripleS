3b9da0b8ab79535756bb08a101747805636f233f
Shivang Soni <shivangsoni22@gmail.com>
2022-06-12 12:48:56 -0700

    Added Storage Broker Interface for Meal.

---
+++ OtripleS.Web.Api/Brokers/Storages/IStorageBroker.Meals.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OtripleS.Web.Api.Models.Meals;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtripleS.Web.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        public DbSet<Meal> Meals { get; set; }
+        
+    }
+}
