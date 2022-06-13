7ed96053d79193e5ae29debc3bacadf000f0757f
Shivang Soni <shivangsoni22@gmail.com>
2022-06-13 15:01:14 -0700

    Autogenerating migration files

--- OtripleS.Web.Api/Brokers/Storages/IStorageBroker.Meals.cs
+++ OtripleS.Web.Api/Brokers/Storages/IStorageBroker.Meals.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
 using OtripleS.Web.Api.Models.Meals;
 using System.Data.Entity;
 
@@ -8,6 +13,6 @@ namespace OtripleS.Web.Api.Brokers.Storages
     public partial interface IStorageBroker
     {
         
-        
+    
     }
 }
---
+++ OtripleS.Web.Api/Migrations/20220613215907_AddMealsMigration.cs
@@ -0,0 +1,24 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OtripleS.Web.Api.Migrations
+{
+    public partial class AddMealsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
