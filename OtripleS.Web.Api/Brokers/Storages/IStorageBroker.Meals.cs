7402b01ad47bd409f55ec1448d33f21445501ab9
Shivang Soni <shivangsoni22@gmail.com>
2022-06-14 07:31:31 -0700

    DATA: Model & Migrate Meals

---
+++ OtripleS.Web.Api/Brokers/Storages/IStorageBroker.Meals.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+
+namespace OtripleS.Web.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+
+
+    }
+}
--- OtripleS.Web.Api/Migrations/20220614142703_AddMealsModel.Designer.cs
+++ OtripleS.Web.Api/Migrations/20220614142703_AddMealsModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OtripleS.Web.Api.Brokers.Storages;
 
@@ -11,9 +12,10 @@ using OtripleS.Web.Api.Brokers.Storages;
 namespace OtripleS.Web.Api.Migrations
 {
     [DbContext(typeof(StorageBroker))]
-    partial class StorageBrokerModelSnapshot : ModelSnapshot
+    [Migration("20220614142703_AddMealsModel")]
+    partial class AddMealsModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
