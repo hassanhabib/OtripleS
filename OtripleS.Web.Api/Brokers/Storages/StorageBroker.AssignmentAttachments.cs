8688c641102b810549471c696a9272e1eb5373e1
Mahder Awod <mahinoah_2019@yahoo.com>
2022-08-03 23:25:49 -0700

    CODE RUB:  Remove Extra Word

--- OtripleS.Web.Api/Brokers/Storages/StorageBroker.AssignmentAttachments.cs
+++ OtripleS.Web.Api/Brokers/Storages/StorageBroker.AssignmentAttachments.cs
@@ -19,7 +19,7 @@ namespace OtripleS.Web.Api.Brokers.Storages
         public async ValueTask<AssignmentAttachment> InsertAssignmentAttachmentAsync(
             AssignmentAttachment assignmentAttachment)
         {
-            using var broker = new StorageBroker(this.configuration);
+            var broker = new StorageBroker(this.configuration);
 
             EntityEntry<AssignmentAttachment> assignmentAttachmentEntityEntry =
                 await broker.AssignmentAttachments.AddAsync(entity: assignmentAttachment);
