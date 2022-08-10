d233c1c5316ca2d1081373dff48f94e91ed2c5b9
Mahder Awod <mahinoah_2019@yahoo.com>
2022-08-10 15:59:09 -0700

    CODE RUB:  Remove Extra Word

--- OtripleS.Web.Api/Brokers/Storages/StorageBroker.Calendars.cs
+++ OtripleS.Web.Api/Brokers/Storages/StorageBroker.Calendars.cs
@@ -54,7 +54,7 @@ namespace OtripleS.Web.Api.Brokers.Storages
 
         public async ValueTask<Calendar> DeleteCalendarAsync(Calendar calendar)
         {
-            using var broker = new StorageBroker(this.configuration);
+            var broker = new StorageBroker(this.configuration);
 
             EntityEntry<Calendar> calendarEntityEntry =
                 broker.Calendars.Remove(entity: calendar);
