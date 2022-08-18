b99a2272153529f09b23edf89ef9cb8156afbd03
Mahder Awod <mahinoah_2019@yahoo.com>
2022-08-17 22:43:22 -0700

    CODE RUB:  Remove Extra Word

--- OtripleS.Web.Api/Brokers/Storages/StorageBroker.Contacts.cs
+++ OtripleS.Web.Api/Brokers/Storages/StorageBroker.Contacts.cs
@@ -37,7 +37,7 @@ namespace OtripleS.Web.Api.Brokers.Storages
 
         public async ValueTask<Contact> UpdateContactAsync(Contact contact)
         {
-            using var broker = new StorageBroker(this.configuration);
+            var broker = new StorageBroker(this.configuration);
             EntityEntry<Contact> contactEntityEntry = broker.Contacts.Update(entity: contact);
             await broker.SaveChangesAsync();
 
