85398b6c4fb0f3324e025a14ad5e6c7bf6a32e95
Mahder Awod <mahinoah_2019@yahoo.com>
2022-09-13 01:02:21 -0700

    CODE RUB: Remove Extra Word

--- OtripleS.Web.Api/Brokers/Storages/StorageBroker.StudentGuardians.cs
+++ OtripleS.Web.Api/Brokers/Storages/StorageBroker.StudentGuardians.cs
@@ -52,7 +52,7 @@ namespace OtripleS.Web.Api.Brokers.Storages
 
         public async ValueTask<StudentGuardian> DeleteStudentGuardianAsync(StudentGuardian studentGuardian)
         {
-            using var broker = new StorageBroker(this.configuration);
+            var broker = new StorageBroker(this.configuration);
 
             EntityEntry<StudentGuardian> studentGuardianEntityEntry =
                 broker.StudentGuardians.Remove(entity: studentGuardian);
