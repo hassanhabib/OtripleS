a6bdd4d03c959c8c5dce8cb18a1e8691ccb0c31d
Mahder Awod <mahinoah_2019@yahoo.com>
2022-09-13 01:11:19 -0700

    CODE RUB: Remove Extra Word

--- OtripleS.Web.Api/Brokers/Storages/StorageBroker.Students.cs
+++ OtripleS.Web.Api/Brokers/Storages/StorageBroker.Students.cs
@@ -18,7 +18,7 @@ namespace OtripleS.Web.Api.Brokers.Storages
 
         public async ValueTask<Student> InsertStudentAsync(Student student)
         {
-            using var broker = new StorageBroker(this.configuration);
+            var broker = new StorageBroker(this.configuration);
             EntityEntry<Student> studentEntityEntry = await broker.Students.AddAsync(entity: student);
             await broker.SaveChangesAsync();
 
