ec3113d1cc90b38c6c05071df982364f544c6e75
Mahder Awod <mahinoah_2019@yahoo.com>
2022-09-13 00:59:24 -0700

    CODE RUB: Remove Extra Word

--- OtripleS.Web.Api/Brokers/Storages/StorageBroker.StudentGuardians.cs
+++ OtripleS.Web.Api/Brokers/Storages/StorageBroker.StudentGuardians.cs
@@ -32,7 +32,7 @@ namespace OtripleS.Web.Api.Brokers.Storages
 
         public async ValueTask<StudentGuardian> SelectStudentGuardianByIdAsync(Guid studentId, Guid guardianId)
         {
-            using var broker = new StorageBroker(this.configuration);
+            var broker = new StorageBroker(this.configuration);
             broker.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
 
             return await broker.StudentGuardians.FindAsync(studentId, guardianId);
