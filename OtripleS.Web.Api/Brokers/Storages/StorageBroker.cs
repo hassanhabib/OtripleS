4b942ca7712aacdf347feec1083f2c4d01584dd1
Hassan Habib <hassanhabib@live.com>
2022-09-25 15:24:46 -0700

    STANDARD: Change EF Implementation

--- OtripleS.Web.Api/Brokers/Storages/StorageBroker.cs
+++ OtripleS.Web.Api/Brokers/Storages/StorageBroker.cs
@@ -4,6 +4,8 @@
 // ---------------------------------------------------------------
 
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 using EFxceptions.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -19,6 +21,35 @@ namespace OtripleS.Web.Api.Brokers.Storages
         {
             this.configuration = configuration;
             this.Database.Migrate();
+        }
+
+        private async ValueTask<T> InsertAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Added;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
+        private IQueryable<T> SelectAll<T>() where T : class => this.Set<T>();
+
+        private async ValueTask<T> SelectAsync<T>(params object[] @objectIds) where T : class =>
+            await this.FindAsync<T>(objectIds);
+
+        private async ValueTask<T> UpdateAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Modified;
+            await this.SaveChangesAsync();
+
+            return @object;
+        }
+
+        private async ValueTask<T> DeleteAsync<T>(T @object)
+        {
+            this.Entry(@object).State = EntityState.Deleted;
+            await this.SaveChangesAsync();
+
+            return @object;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -50,6 +81,7 @@ namespace OtripleS.Web.Api.Brokers.Storages
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
             string connectionString = this.configuration.GetConnectionString("DefaultConnection");
             optionsBuilder.UseSqlServer(connectionString);
         }
