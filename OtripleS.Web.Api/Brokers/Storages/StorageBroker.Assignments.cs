cc94b0439dd5f3f5f89e5384a4b9cba89b9c557d
Mahder Awod <mahinoah_2019@yahoo.com>
2022-11-15 22:55:51 -0800

    CODE RUB: code update

--- OtripleS.Web.Api/Brokers/Storages/StorageBroker.Assignments.cs
+++ OtripleS.Web.Api/Brokers/Storages/StorageBroker.Assignments.cs
@@ -25,7 +25,7 @@ namespace OtripleS.Web.Api.Brokers.Storages
             return assignmentEntityEntry.Entity;
         }
 
-        public IQueryable<Assignment> SelectAllAssignments() => this.Assignments;
+        public IQueryable<Assignment> SelectAllAssignments() => SelectAllAssignments();
 
         public async ValueTask<Assignment> SelectAssignmentByIdAsync(Guid assignmentId)
         {
