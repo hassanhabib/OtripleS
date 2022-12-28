a12215b5e28cc03b384fce99a5a9c34738999d3e
Mahder Awod <mahinoah_2019@yahoo.com>
2022-12-27 23:32:08 -0800

    CODE RUB: code update

--- OtripleS.Web.Api/Brokers/Storages/StorageBroker.CalendarEntryAttachments.cs
+++ OtripleS.Web.Api/Brokers/Storages/StorageBroker.CalendarEntryAttachments.cs
@@ -55,17 +55,7 @@ namespace OtripleS.Web.Api.Brokers.Storages
             return calendarEntryAttachmentEntityEntry.Entity;
         }
 
-        public async ValueTask<CalendarEntryAttachment> DeleteCalendarEntryAttachmentAsync(
-            CalendarEntryAttachment calendarEntryAttachment)
-        {
-            var broker = new StorageBroker(this.configuration);
-
-            EntityEntry<CalendarEntryAttachment> calendarEntryAttachmentEntityEntry =
-                broker.CalendarEntriesAttachments.Remove(entity: calendarEntryAttachment);
-
-            await broker.SaveChangesAsync();
-
-            return calendarEntryAttachmentEntityEntry.Entity;
-        }
+        public async ValueTask<CalendarEntryAttachment> DeleteCalendarEntryAttachmentAsync(CalendarEntryAttachment CalendarEntryAttachment) =>
+              await DeleteCalendarEntryAttachmentAsync(CalendarEntryAttachment);
     }
 }
